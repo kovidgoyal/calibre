20b7378b190d3179e1c193aeecd41e529d3cf91b
Kovid Goyal <kovid@kovidgoyal.net>
2006-12-14 03:10:09 +0000

    Updated package meta-data

--- setup.py
+++ setup.py
@@ -29,14 +29,18 @@ ez_setup.use_setuptools()
 from setuptools import setup, find_packages
 from libprs500 import __version__ as VERSION
 
-setup(name='libprs500',
+setup(
+      name='libprs500',
+      packages = find_packages(), 
+      version=VERSION,
+      author='Kovid Goyal',
+      author_email='kovid@kovidgoyal.net',
+      url = 'http://libprs500.kovidgoyal.net',      
       entry_points = {
         'console_scripts': [ 'prs500 = libprs500.cli.main:main', 'lrf-meta = libprs500.lrf.meta:main' ],
         'gui_scripts'    : [ 'prs500-gui = libprs500.gui.main:main']
-      },
-      package_data = {'libprs500.gui' : ['*.ui']},
-      zip_safe = True,
-      version=VERSION,
+      },      
+      zip_safe = True,      
       install_requires=["pyusb>=0.3.5","pyxml>=0.8.4"],
       dependency_links=["http://sourceforge.net/project/showfiles.php?group_id=145185",
                                       "http://sourceforge.net/project/showfiles.php?group_id=6473",
@@ -48,7 +52,8 @@ setup(name='libprs500',
       It provides methods to list the contents of the file system on the device,
       as well as copy files from and to the device. 
       It also provides a command line and a graphical user interface via the commands prs500 and
-      prs500-gui. 
+      prs500-gui. The graphical user interface is designed to manage an ebook library and allows for easy 
+      syncing between the library and the ebook reader. 
       In addition libprs500 has a utility to read/write the metadata from LRF files (unencrypted books in the SONY BBeB format). A command line
       interface to this is provided via the command lrf-meta.
       
@@ -57,12 +62,7 @@ setup(name='libprs500',
         .. _SONY Portable Reader: http://Sony.com/reader
         .. _USB: http://www.usb.org  
       """,      
-      author='Kovid Goyal',
-      author_email='kovid@kovidgoyal.net',
-      provides=['libprs500'],      
-      packages = find_packages(),      
       license = 'GPL',
-      url = 'http://libprs500.kovidgoyal.net',
       classifiers = [
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
