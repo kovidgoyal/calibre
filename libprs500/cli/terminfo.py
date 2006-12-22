4b2afac7195c30eee88bda857dc16f0a7b172502
Kovid Goyal <kovid@kovidgoyal.net>
2006-12-22 19:57:01 +0000

    Re-indent libprs500.cli to 4 spaces as per PEP8

--- libprs500/cli/terminfo.py
+++ libprs500/cli/terminfo.py
@@ -25,16 +25,16 @@ class TerminalController:
     values are initialized to the control sequence necessary to
     perform a given action.  These can be simply included in normal
     output to the terminal:
-
-        >>> term = TerminalController()
-        >>> print 'This is '+term.GREEN+'green'+term.NORMAL
-
+    
+    >>> term = TerminalController()
+    >>> print 'This is '+term.GREEN+'green'+term.NORMAL
+    
     Alternatively, the `render()` method can used, which replaces
     '${action}' with the string required to perform 'action':
-
-        >>> term = TerminalController()
-        >>> print term.render('This is ${GREEN}green${NORMAL}')
-
+    
+    >>> term = TerminalController()
+    >>> print term.render('This is ${GREEN}green${NORMAL}')
+    
     If the terminal doesn't support a given action, then the value of
     the corresponding instance variable will be set to ''.  As a
     result, the above code will still work on terminals that do not
@@ -42,11 +42,11 @@ class TerminalController:
     Also, this means that you can test whether the terminal supports a
     given action by simply testing the truth value of the
     corresponding instance variable:
-
-        >>> term = TerminalController()
-        >>> if term.CLEAR_SCREEN:
-        ...     print 'This terminal supports clearning the screen.'
-
+    
+    >>> term = TerminalController()
+    >>> if term.CLEAR_SCREEN:
+    ...     print 'This terminal supports clearning the screen.'
+    
     Finally, if the width and height of the terminal are known, then
     they will be stored in the `COLS` and `LINES` attributes.
     """
@@ -56,28 +56,28 @@ class TerminalController:
     DOWN = ''            #: Move the cursor down one line
     LEFT = ''            #: Move the cursor left one char
     RIGHT = ''           #: Move the cursor right one char
-
+    
     # Deletion:
     CLEAR_SCREEN = ''    #: Clear the screen and move to home position
     CLEAR_EOL = ''       #: Clear to the end of the line.
     CLEAR_BOL = ''       #: Clear to the beginning of the line.
     CLEAR_EOS = ''       #: Clear to the end of the screen
-
+    
     # Output modes:
     BOLD = ''            #: Turn on bold mode
     BLINK = ''           #: Turn on blink mode
     DIM = ''             #: Turn on half-bright mode
     REVERSE = ''         #: Turn on reverse-video mode
     NORMAL = ''          #: Turn off all modes
-
+    
     # Cursor display:
     HIDE_CURSOR = ''     #: Make the cursor invisible
     SHOW_CURSOR = ''     #: Make the cursor visible
-
+    
     # Terminal size:
     COLS = None          #: Width of the terminal (None for unknown)
     LINES = None         #: Height of the terminal (None for unknown)
-
+    
     # Foreground colors:
     BLACK = BLUE = GREEN = CYAN = RED = MAGENTA = YELLOW = WHITE = ''
     
@@ -92,7 +92,7 @@ class TerminalController:
     HIDE_CURSOR=cinvis SHOW_CURSOR=cnorm""".split()
     _COLORS = """BLACK BLUE GREEN CYAN RED MAGENTA YELLOW WHITE""".split()
     _ANSICOLORS = "BLACK RED GREEN YELLOW BLUE MAGENTA CYAN WHITE".split()
-
+    
     def __init__(self, term_stream=sys.stdout):
         """
         Create a `TerminalController` and initialize its attributes
@@ -104,15 +104,15 @@ class TerminalController:
         # Curses isn't available on all platforms
         try: import curses
         except: return
-
+        
         # If the stream isn't a tty, then assume it has no capabilities.
         if not term_stream.isatty(): return
-
+        
         # Check the terminal type.  If we fail, then assume that the
         # terminal has no capabilities.
         try: curses.setupterm()
         except: return
-
+        
         # Look up numeric capabilities.
         self.COLS = curses.tigetnum('cols')
         self.LINES = curses.tigetnum('lines')
@@ -121,7 +121,7 @@ class TerminalController:
         for capability in self._STRING_CAPABILITIES:
             (attrib, cap_name) = capability.split('=')
             setattr(self, attrib, self._tigetstr(cap_name) or '')
-
+        
         # Colors
         set_fg = self._tigetstr('setf')
         if set_fg:
@@ -139,7 +139,7 @@ class TerminalController:
         if set_bg_ansi:
             for i,color in zip(range(len(self._ANSICOLORS)), self._ANSICOLORS):
                 setattr(self, 'BG_'+color, curses.tparm(set_bg_ansi, i) or '')
-
+    
     def _tigetstr(self, cap_name):
         # String capabilities can include "delays" of the form "$<2>".
         # For any modern terminal, we should be able to just ignore
@@ -147,7 +147,7 @@ class TerminalController:
         import curses
         cap = curses.tigetstr(cap_name) or ''
         return re.sub(r'\$<\d+>[/*]?', '', cap)
-
+    
     def render(self, template):
         """
         Replace each $-substitutions in the given template string with
@@ -155,7 +155,7 @@ class TerminalController:
         '' (if it's not).
         """
         return re.sub(r'\$\$|\${\w+}', self._render_sub, template)
-
+    
     def _render_sub(self, match):
         s = match.group()
         if s == '$$': return s
@@ -169,40 +169,40 @@ class ProgressBar:
     """
     A 3-line progress bar, which looks like::
     
-                                Header
-        20% [===========----------------------------------]
-                           progress message
-
+    Header
+    20% [===========----------------------------------]
+    progress message
+    
     The progress bar is colored, if the terminal supports color
     output; and adjusts to the width of the terminal.
     """
     BAR = '%3d%% ${GREEN}[${BOLD}%s%s${NORMAL}${GREEN}]${NORMAL}\n'
     HEADER = '${BOLD}${CYAN}%s${NORMAL}\n\n'
-        
+    
     def __init__(self, term, header):
         self.term = term
         if not (self.term.CLEAR_EOL and self.term.UP and self.term.BOL):
             raise ValueError("Terminal isn't capable enough -- you "
-                             "should use a simpler progress dispaly.")
+            "should use a simpler progress dispaly.")
         self.width = self.term.COLS or 75
         self.bar = term.render(self.BAR)
         self.header = self.term.render(self.HEADER % header.center(self.width))
         self.cleared = 1 #: true if we haven't drawn the bar yet.
         self.update(0, '')
-
+    
     def update(self, percent, message):
         if self.cleared:
             sys.stdout.write(self.header)
             self.cleared = 0
         n = int((self.width-10)*percent)
         sys.stdout.write(
-            self.term.BOL + self.term.UP + self.term.CLEAR_EOL +
-            (self.bar % (100*percent, '='*n, '-'*(self.width-10-n))) +
-            self.term.CLEAR_EOL + message.center(self.width))
-
+        self.term.BOL + self.term.UP + self.term.CLEAR_EOL +
+        (self.bar % (100*percent, '='*n, '-'*(self.width-10-n))) +
+        self.term.CLEAR_EOL + message.center(self.width))
+    
     def clear(self):
         if not self.cleared:
             sys.stdout.write(self.term.BOL + self.term.CLEAR_EOL +
-                             self.term.UP + self.term.CLEAR_EOL +
-                             self.term.UP + self.term.CLEAR_EOL)
+            self.term.UP + self.term.CLEAR_EOL +
+            self.term.UP + self.term.CLEAR_EOL)
             self.cleared = 1
