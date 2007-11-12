4b822e533c3932f05945f4dcc3996245709b8b58
Kovid Goyal <kovid@kovidgoyal.net>
2007-11-12 04:20:03 +0000

    Dont report exceptions in error handler.

--- src/libprs500/gui2/main_window.py
+++ src/libprs500/gui2/main_window.py
@@ -24,11 +24,14 @@ class MainWindow(QMainWindow):
         QMainWindow.__init__(self, parent)
     
     def unhandled_exception(self, type, value, tb):
-        sio = StringIO.StringIO()
-        traceback.print_exception(type, value, tb, file=sio)
-        fe = sio.getvalue()
-        print >>sys.stderr, fe
-        msg = '<p><b>' + unicode(str(value), 'utf8', 'replace') + '</b></p>'
-        msg += '<p>Detailed <b>traceback</b>:<pre>'+fe+'</pre>'
-        d = ConversionErrorDialog(self, 'ERROR: Unhandled exception', msg)
-        d.exec_()
+        try:
+            sio = StringIO.StringIO()
+            traceback.print_exception(type, value, tb, file=sio)
+            fe = sio.getvalue()
+            print >>sys.stderr, fe
+            msg = '<p><b>' + unicode(str(value), 'utf8', 'replace') + '</b></p>'
+            msg += '<p>Detailed <b>traceback</b>:<pre>'+fe+'</pre>'
+            d = ConversionErrorDialog(self, 'ERROR: Unhandled exception', msg)
+            d.exec_()
+        except:
+            pass
