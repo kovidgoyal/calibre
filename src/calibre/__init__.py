36a468dd5f06faa2519e41a6fa66f1e39b758990
Kovid Goyal <kovid@kovidgoyal.net>
2008-05-24 19:03:37 -0700

    version 0.4.62

--- src/calibre/__init__.py
+++ src/calibre/__init__.py
@@ -1,7 +1,7 @@
 ''' E-book management software'''
 __license__   = 'GPL v3'
 __copyright__ = '2008, Kovid Goyal <kovid at kovidgoyal.net>'
-__version__   = '0.4.61'
+__version__   = '0.4.62'
 __docformat__ = "epytext"
 __author__    = "Kovid Goyal <kovid at kovidgoyal.net>"
 __appname__   = 'calibre'
