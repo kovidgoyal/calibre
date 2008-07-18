7e20defcd8e87e1e1fc74b85ec8b3ac01010e56e
Kovid Goyal <kovid@kovidgoyal.net>
2008-07-18 12:06:50 -0700

    Add option to calibre-debug to update frozen code

--- src/calibre/libunzip.py
+++ src/calibre/libunzip.py
@@ -1,8 +1,41 @@
+#!/usr/bin/env  python
 __license__   = 'GPL v3'
-__copyright__ = '2008, Kovid Goyal <kovid at kovidgoyal.net>'
+__copyright__ = '2008, Kovid Goyal kovid@kovidgoyal.net'
+__docformat__ = 'restructuredtext en'
 
-import os, zipfile
+import os
 from cStringIO import StringIO
+from calibre.utils import zipfile
+
+def update(pathtozip, patterns, filepaths, names, compression=zipfile.ZIP_DEFLATED, verbose=True):
+    '''
+    Update files in the zip file at `pathtozip` matching the given 
+    `patterns` with the given `filepaths`. If more than
+    one file matches, all of the files are replaced. 
+    
+    :param patterns:    A list of compiled regular expressions
+    :param filepaths:   A list of paths to the replacement files. Must have the
+                        same length as `patterns`.
+    :param names:       A list of archive names for each file in filepaths. 
+                        A name can be `None` in which case the name of the existing
+                        file in the archive is used.
+    :param compression: The compression to use when replacing files. Can be
+                        either `zipfile.ZIP_DEFLATED` or `zipfile.ZIP_STORED`.
+    '''
+    assert len(patterns) == len(filepaths) == len(names)
+    z = zipfile.ZipFile(pathtozip, mode='a')
+    for name in z.namelist():
+        for pat, fname, new_name in zip(patterns, filepaths, names):
+            if pat.search(name):
+                if verbose:
+                    print 'Updating %s with %s' % (name, fname)
+                if new_name is None:
+                    z.replace(fname, arcname=name, compress_type=compression)
+                else:
+                    z.delete(name)
+                    z.write(fname, new_name, compress_type=compression)
+                break
+    z.close()
 
 def extract(filename, dir):
     """
