24d630842bfcd5c035ce5e72e0794a1cfd8ed0c1
Kovid Goyal <kovid@kovidgoyal.net>
2008-11-03 17:05:03 -0800

    IGN:...

--- src/calibre/ebooks/epub/__init__.py
+++ src/calibre/ebooks/epub/__init__.py
@@ -20,7 +20,7 @@ class DefaultProfile(object):
 class PRS505(DefaultProfile):
     
     flow_size   = 270000
-    screen_size = (590, 770)
+    screen_size = (590, 765)
     remove_special_chars = re.compile(u'[\u200b\u00ad]')
         
 
