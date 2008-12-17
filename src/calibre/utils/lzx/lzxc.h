0ea10932e2cf2d1941a88ffeaa74507fa19506b6
Kovid Goyal <kovid@kovidgoyal.net>
2008-12-17 14:30:14 -0800

    IGN:Add stdint.h for visual studio compilation

--- src/calibre/utils/lzx/lzxc.h
+++ src/calibre/utils/lzx/lzxc.h
@@ -15,6 +15,11 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
+#ifdef _MSC_VER
+#include "stdint.h"
+#endif
+
 typedef struct lzxc_data lzxc_data;
 typedef int (*lzxc_get_bytes_t)(void *arg, int n, void *buf);
 typedef int (*lzxc_put_bytes_t)(void *arg, int n, void *buf);
