af1319138aa0730c8d1f0ceef575b93a1bb99ba5
Kovid Goyal <kovid@kovidgoyal.net>
2008-12-21 22:02:24 -0800

    EPUB output: Fix parsing of HTML documents that contain spurious 0 bytes. This fixes Time recipe.

--- src/calibre/ebooks/html.py
+++ src/calibre/ebooks/html.py
@@ -447,6 +447,7 @@ class Parser(PreProcessor, LoggingInterface):
         ''' Create lxml ElementTree from HTML '''
         self.log_info('\tParsing '+os.sep.join(self.htmlfile.path.split(os.sep)[-3:]))
         src = open(self.htmlfile.path, 'rb').read().decode(self.htmlfile.encoding, 'replace').strip()
+        src = src.replace('\x00', '')
         src = self.preprocess(src)
         # lxml chokes on unicode input when it contains encoding declarations
         for pat in ENCODING_PATS:
