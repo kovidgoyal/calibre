fbba5f5db479d23cbeb9bbe8c4cbebb090c97e0e
Kovid Goyal <kovid@kovidgoyal.net>
2008-12-29 14:07:37 -0800

    Fix #1457 (error when add fb2 to library (PRS 505))

--- src/calibre/library/database2.py
+++ src/calibre/library/database2.py
@@ -471,8 +471,8 @@ class LibraryDatabase2(LibraryDatabase):
         authors = self.authors(id, index_is_id=True)
         if not authors:
             authors = _('Unknown')
-        author = sanitize_file_name(authors.split(',')[0][:self.PATH_LIMIT]).decode(filesystem_encoding)
-        title  = sanitize_file_name(self.title(id, index_is_id=True)[:self.PATH_LIMIT]).decode(filesystem_encoding)
+        author = sanitize_file_name(authors.split(',')[0][:self.PATH_LIMIT]).decode(filesystem_encoding, 'replace')
+        title  = sanitize_file_name(self.title(id, index_is_id=True)[:self.PATH_LIMIT]).decode(filesystem_encoding, 'replace')
         name   = title + ' - ' + author
         return name
     
