833b3d0a3526b83c1ae7c96b117314f406e6524b
Kovid Goyal <kovid@kovidgoyal.net>
2008-12-30 11:38:52 -0800

    version 0.4.122

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -2,7 +2,7 @@ __license__   = 'GPL v3'
 __copyright__ = '2008, Kovid Goyal kovid@kovidgoyal.net'
 __docformat__ = 'restructuredtext en'
 __appname__   = 'calibre'
-__version__   = '0.4.121'
+__version__   = '0.4.122'
 __author__    = "Kovid Goyal <kovid@kovidgoyal.net>"
 '''
 Various run time constants.
