4dc946d79b321f432868fcde1e6e5a1ac96dac5e
Kovid Goyal <kovid@kovidgoyal.net>
2009-01-12 12:32:32 -0800

    Fix #1603 (traceback on adding multiple books from directories, each directory single book with multiple versions)

--- src/calibre/library/database2.py
+++ src/calibre/library/database2.py
@@ -561,7 +561,15 @@ class LibraryDatabase2(LibraryDatabase):
                 img.loadFromData(f.read())
                 return img
             return f if as_file else f.read()
-        
+    
+    def has_book(self, mi):
+        title = mi.title
+        if title:
+            if not isinstance(title, unicode):
+                title = title.decode(preferred_encoding, 'replace')
+            return bool(self.conn.get('SELECT id FROM books where title=?', (title,), all=False))
+        return False
+    
     def has_cover(self, index, index_is_id=False):
         id = index if  index_is_id else self.id(index)
         path = os.path.join(self.library_path, self.path(id, index_is_id=True), 'cover.jpg')
