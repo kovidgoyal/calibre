1c9c8870d25722330234f1e6220c099f66e0afb0
Kovid Goyal <kovid@kovidgoyal.net>
2009-02-07 13:28:50 -0800

    IGN:Downloaded news HTML files are now always saved with the .xhtml extension

--- src/calibre/web/fetch/simple.py
+++ src/calibre/web/fetch/simple.py
@@ -410,6 +410,7 @@ class RecursiveFetcher(object, LoggingInterface):
                         _fname.decode('latin1', 'replace')
                     _fname = _fname.encode('ascii', 'replace').replace('%', '').replace(os.sep, '')
                     _fname = sanitize_file_name(_fname)
+                    _fname = os.path.splitext(_fname)[0]+'.xhtml'
                     res = os.path.join(linkdiskpath, _fname)
                     self.downloaded_paths.append(res)
                     self.filemap[nurl] = res
