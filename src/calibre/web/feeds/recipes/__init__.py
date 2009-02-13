b92651ec37fc94b087c8b6f7d1fd71f99a6fe841
Kovid Goyal <kovid@kovidgoyal.net>
2009-02-13 13:08:36 -0800

    New recipe for The Glasgow Herald by mccande

--- src/calibre/web/feeds/recipes/__init__.py
+++ src/calibre/web/feeds/recipes/__init__.py
@@ -28,7 +28,7 @@ recipe_modules = ['recipe_' + r for r in (
            'la_tercera', 'el_mercurio_chile', 'la_cuarta', 'lanacion_chile', 'la_segunda',
            'jb_online', 'estadao', 'o_globo', 'vijesti', 'elmundo', 'the_oz',
            'honoluluadvertiser', 'starbulletin', 'exiled', 'indy_star', 'dna',
-           'pobjeda', 'chicago_breaking_news',
+           'pobjeda', 'chicago_breaking_news', 'glasgow_herald',
           )]
 
 import re, imp, inspect, time, os
