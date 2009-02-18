f9501dce90a5fe13aafc35adf4966f0312ae033e
Kovid Goyal <kovid@kovidgoyal.net>
2009-02-18 12:49:31 -0800

    IGN:Updated translations

--- installer/windows/freeze.py
+++ installer/windows/freeze.py
@@ -135,33 +135,35 @@ class BuildEXE(py2exe.build_exe.py2exe):
         return (24, cls.manifest_resource_id,
                 cls.MANIFEST_TEMPLATE % dict(prog=prog, version=(VERSION+'.0')))
 
+def exe_factory(dest_base, script, icon_resources=None):
+    exe = {
+           'dest_base'       : dest_base,
+           'script'          : script,
+           'name'            : dest_base,
+           'version'         : VERSION+'.0',
+           'description'     : 'calibre - E-book library management',
+           'author'          : 'Kovid Goyal',
+           'copyright'       : '(c) Kovid Goyal, 2008',
+           'company'         : 'kovidgoyal.net',
+           }
+    if icon_resources is not None:
+        exe['icon_resources'] = icon_resources
+    return exe
 
 def main(args=sys.argv):
     sys.argv[1:2] = ['py2exe']
     if os.path.exists(PY2EXE_DIR):
         shutil.rmtree(PY2EXE_DIR)
 
-    console = [dict(dest_base=basenames['console'][i], script=scripts['console'][i])
+    console = [exe_factory(basenames['console'][i], scripts['console'][i])
                for i in range(len(scripts['console']))]
     setup(
           cmdclass = {'py2exe': BuildEXE},
           windows = [
-                     {'script'          : scripts['gui'][0],
-                      'dest_base'       : APPNAME,
-                      'icon_resources'  : [(1, ICONS[0])],
-                      #'other_resources' : [BuildEXE.manifest(APPNAME)],
-                      },
-                      {'script'         : scripts['gui'][1],
-                      'dest_base'       : 'lrfviewer',
-                      'icon_resources'  : [(1, ICONS[1])],
-                      #'other_resources' : [BuildEXE.manifest('lrfviewer')],
-                      },
-                      {'script'         : scripts['gui'][2],
-                      'dest_base'       : 'ebook-viewer',
-                      'icon_resources'  : [(1, ICONS[1])],
-                      #'other_resources' : [BuildEXE.manifest('ebook-viewer')],
-                      },
-                      ],
+                     exe_factory(APPNAME, scripts['gui'][0], [(1, ICONS[0])]),
+                     exe_factory('lrfviewer', scripts['gui'][1], [(1, ICONS[1])]),
+                     exe_factory('ebook-viewer', scripts['gui'][2], [(1, ICONS[1])]),
+                    ],
           console = console,
           options = { 'py2exe' : {'compressed': 1,
                                   'optimize'  : 2,
