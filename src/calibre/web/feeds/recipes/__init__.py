a2745fed4c35837816fd051c5c16c9757fdebed4
Kovid Goyal <kovid@kovidgoyal.net>
2009-02-17 20:04:48 -0800

    New recipe for Physicsworld and updated Ars Technica recipe for multi-page support

--- src/calibre/web/feeds/recipes/__init__.py
+++ src/calibre/web/feeds/recipes/__init__.py
@@ -29,7 +29,7 @@ recipe_modules = ['recipe_' + r for r in (
            'jb_online', 'estadao', 'o_globo', 'vijesti', 'elmundo', 'the_oz',
            'honoluluadvertiser', 'starbulletin', 'exiled', 'indy_star', 'dna',
            'pobjeda', 'chicago_breaking_news', 'glasgow_herald', 'linuxdevices',
-           'hindu', 'cincinnati_enquirer',
+           'hindu', 'cincinnati_enquirer', 'physics_world',
           )]
 
 import re, imp, inspect, time, os
