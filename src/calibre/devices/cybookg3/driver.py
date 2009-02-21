480a3cd3bbfb68f55d2b47d0d36d3e67d8e6834a
John Schember <john@nachtimwald.com>
2009-02-21 13:41:22 -0500

    Fix Cybook cover size being a postage stamp withing the thumbnail generated.

--- src/calibre/devices/cybookg3/driver.py
+++ src/calibre/devices/cybookg3/driver.py
@@ -35,6 +35,8 @@ class CYBOOKG3(USBMS):
     EBOOK_DIR_CARD = "eBooks"
     SUPPORTS_SUB_DIRS = True
     
+    THUMBNAIL_HEIGHT = 144
+    
     def upload_books(self, files, names, on_card=False, end_session=True, 
                      metadata=None):
         if on_card and not self._card_prefix:
