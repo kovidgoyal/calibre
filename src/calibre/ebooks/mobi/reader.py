1ee09b193012f76ed68fd0109048467e87935c6e
Kovid Goyal <kovid@kovidgoyal.net>
2009-03-08 10:43:28 -0700

    Fix #2017 (Another PRC file cover detection issue)

--- src/calibre/ebooks/mobi/reader.py
+++ src/calibre/ebooks/mobi/reader.py
@@ -370,6 +370,11 @@ class MobiReader(object):
             opf.cover = 'images/%05d.jpg'%(self.book_header.exth.cover_offset+1)
         elif mi.cover is not None:
             opf.cover = mi.cover
+        else:
+            opf.cover = 'images/%05d.jpg'%1
+            if not os.path.exists(os.path.join(os.path.dirname(htmlfile), 
+                                               *opf.cover.split('/'))):
+                opf.cover = None
         manifest = [(htmlfile, 'text/x-oeb1-document')]
         bp = os.path.dirname(htmlfile)
         for i in getattr(self, 'image_names', []):
