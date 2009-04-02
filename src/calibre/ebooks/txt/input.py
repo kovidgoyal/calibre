fca3f98e6a2b3e7dbd98da3618b1f3d805ad8c1e
John Schember <john@nachtimwald.com>
2009-04-01 20:05:34 -0400

    Text input plugin

---
+++ src/calibre/ebooks/txt/input.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from __future__ import with_statement
+
+__license__ = 'GPL 3'
+__copyright__ = '2009, John Schember <john@nachtimwald.com>'
+__docformat__ = 'restructuredtext en'
+
+import os
+
+from calibre.customize.conversion import InputFormatPlugin
+from calibre.ebooks.markdown import markdown
+from calibre.ebooks.metadata.opf import OPFCreator
+from calibre.ebooks.metadata import MetaInformation
+#from calibre.ebooks.metadata.meta import metadata_from_formats
+
+class TXTInput(InputFormatPlugin):
+    
+    name        = 'TXT Input'
+    author      = 'John Schember'
+    description = 'Convert TXT files to HTML'
+    file_types  = set(['txt'])
+
+    def convert(self, stream, options, file_ext, log,
+                accelerators):
+        txt = stream.read()
+        
+        md = markdown.Markdown(
+            extensions=['footnotes', 'tables', 'toc'],
+            safe_mode=False,)
+        html = '<html><body>'+md.convert(txt)+'</body></html>'
+        with open('index.html', 'wb') as index:
+            index.write(html.encode('utf-8'))
+            
+        #mi = metadata_from_formats([stream.name])
+        mi = MetaInformation(_('Unknown'), _('Unknown'))
+        opf = OPFCreator(os.getcwd(), mi)
+        opf.create_manifest([('index.html', None)])
+        opf.create_spine(['index.html'])
+        with open('metadata.opf', 'wb') as opffile:
+            opf.render(opffile)
+        
+        return os.path.join(os.getcwd(), 'metadata.opf')
