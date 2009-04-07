0847fcfb2d1944b3f98f20399e65143f95f5b52c
Kovid Goyal <kovid@kovidgoyal.net>
2009-04-07 11:04:28 -0700

    Preliminary support for the Netronix EB600

---
+++ src/calibre/devices/eb600/__init__.py
@@ -0,0 +1,2 @@
+__license__   = 'GPL v3'
+__copyright__ = '2008, Kovid Goyal <kovid at kovidgoyal.net>'
---
+++ src/calibre/devices/eb600/driver.py
@@ -0,0 +1,31 @@
+__license__   = 'GPL v3'
+__copyright__ = '2009, John Schember <john at nachtimwald.com>'
+'''
+Device driver for Amazon's Kindle
+'''
+
+from calibre.devices.usbms.driver import USBMS
+
+class EB600(USBMS):
+    # Ordered list of supported formats
+    FORMATS     = ['epub', 'pdf']
+
+    VENDOR_ID   = [0x1f85]
+    PRODUCT_ID  = [0x1688]
+    BCD         = [0x110]
+
+    VENDOR_NAME = 'NETRONIX'
+    WINDOWS_MAIN_MEM = 'EBOOK'
+    WINDOWS_CARD_MEM = 'CARD_STORAGE'
+
+    OSX_MAIN_MEM = 'EB600 Internal Storage Media'
+    OSX_CARD_MEM = 'EB600 Card Storage Media'
+
+    MAIN_MEMORY_VOLUME_LABEL  = 'EB600 Main Memory'
+    STORAGE_CARD_VOLUME_LABEL = 'EB600 Storage Card'
+
+    EBOOK_DIR_MAIN = ''
+    EBOOK_DIR_CARD = ''
+    SUPPORTS_SUB_DIRS = True
+
+
