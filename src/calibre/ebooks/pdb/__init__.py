5c40057adf085c13fd95b4f9a4e6f8ed5711c546
John Schember <john@nachtimwald.com>
2009-05-03 12:07:45 -0400

    pdb input: Better reporting of unknown formats

--- src/calibre/ebooks/pdb/__init__.py
+++ src/calibre/ebooks/pdb/__init__.py
@@ -7,17 +7,45 @@ __docformat__ = 'restructuredtext en'
 
 from calibre.ebooks.pdb.ereader.reader import Reader as eReader
 from calibre.ebooks.pdb.ztxt.reader import Reader as zTXT
+#from calibre.ebooks.pdb.palmdoc.reader import Reader as PalmDoc
 
 FORMATS = {
     'PNPdPPrs' : eReader,
     'PNRdPPrs' : eReader,
     'zTXTGPlm' : zTXT,
+#    'TEXtREAd' : PalmDoc,
 }
 
 IDENTITY_TO_NAME = {
     'PNPdPPrs' : 'eReader',
     'PNRdPPrs' : 'eReader',
     'zTXTGPlm' : 'zTXT',
+    'TEXtREAd' : 'PalmDOC',
+    
+    '.pdfADBE' : 'Adobe Reader',
+    'BVokBDIC' : 'BDicty',
+    'DB99DBOS' : 'DB (Database program)',
+    'vIMGView' : 'FireViewer (ImageViewer)',
+    'PmDBPmDB' : 'HanDBase',
+    'InfoINDB' : 'InfoView',
+    'ToGoToGo' : 'iSilo',
+    'SDocSilX' : 'iSilo 3',
+    'JbDbJBas' : 'JFile',
+    'JfDbJFil' : 'JFile Pro',
+    'DATALSdb' : 'LIST',
+    'Mdb1Mdb1' : 'MobileDB',
+    'BOOKMOBI' : 'MobiPocket',
+    'DataPlkr' : 'Plucker',
+    'DataSprd' : 'QuickSheet',
+    'SM01SMem' : 'SuperMemo',
+    'TEXtTlDc' : 'TealDoc',
+    'InfoTlIf' : 'TealInfo',
+    'DataTlMl' : 'TealMeal',
+    'DataTlPt' : 'TealPaint',
+    'dataTDBP' : 'ThinkDB',
+    'TdatTide' : 'Tides',
+    'ToRaTRPW' : 'TomeRaider',
+    'BDOCWrdS' : 'WordSmith',
 }
 
 class PDBError(Exception):
