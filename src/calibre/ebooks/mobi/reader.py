66b0b6c58fa30a8dca59a6604ab2ddb03bbc93cc
Kovid Goyal <kovid@kovidgoyal.net>
2009-05-02 19:26:40 -0700

    MOBI Input: Fix regression causing crash when converting certain MOBI/PRC files

--- src/calibre/ebooks/mobi/reader.py
+++ src/calibre/ebooks/mobi/reader.py
@@ -294,8 +294,7 @@ class MobiReader(object):
         root = html.fromstring(self.processed_html)
         if root.xpath('descendant::p/descendant::p'):
             from lxml.html import soupparser
-            self.log.warning('Markup contains unclosed <p> tags, parsing using',
-                'BeatifulSoup')
+            print 'Markup contains unclosed <p> tags, parsing using BeatifulSoup'
             root = soupparser.fromstring(self.processed_html)
         self.upshift_markup(root)
         guides = root.xpath('//guide')
