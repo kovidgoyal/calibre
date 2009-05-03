9b9381da3aae3384b29975e352cacd371daaf8d1
John Schember <john@nachtimwald.com>
2009-05-03 08:35:44 -0400

    plucker identity

--- src/calibre/ebooks/pdb/__init__.py
+++ src/calibre/ebooks/pdb/__init__.py
@@ -6,15 +6,18 @@ __copyright__ = '2009, John Schember <john@nachtimwald.com>'
 __docformat__ = 'restructuredtext en'
 
 from calibre.ebooks.pdb.ereader.reader import Reader as eReader
+from calibre.ebooks.pdb.plucker.reader import Reader as Plucker
 
 FORMATS = {
     'PNPdPPrs' : eReader,
     'PNRdPPrs' : eReader,
+    'DataPlkr' : Plucker,
 }
 
 IDENTITY_TO_NAME = {
     'PNPdPPrs' : 'eReader',
     'PNRdPPrs' : 'eReader',
+    'DataPlkr' : 'Plucker',
 }
 
 class PDBError(Exception):
