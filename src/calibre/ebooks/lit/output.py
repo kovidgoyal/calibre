aec5beb4c36c745fbfb67b3b8e72e87a8e4fdac1
Kovid Goyal <kovid@kovidgoyal.net>
2009-05-02 18:23:41 -0700

    Plugin for LIT Output

---
+++ src/calibre/ebooks/lit/output.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import with_statement
+
+__license__   = 'GPL v3'
+__copyright__ = '2009, Kovid Goyal <kovid@kovidgoyal.net>'
+__docformat__ = 'restructuredtext en'
+
+
+from calibre.customize.conversion import OutputFormatPlugin
+
+class LITOutput(OutputFormatPlugin):
+
+    name = 'LIT Output'
+    author = 'Marshall T. Vandegrift'
+    file_type = 'lit'
+
+    def convert(self, oeb, output_path, input_plugin, opts, log):
+        self.log, self.opts, self.oeb = log, opts, oeb
+        from calibre.ebooks.oeb.transforms.manglecase import CaseMangler
+        from calibre.ebooks.oeb.transforms.rasterize import SVGRasterizer
+        from calibre.ebooks.oeb.transforms.htmltoc import HTMLTOCAdder
+        from calibre.ebooks.lit.writer import LitWriter
+        tocadder = HTMLTOCAdder()
+        tocadder(oeb, opts)
+        mangler = CaseMangler()
+        mangler(oeb, opts)
+        rasterizer = SVGRasterizer()
+        rasterizer(oeb, opts)
+        lit = LitWriter()
+        lit(oeb, output_path)
+
+
