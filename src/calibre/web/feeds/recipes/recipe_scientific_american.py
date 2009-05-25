c410bb9ea9803d811f892c01352b7732411f8fb4
Kovid Goyal <kovid@kovidgoyal.net>
2009-05-25 13:08:55 -0700

    Fix #2497 (Scientific American recipe not working)

--- src/calibre/web/feeds/recipes/recipe_scientific_american.py
+++ src/calibre/web/feeds/recipes/recipe_scientific_american.py
@@ -7,124 +7,92 @@ __docformat__ = 'restructuredtext en'
 sciam.com
 '''
 import re
-from lxml import html
 from calibre.web.feeds.news import BasicNewsRecipe
 
 class ScientificAmerican(BasicNewsRecipe):
     title = u'Scientific American'
-    description = u'Popular science. Monthly magazine.' 
+    description = u'Popular science. Monthly magazine.'
     __author__ = 'Kovid Goyal'
     language = _('English')
-    oldest_article = 30 
+    oldest_article = 30
     max_articles_per_feed = 100
     no_stylesheets = True
     use_embedded_content   = False
     remove_tags_before = dict(name='div', attrs={'class':'headline'})
-    remove_tags_after  = dict(id='article')
+    remove_tags_after  = dict(id=['article'])
     remove_tags        = [
-                          dict(id=['sharetools', 'reddit']), 
+                          dict(id=['sharetools', 'reddit']),
                           dict(name='script'),
+                          {'class':['float_left', 'atools']},
                           {"class": re.compile(r'also-in-this')}
                          ]
     html2lrf_options = ['--base-font-size', '8']
     recursions = 1
-    match_regexps = [r'article.cfm.id=\S+page=(2|3|4|5|6|7|8|9|10|11|12|13|14)']
-#    feeds = [
-#             (u'Latest News', u'http://rss.sciam.com/ScientificAmerican-News'), 
-#             (u'Global', u'http://rss.sciam.com/ScientificAmerican-Global'), 
-#             (u'Health', u'http://rss.sciam.com/sciam/health'), 
-#             (u'Space', u'http://rss.sciam.com/sciam/space'), 
-#             (u'Technology', u'http://rss.sciam.com/sciam/technology'), 
-#             (u'Biology', u'http://rss.sciam.com/sciam/biology'), 
-#             (u'Mind & Brain', u'http://rss.sciam.com/sciam/mind-and-brain'), 
-#             (u"What's Next", u'http://rss.sciam.com/sciam/whats-next'), 
-#             (u'Archeology and Paleontology', u'http://www.sciam.com/page.cfm?section=rsscategory&alias=archaeology-and-paleontology'), 
-#             (u'Physics', u'http://www.sciam.com/page.cfm?section=rsscategory&alias=physics'), 
-#             (u'Math', u'http://rss.sciam.com/sciam/math'), 
-#             (u'History of Science', u'http://www.sciam.com/page.cfm?section=rsscategory&alias=history-of-science'), 
-#             (u'Chemistry', u'http://rss.sciam.com/sciam/chemistry'), 
-#             (u'Mind Matters', u'http://rss.sciam.com/ScientificAmerican-MindBlog')
-#            ]
-#    
+    match_regexps = [r'article.cfm.id=\S+page=(2|3|4|5|6|7|8|9|10|11|12|13|14|15)']
+
     def parse_index(self):
-        src = self.browser.open('http://www.sciam.com/sciammag/').read()
-        root = html.fromstring(src)
-        self.cover_url = root.xpath('//img[re:match(@src, "cover_")]', 
-                                    namespaces={'re':'http://exslt.org/regular-expressions'}
-                                    )[0].get('src')
-        self.timefmt = ' [%s]'%(root.xpath('//div[@id = "magazine-month"]')[0].text)
-        feeds = []
-        features = []
-        for a in root.xpath('//a[@href and @title = "Feature"]'):
-            if not a.text.strip():
-                continue
+        soup = self.index_to_soup('http://www.scientificamerican.com/sciammag/')
+        month = soup.find(id='magazine-month')
+        self.timefmt = ' [%s]'%(self.tag_to_string(month))
+        img = soup.find('img', alt='Scientific American Magazine', src=True)
+        if img is not None:
+            self.cover_url = img['src']
+        features, feeds = [], []
+        for p in soup.find(id='magazine-info').findAll('p') + \
+                soup.find(id='magazine-info-more').findAll('p'):
+            all_as = p.findAll('a', href=True)
+            a = all_as[0]
+            if a is None: continue
+            desc = ''
+            for s in p.find('span', attrs={'class':'sub'}):
+                desc += self.tag_to_string(s)
+
             article = {
-                       'url'    : a.get('href'),
-                       'title'  : u''.join(a.xpath('./text()')),
-                       'date'   : '',
-                       'description' : '',   
-                       }
-            for s in a.itersiblings('span'):
-                if s.get('class', '') == 'sub':
-                    article['description'] += u''.join(s.xpath('./text()')) + ' '
+                    'url' : a.get('href'),
+                    'title' : self.tag_to_string(all_as[-1]),
+                    'date' : '',
+                    'description' : desc,
+                    }
             features.append(article)
-        if features:
-            feeds.append(('Features', features))
-            
-        departments = []
-        for a in root.xpath('//a[@href and @class="title"]'):
-            txt = u''.join(a.xpath('./text()')).strip()
-            if not txt:
-                continue
-            article = {
-                       'url'    : a.get('href'),
-                       'title'  : txt,
-                       'date'   : '',
-                       'description' : '',   
-                       }
-            p = a.getparent()
-            p.remove(a)
-            article['description'] = u''.join(p.xpath('./text()'))
-            departments.append(article)
-            
-        feeds.append(('Departments', departments))
-        opinion = []
-        for a in root.xpath('//div[@id = "opinion"]//a[@href]'):
-            txt = u''.join(a.xpath('./text()')).strip()
-            if not txt:
-                continue
-            article = {
-                       'url'    : a.get('href'),
-                       'title'  : txt,
-                       'date'   : '',
-                       'description' : '',   
-                       }
-            opinion.append(article)
-        feeds.append(('Opinion', opinion))
-        
-        ontheweb = []
-        for a in root.xpath('//div[@id = "ontheweb"]//a[@href]'):
-            txt = u''.join(a.xpath('./text()')).strip()
-            if not txt:
-                continue
-            article = {
-                       'url'    : a.get('href'),
-                       'title'  : txt,
-                       'date'   : '',
-                       'description' : '',   
-                       }
-            ontheweb.append(article)
-        feeds.append(('On the web', ontheweb))
-        
+        feeds.append(('Features', features))
+
+        section = []
+        found = []
+        title = None
+        for x in soup.find(id='magazine-main_col1').findAll(['div', 'a']):
+            if x.name == 'div':
+                if section:
+                    feeds.append((title, section))
+                title = self.tag_to_string(x)
+                section = []
+            else:
+                if title is None or not a.get('href', False) or a.get('href', None) in found:
+                    continue
+                article = {
+                        'url' : x['href'],
+                        'title' : self.tag_to_string(x),
+                        'date': '',
+                        'description': '',
+                        }
+                section.append(article)
+        if section:
+            feeds.append((title, section))
+
+        articles = []
+        for a in soup.find(id='opinion').findAll('a', href=True):
+            articles.append({'url':a['href'], 'title':self.tag_to_string(a),
+                'description':'', 'date':''})
+        feeds.append(('Opinion', articles))
+
         return feeds
-        
-    
+
+
     def postprocess_html(self, soup, first_fetch):
         if soup is not None:
             for span in soup.findAll('span', attrs={'class':'pagination'}):
                 span.extract()
             if not first_fetch:
                 div = soup.find('div', attrs={'class':'headline'})
-                if div: 
+                if div:
                     div.extract()
         return soup
