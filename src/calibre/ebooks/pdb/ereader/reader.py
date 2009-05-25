6b9ea1d0aee5de8425e830eb445b1034f908b402
John Schember <john@nachtimwald.com>
2009-05-25 07:34:42 -0400

    Update inspector with new known values and makebook files. Better error message.

--- src/calibre/ebooks/pdb/ereader/reader.py
+++ src/calibre/ebooks/pdb/ereader/reader.py
@@ -8,6 +8,7 @@ __license__   = 'GPL v3'
 __copyright__ = '2009, John Schember <john@nachtimwald.com>'
 __docformat__ = 'restructuredtext en'
 
+from calibre.ebooks.pdb.ereader import EreaderError
 from calibre.ebooks.pdb.formatreader import FormatReader
 from calibre.ebooks.pdb.ereader.reader132 import Reader132
 from calibre.ebooks.pdb.ereader.reader202 import Reader202
@@ -22,7 +23,7 @@ class Reader(FormatReader):
         elif record0_size == 202:
             self.reader = Reader202(header, stream, log, encoding)
         else:
-            raise ValueError('Unknown eReader Header')
+            raise EreaderError('Size mismatch. eReader header record size %s KB is not supported.' % record0_size)
 
     def extract_content(self, output_dir):
         return self.reader.extract_content(output_dir)
