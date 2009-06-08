79f15cd0109955e84fc069e1ee7598d784a8ea90
Kovid Goyal <kovid@kovidgoyal.net>
2009-06-07 17:58:36 -0700

    Updated recipes for Harpers Magazine

--- src/calibre/web/feeds/recipes/recipe_harpers.py
+++ src/calibre/web/feeds/recipes/recipe_harpers.py
@@ -6,6 +6,7 @@ __copyright__ = '2008-2009, Darko Miletic <darko.miletic at gmail.com>'
 harpers.org
 '''
 from calibre.web.feeds.news import BasicNewsRecipe
+from calibre.ebooks.BeautifulSoup import Tag
 
 class Harpers(BasicNewsRecipe):
     title                 = u"Harper's Magazine"
@@ -18,23 +19,30 @@ class Harpers(BasicNewsRecipe):
     max_articles_per_feed = 100
     no_stylesheets        = True
     use_embedded_content  = False
-    remove_javascript     = True
 
     html2lrf_options = [
                           '--comment', description
                         , '--category', category
                         , '--publisher', publisher
                         ]
-    
-    html2epub_options = 'publisher="' + publisher + '"\ncomments="' + description + '"\ntags="' + category + '"\noverride_css=" p {text-indent: 0em; margin-top: 0em; margin-bottom: 0.5em} img {margin-top: 0em; margin-bottom: 0.4em}"' 
-    
-    
+
+    html2epub_options = 'publisher="' + publisher + '"\ncomments="' + description + '"\ntags="' + category + '"\noverride_css=" p {text-indent: 0em; margin-top: 0em; margin-bottom: 0.5em} img {margin-top: 0em; margin-bottom: 0.4em}"'
+
+
     keep_only_tags = [ dict(name='div', attrs={'id':'cached'}) ]
     remove_tags = [
-                     dict(name='table', attrs={'class':'rcnt'})
-                    ,dict(name='table', attrs={'class':'rcnt topline'})
+                     dict(name='table', attrs={'class':['rcnt','rcnt topline']})
                     ,dict(name=['link','object','embed'])
                   ]
 
     feeds       = [(u"Harper's Magazine", u'http://www.harpers.org/rss/frontpage-rss20.xml')]
+
+    def preprocess_html(self, soup):
+        mcharset = Tag(soup,'meta',[("http-equiv","Content-Type"),("content","text/html; charset=utf-8")])
+        soup.head.insert(1,mcharset)
+        for item in soup.findAll(style=True):
+            del item['style']
+        for item in soup.findAll(xmlns=True):
+            del item['xmlns']
+        return soup
 
