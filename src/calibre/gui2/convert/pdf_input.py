28bd7cd3f9a6059f6d99697103660d80bf31c376
John Schember <john@nachtimwald.com>
2009-06-23 18:22:10 -0400

    GUI config for PDF intput.

---
+++ src/calibre/gui2/convert/pdf_input.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+__license__ = 'GPL 3'
+__copyright__ = '2009, John Schember <john@nachtimwald.com>'
+__docformat__ = 'restructuredtext en'
+
+from calibre.gui2.convert.pdf_input_ui import Ui_Form
+from calibre.gui2.convert import Widget
+
+class PluginWidget(Widget, Ui_Form):
+
+    TITLE = _('PDF Input')
+    HELP = _('Options specific to')+' PDF '+_('input')
+
+    def __init__(self, parent, get_option, get_help, db=None, book_id=None):
+        Widget.__init__(self, parent, 'pdf_input',
+            ['no_images', 'pdf_line_length'])
+        self.db, self.book_id = db, book_id
+        self.initialize_options(get_option, get_help, db, book_id)
