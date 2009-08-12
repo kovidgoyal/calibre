49ab3466190c93025d9f6cce7f50100b7ee25bea
Kovid Goyal <kovid@kovidgoyal.net>
2009-08-11 20:20:28 -0600

    Fix detection of non-US PRS-700 readers in OS X and Windows

--- src/calibre/devices/prs700/driver.py
+++ src/calibre/devices/prs700/driver.py
@@ -9,6 +9,7 @@ Device driver for the SONY PRS-700
 '''
 
 from calibre.devices.prs505.driver import PRS505
+import re
 
 class PRS700(PRS505):
 
@@ -20,9 +21,10 @@ class PRS700(PRS505):
     BCD          = [0x31a]
 
     WINDOWS_MAIN_MEM = 'PRS-700'
-    WINDOWS_CARD_A_MEM = 'PRS-700/UC:MS'
-    WINDOWS_CARD_B_MEM = 'PRS-700/UC:SD'
+    WINDOWS_CARD_A_MEM = re.compile(r'PRS-700/\S+:MS')
+    WINDOWS_CARD_B_MEM = re.compile(r'PRS-700/\S+:SD')
 
-    OSX_MAIN_MEM = 'Sony PRS-700/UC Media'
-    OSX_CARD_A_MEM = 'Sony PRS-700/UC:MS Media'
-    OSX_CARD_B_MEM = 'Sony PRS-700/UC:SD'
+    OSX_MAIN_MEM   = re.compile(r'Sony PRS-700/[^:]+ Media')
+    OSX_CARD_A_MEM = re.compile(r'Sony PRS-700/[^:]+:MS Media')
+    OSX_CARD_B_MEM = re.compile(r'Sony PRS-700/[^:]+:SD Media')
+
