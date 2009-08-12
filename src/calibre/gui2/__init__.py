1f349f330ee4d6827bce7d8ec4841af359429ceb
Kovid Goyal <kovid@kovidgoyal.net>
2009-08-12 14:06:52 -0600

    IGN:Improve X server detection

--- src/calibre/gui2/__init__.py
+++ src/calibre/gui2/__init__.py
@@ -538,7 +538,7 @@ class Application(QApplication):
 
 def is_ok_to_use_qt():
     global gui_thread
-    if islinux and os.environ.get('DISPLAY', None) is None:
+    if islinux and ':' in os.environ.get('DISPLAY', ''):
         return False
     if QApplication.instance() is None:
         QApplication([])
