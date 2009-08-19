1a35d8895232156094b937bacdce876c99eec41d
Kovid Goyal <kovid@kovidgoyal.net>
2009-08-19 13:57:45 -0600

    New recipe for BETA by Darko Miletic

---
+++ src/calibre/web/feeds/recipes/recipe_beta_en.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env  python
+
+__license__   = 'GPL v3'
+__copyright__ = '2009, Darko Miletic <darko.miletic at gmail.com>'
+'''
+beta.rs
+'''
+
+from calibre.web.feeds.news import BasicNewsRecipe
+from calibre.ebooks.BeautifulSoup import BeautifulSoup, Tag
+
+class Danas(BasicNewsRecipe):
+    title                 = 'BETA - English'
+    __author__            = 'Darko Miletic'
+    description           = 'Serbian news agency'
+    publisher             = 'Beta'
+    category              = 'news, politics, Serbia'    
+    oldest_article        = 2
+    max_articles_per_feed = 100
+    no_stylesheets        = False
+    use_embedded_content  = True
+    language              = _('English')
+    lang                  = 'en'
+    
+    conversion_options = {
+                          'comment'          : description
+                        , 'tags'             : category
+                        , 'publisher'        : publisher
+                        , 'language'         : lang
+                        , 'pretty_print'     : True
+                        }
+ 
+
+    feeds          = [(u'News', u'http://www.beta.rs/rssen.asp')]
+
+    def preprocess_html(self, soup):
+        return self.adeify_images(soup)
