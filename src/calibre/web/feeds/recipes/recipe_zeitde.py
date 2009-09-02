63e13101beb43d1ae180e45de7cd82b117906f74
Kovid Goyal <kovid@kovidgoyal.net>
2009-09-01 18:30:42 -0600

    Fix #3345 (Please add more sections to "Die Zeit" recipe)

--- src/calibre/web/feeds/recipes/recipe_zeitde.py
+++ src/calibre/web/feeds/recipes/recipe_zeitde.py
@@ -9,20 +9,27 @@ from calibre.web.feeds.news import BasicNewsRecipe
 
 
 class ZeitDe(BasicNewsRecipe):
-    
+
     title = 'Die Zeit Nachrichten'
     description = 'Die Zeit - Online Nachrichten'
     language = _('German')
-    __author__ = 'Kovid Goyal'
+    __author__ = 'Kovid Goyal and Martin Pitt'
     use_embedded_content   = False
     timefmt = ' [%d %b %Y]'
     max_articles_per_feed = 40
     no_stylesheets = True
     encoding = 'latin1'
 
-    
-    feeds =  [ ('Zeit.de', 'http://newsfeed.zeit.de/news/index') ] 
-    
+
+    feeds =  [ ('Kurznachrichten', 'http://newsfeed.zeit.de/news/index'),
+               ('Politik', 'http://newsfeed.zeit.de/politik/index'),
+               ('Wirtschaft', 'http://newsfeed.zeit.de/wirtschaft/index'),
+               ('Meinung', 'http://newsfeed.zeit.de/meinung/index'),
+               ('Gesellschaft', 'http://newsfeed.zeit.de/gesellschaft/index'),
+               ('Kultur', 'http://newsfeed.zeit.de/kultur/index'),
+               ('Wissen', 'http://newsfeed.zeit.de/wissen/index'),
+             ]
+
     def print_version(self,url):
         return url.replace('http://www.zeit.de/', 'http://images.zeit.de/text/').replace('?from=rss', '')
 
