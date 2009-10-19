df6d759b3852e77c066ea007815177db079dfa01
John Schember <john@nachtimwald.com>
2009-10-19 07:15:27 -0400

    TCR Output. Move TCR decompression to TCR compression file.

--- src/calibre/ebooks/tcr/input.py
+++ src/calibre/ebooks/tcr/input.py
@@ -9,6 +9,7 @@ import os
 from calibre.customize.conversion import InputFormatPlugin, OptionRecommendation
 from calibre.ebooks.txt.processor import convert_basic, opf_writer, \
     separate_paragraphs_single_line, separate_paragraphs_print_formatted
+from calibre.ebooks.compression.tcr import decompress
 
 class TCRInput(InputFormatPlugin):
 
@@ -31,28 +32,9 @@ class TCRInput(InputFormatPlugin):
     ])
 
     def convert(self, stream, options, file_ext, log, accelerators):
-        txt = []
-
-        log.debug('Checking TCR header...')
-        if stream.read(9) != '!!8-Bit!!':
-            raise ValueError('File %s contaions an invalid TCR header.' % stream.name)
-
-        log.debug('Building string dictionary...')
-        # Dictionary codes that the file contents are broken down into.
-        entries = []
-        for i in xrange(256):
-            entry_len = ord(stream.read(1))
-            entries.append(stream.read(entry_len))
-
         log.info('Decompressing text...')
-        # Map the values in the file to locations in the string list.
-        entry_loc = stream.read(1)
-        while entry_loc != '': # EOF
-            txt.append(entries[ord(entry_loc)])
-            entry_loc = stream.read(1)
-
         ienc = options.input_encoding if options.input_encoding else 'utf-8'
-        txt = ''.join(txt).decode(ienc, 'replace')
+        txt = decompress(stream).decode(ienc, 'replace')
 
         log.info('Converting text to OEB...')
         if options.single_line_paras:
