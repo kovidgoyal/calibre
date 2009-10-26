d70d42f90081924b5fa1f2598fcdadb2d5a7336c
Kovid Goyal <kovid@kovidgoyal.net>
2009-10-25 18:59:05 -0600

    Add a wizard to make customizing the font rescaling algorithm easier

--- src/calibre/gui2/convert/look_and_feel.py
+++ src/calibre/gui2/convert/look_and_feel.py
@@ -6,6 +6,7 @@ __license__   = 'GPL v3'
 __copyright__ = '2009, Kovid Goyal <kovid@kovidgoyal.net>'
 __docformat__ = 'restructuredtext en'
 
+from PyQt4.Qt import SIGNAL
 
 from calibre.gui2.convert.look_and_feel_ui import Ui_Form
 from calibre.gui2.convert import Widget
@@ -29,4 +30,16 @@ class LookAndFeelWidget(Widget, Ui_Form):
         self.initialize_options(get_option, get_help, db, book_id)
         self.opt_disable_font_rescaling.toggle()
         self.opt_disable_font_rescaling.toggle()
+        self.connect(self.button_font_key, SIGNAL('clicked()'),
+                self.font_key_wizard)
+
+    def font_key_wizard(self):
+        from calibre.gui2.convert.font_key import FontKeyChooser
+        d = FontKeyChooser(self, self.opt_base_font_size.value(),
+                unicode(self.opt_font_size_mapping.text()).strip())
+        if d.exec_() == d.Accepted:
+            self.opt_font_size_mapping.setText(', '.join(['%.1f'%x for x in
+                d.fsizes]))
+            self.opt_base_font_size.setValue(d.dbase)
+
 
