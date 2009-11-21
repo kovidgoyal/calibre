87c7c91ab276005988570dca14fb79418e4df5ec
Kovid Goyal <kovid@kovidgoyal.net>
2009-11-21 06:21:46 -0700

    IGN:...

--- src/calibre/devices/eb600/driver.py
+++ src/calibre/devices/eb600/driver.py
@@ -93,7 +93,7 @@ class POCKETBOOK360(EB600):
     VENDOR_NAME = 'PHILIPS'
     WINDOWS_MAIN_MEM = 'MASS_STORGE'
 
-    OSX_MAIN_MEM = 'Philips Mass Storge Media'
+    OSX_MAIN_MEM   = 'Philips Mass Storge Media'
     OSX_CARD_A_MEM = 'Philips Mass Storge Media'
 
 
