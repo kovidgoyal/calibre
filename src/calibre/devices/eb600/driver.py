84fafe6b9397e06b04161d4ba7c6ac562d2b56da
Kovid Goyal <kovid@kovidgoyal.net>
2009-12-02 11:44:52 -0700

    Fix #4118 (PocketBook 360 will not send MOBI files)

--- src/calibre/devices/eb600/driver.py
+++ src/calibre/devices/eb600/driver.py
@@ -88,7 +88,7 @@ class POCKETBOOK360(EB600):
 
     gui_name = 'PocketBook 360'
 
-    FORMATS = ['epub', 'fb2', 'pdf', 'djvu', 'rtf', 'chm', 'txt']
+    FORMATS = ['epub', 'fb2', 'prc', 'mobi', 'pdf', 'djvu', 'rtf', 'chm', 'txt']
 
     VENDOR_NAME = 'PHILIPS'
     WINDOWS_MAIN_MEM = 'MASS_STORGE'
