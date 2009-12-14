e7eb7885ab33d30b226d266e36e6ecd052c3efa3
Kovid Goyal <kovid@kovidgoyal.net>
2009-12-14 13:53:45 -0700

    Add checkbox to control CPU limiting of worker processes

--- src/calibre/gui2/dialogs/confirm_delete.py
+++ src/calibre/gui2/dialogs/confirm_delete.py
@@ -11,20 +11,20 @@ def _config_name(name):
     return name + '_again'
 
 class Dialog(QDialog, Ui_Dialog):
-    
+
     def __init__(self, msg, name, parent):
         QDialog.__init__(self, parent)
         self.setupUi(self)
-        
+
         self.msg.setText(msg)
         self.name = name
         self.connect(self.again, SIGNAL('stateChanged(int)'), self.toggle)
         self.buttonBox.setFocus(Qt.OtherFocusReason)
-        
+
 
     def toggle(self, x):
         dynamic[_config_name(self.name)] = self.again.isChecked()
-        
+
 def confirm(msg, name, parent=None):
     if not dynamic.get(_config_name(name), True):
         return True
