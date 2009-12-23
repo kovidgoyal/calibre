e4164d71d2b899294f4bb1158557619cc7278122
Kovid Goyal <kovid@kovidgoyal.net>
2009-12-22 19:44:34 -0700

    Add option to swap author first name and last name when reading metadata from a file name

--- src/calibre/ebooks/metadata/meta.py
+++ src/calibre/ebooks/metadata/meta.py
@@ -132,6 +132,15 @@ def metadata_from_filename(name, pat=None):
             au = match.group('author')
             aus = string_to_authors(au)
             mi.authors = aus
+            if prefs['swap_author_names'] and mi.authors:
+                def swap(a):
+                    parts = a.split()
+                    if len(parts) > 1:
+                        t = parts[-1]
+                        parts = parts[:-1]
+                        parts.insert(0, t)
+                    return ' '.join(parts)
+                mi.authors = [swap(x) for x in mi.authors]
         except (IndexError, ValueError):
             pass
         try:
