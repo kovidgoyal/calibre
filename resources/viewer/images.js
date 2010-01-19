e936eb0c84a19765796dfa070b11878be6aaa8f7
Kovid Goyal <kovid@kovidgoyal.net>
2010-01-19 14:02:16 -0700

    MOBI Input: Rescale img width and height attributes that were specified in em units. Fixes #4608 (Built in viewre picture issue)

--- resources/viewer/images.js
+++ resources/viewer/images.js
@@ -7,6 +7,7 @@
 function scale_images() {
     $("img:visible").each(function() {
         var offset = $(this).offset();
+        //window.py_bridge.debug(window.getComputedStyle(this, '').getPropertyValue('max-width'));
         $(this).css("max-width", (window.innerWidth-offset.left-5)+"px");
         $(this).css("max-height", (window.innerHeight-5)+"px");
     });
