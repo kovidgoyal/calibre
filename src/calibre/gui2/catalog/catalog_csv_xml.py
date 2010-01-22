44f6c0a1f5101da9154f4b4a4482453f2d19f9cd
Kovid Goyal <kovid@kovidgoyal.net>
2010-01-21 17:25:59 -0700

    more cleanups

--- src/calibre/gui2/catalog/catalog_csv_xml.py
+++ src/calibre/gui2/catalog/catalog_csv_xml.py
@@ -9,20 +9,20 @@ __docformat__ = 'restructuredtext en'
 
 from calibre.gui2 import gprefs
 from calibre.gui2.catalog.catalog_csv_xml_ui import Ui_Form
-from PyQt4.Qt import QDialog, QWidget, SIGNAL
+from PyQt4.Qt import QWidget
 
 class PluginWidget(QWidget,Ui_Form):
 
     TITLE = _('CSV/XML Output')
     HELP  = _('Options specific to')+' CSV/XML '+_('output')
     sync_enabled = False
-    
+
     def initialize(self, name):
         QWidget.__init__(self)
         self.setupUi(self)
         self.name = name
         # Restore the activated fields from last use
-        for x in range(self.db_fields.count()):        
+        for x in range(self.db_fields.count()):
             pref = '%s_db_fields_%s' % (self.name, self.db_fields.item(x).text())
             activated = gprefs[pref] if pref in gprefs else False
             self.db_fields.item(x).setSelected(activated)
@@ -31,9 +31,9 @@ class PluginWidget(QWidget,Ui_Form):
         # Save the currently activated fields
         for x in range(self.db_fields.count()):
             pref = '%s_db_fields_%s' % (self.name, self.db_fields.item(x).text())
-            gprefs[pref] =  self.db_fields.item(x).isSelected()           
-        
-        # Return a dictionary with current options for this widget 
+            gprefs[pref] =  self.db_fields.item(x).isSelected()
+
+        # Return a dictionary with current options for this widget
         if len(self.db_fields.selectedItems()):
             return {'fields':[str(item.text()) for item in self.db_fields.selectedItems()]}
         else:
