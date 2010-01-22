adb332fd37617d4baf7c421a30994371a8ac06cc
Kovid Goyal <kovid@kovidgoyal.net>
2010-01-21 21:58:27 -0700

    ...

--- src/calibre/gui2/dialogs/catalog.py
+++ src/calibre/gui2/dialogs/catalog.py
@@ -17,7 +17,6 @@ from calibre.customize.ui import catalog_plugins
 
 class Catalog(QDialog, Ui_Dialog):
     ''' Catalog Dialog builder'''
-    widgets = []
 
     def __init__(self, parent, dbspec, ids):
         import re, cStringIO
