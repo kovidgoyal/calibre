b3dbf7024f5171904344486b917f2cca24c785eb
Kovid Goyal <kovid@kovidgoyal.net>
2010-03-07 10:57:35 -0700

    Implement #5077 (Calibre support for Samsung Moment)

--- src/calibre/devices/android/driver.py
+++ src/calibre/devices/android/driver.py
@@ -21,6 +21,7 @@ class ANDROID(USBMS):
             0x0bb4 : { 0x0c02 : [0x100], 0x0c01 : [0x100]},
             0x22b8 : { 0x41d9 : [0x216]},
             0x18d1 : { 0x4e11 : [0x0100], 0x4e12: [0x0100]},
+            0x04e8 : { 0x681d : [0x0222]},
             }
     EBOOK_DIR_MAIN = ['wordplayer/calibretransfer', 'eBooks/import', 'Books']
     EXTRA_CUSTOMIZATION_MESSAGE = _('Comma separated list of directories to '
@@ -28,8 +29,9 @@ class ANDROID(USBMS):
             'be used')
     EXTRA_CUSTOMIZATION_DEFAULT = ', '.join(EBOOK_DIR_MAIN)
 
-    VENDOR_NAME      = ['HTC', 'MOTOROLA', 'GOOGLE_']
-    WINDOWS_MAIN_MEM = ['ANDROID_PHONE', 'A855', 'INC.NEXUS_ONE']
+    VENDOR_NAME      = ['HTC', 'MOTOROLA', 'GOOGLE_', 'ANDROID']
+    WINDOWS_MAIN_MEM = ['ANDROID_PHONE', 'A855', 'INC.NEXUS_ONE',
+            '___UMS_COMPOSITE',]
 
     OSX_MAIN_MEM = 'HTC Android Phone Media'
 
