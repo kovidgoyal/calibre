4d899142fdd1f38d5eeb78d806abc33cfd56c0bf
Kovid Goyal <kovid@kovidgoyal.net>
2010-04-03 12:03:17 +0530

    Conversion pipeline: When generating filenames use case insensitive checks for pre-existing filenames

--- src/calibre/ebooks/oeb/base.py
+++ src/calibre/ebooks/oeb/base.py
@@ -1181,6 +1181,12 @@ class Manifest(object):
         :param:`href` are `None` then the corresponding item in the return
         tuple will also be `None`.
         """
+        def href_present(href):
+            for x in self.hrefs:
+                if x.lower() == href.lower():
+                    return True
+            return False
+
         if id is not None:
             base = id
             index = 1
@@ -1191,7 +1197,7 @@ class Manifest(object):
             href = urlnormalize(href)
             base, ext = os.path.splitext(href)
             index = 1
-            while href in self.hrefs:
+            while href_present(href):
                 href = base + str(index) + ext
                 index += 1
         return id, href
