6dadf8cf0ea5515c5c04c8f6a6de58c6118fec52
Kovid Goyal <kovid@kovidgoyal.net>
2010-05-19 22:37:31 -0600

    If last sort was ondevice, ersort on device re-connect

--- src/calibre/gui2/library/models.py
+++ src/calibre/gui2/library/models.py
@@ -102,6 +102,9 @@ class BooksModel(QAbstractTableModel): # {{{
     def set_device_connected(self, is_connected):
         self.device_connected = is_connected
         self.db.refresh_ondevice()
+        if is_connected and self.sorted_on[0] == 'ondevice':
+            self.resort()
+
 
     def set_book_on_device_func(self, func):
         self.book_on_device = func
