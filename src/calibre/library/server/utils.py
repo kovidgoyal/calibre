d7fa2363a878f6b9e5676d6ea7a83cb241e9bacd
Kovid Goyal <kovid@kovidgoyal.net>
2010-05-23 12:28:24 -0600

    Timing infrastructure for the content server

--- src/calibre/library/server/utils.py
+++ src/calibre/library/server/utils.py
@@ -5,7 +5,9 @@ __license__   = 'GPL v3'
 __copyright__ = '2010, Kovid Goyal <kovid@kovidgoyal.net>'
 __docformat__ = 'restructuredtext en'
 
-from calibre import strftime as _strftime
+import time
+
+from calibre import strftime as _strftime, prints
 from calibre.utils.date import now as nowf
 
 
@@ -19,6 +21,19 @@ def expose(func):
         return func(self, *args, **kwargs)
 
     return cherrypy.expose(do)
+
+def timeit(func):
+
+    def do(self, *args, **kwargs):
+        if self.opts.develop:
+            start = time.time()
+        ans = func(self, *args, **kwargs)
+        if self.opts.develop:
+            prints('Function', func.__name__, 'called with args:', args, kwargs)
+            prints('\tTime:', func.__name__, time.time()-start)
+        return ans
+
+    return do
 
 def strftime(fmt='%Y/%m/%d %H:%M:%S', dt=None):
     if not hasattr(dt, 'timetuple'):
