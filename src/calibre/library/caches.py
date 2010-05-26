3d38872b56da7cff99bad9f1cc66c9cd2896c16e
Charles Haley <>
2010-05-26 14:48:53 +0100

    Fix bareword searches trying to match against non-text types.

--- src/calibre/library/caches.py
+++ src/calibre/library/caches.py
@@ -381,12 +381,17 @@ class ResultCache(SearchQueryParser):
                 location += 's'
 
             MAP = {}
+            # Fields not used when matching against text contents. These are
+            # the non-text fields
+            EXCLUDE_FIELDS = []
 
             # get the db columns for the standard searchables
             for x in self.tag_browser_categories:
                 if (len(self.tag_browser_categories[x]['search_labels']) and \
                         self.tag_browser_categories[x]['kind'] in ['standard', 'not_cat']):
                     MAP[x] = self.FIELD_MAP[self.tag_browser_categories.get_field_label(x)]
+                    if self.tag_browser_categories[x]['datatype'] != 'text':
+                        EXCLUDE_FIELDS.append(MAP[x])
 
             # add custom columns to MAP. Put the column's type into IS_CUSTOM
             IS_CUSTOM = []
@@ -399,8 +404,6 @@ class ResultCache(SearchQueryParser):
                     MAP[x] = self.FIELD_MAP[self.tag_browser_categories[x]['colnum']]
                     IS_CUSTOM[MAP[x]] = self.tag_browser_categories[x]['datatype']
 
-            # Some fields not used when matching against contents
-            EXCLUDE_FIELDS = [MAP['rating'], MAP['cover']]
             SPLITABLE_FIELDS = [MAP['authors'], MAP['tags'], MAP['formats']]
             for x in self.tag_browser_categories.get_custom_fields():
                 if self.tag_browser_categories[x]['is_multiple']:
