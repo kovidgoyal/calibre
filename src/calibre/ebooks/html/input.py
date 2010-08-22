aca34337d1a14db1b7ef4879bdf40e2788c0ef19
ldolse <ldolse@yahoo.com>
2010-08-23 03:29:17 +1000

    enhancements to preprocessing for Lit, html, & pdf

--- src/calibre/ebooks/html/input.py
+++ src/calibre/ebooks/html/input.py
@@ -24,6 +24,7 @@ from calibre.constants import islinux, isfreebsd, iswindows
 from calibre import unicode_path
 from calibre.utils.localization import get_lang
 from calibre.utils.filenames import ascii_filename
+from calibre.ebooks.conversion.preprocess import line_length
 
 class Link(object):
     '''
@@ -489,5 +490,18 @@ class HTMLInput(InputFormatPlugin):
             return (None, None)
         return (None, raw)
 
-
-
+	def preprocess_html(self, html):
+		print "*********  Preprocessing HTML  *********\n"
+		# Detect Chapters to match the xpath in the GUI
+		chapdetect = re.compile(r'(?=</?(br|p|span))(</?(br|p|span)[^>]*>)?\s*(?P<chap>(<(i|b)><(i|b)>|<(i|b)>)?(.?Chapter|Epilogue|Prologue|Book|Part|Dedication)\s*([\d\w-]+(\s\w+)?)?(</(i|b)></(i|b)>|</(i|b)>)?)(</?(p|br|span)[^>]*>)', re.IGNORECASE)
+		html = chapdetect.sub('<h2>'+'\g<chap>'+'</h2>\n', html)
+		# Unwrap lines using punctation if the median length of all lines is less than 150
+		#
+		# Insert extra line feeds so the line length regex functions properly
+		html = re.sub(r"</p>", "</p>\n", html)
+		length = line_length('html', html, 0.4)
+		print "*** Median length is " + str(length) + " ***\n"
+		unwrap = re.compile(r"(?<=.{%i}[a-z,;:\IA])\s*</(span|p|div)>\s*(</(p|span|div)>)?\s*(?P<up2threeblanks><(p|span|div)[^>]*>\s*(<(p|span|div)[^>]*>\s*</(span|p|div)>\s*)</(span|p|div)>\s*){0,3}\s*<(span|div|p)[^>]*>\s*(<(span|div|p)[^>]*>)?\s*" % length, re.UNICODE)
+		if length < 150:
+			html = unwrap.sub(' ', html)
+        return html
--- src/calibre/ebooks/lit/input.py
+++ src/calibre/ebooks/lit/input.py
@@ -9,6 +9,7 @@ __docformat__ = 'restructuredtext en'
 import re
 
 from calibre.customize.conversion import InputFormatPlugin
+from calibre.ebooks.conversion.preprocess import line_length
 
 class LITInput(InputFormatPlugin):
 
@@ -53,8 +54,18 @@ class LITInput(InputFormatPlugin):
 
 
 	def preprocess_html(self, html):
+		print "*********  Preprocessing HTML  *********\n"
+		# Detect Chapters to match the xpath in the GUI
 		chapdetect = re.compile(r'(?=</?(br|p|span))(</?(br|p|span)[^>]*>)?\s*(?P<chap>(<(i|b)><(i|b)>|<(i|b)>)?(.?Chapter|Epilogue|Prologue|Book|Part|Dedication)\s*([\d\w-]+(\s\w+)?)?(</(i|b)></(i|b)>|</(i|b)>)?)(</?(p|br|span)[^>]*>)', re.IGNORECASE)
 		html = chapdetect.sub('<h2>'+'\g<chap>'+'</h2>\n', html)
-		html = re.sub(r"(?<=.{65}[a-z,\IA])\s*</(span|p|div)>\s*(</(p|span|div)>\s*<p[^>]*>(\s*<(p|span|div)>\s*</(p|span|div)[^>]*>)?\s*(</(p|span|div)>\s*<p[^>]*>)?)?\s*<(span|div|p)[^>]*>", " ", html)
-		return html
+		# Unwrap lines using punctation if the median length of all lines is less than 150
+		#
+		# Insert extra line feeds so the line length regex functions properly
+		html = re.sub(r"</p>", "</p>\n", html)
+		length = line_length('html', html, 0.4)
+		print "*** Median length is " + str(length) + " ***\n"
+		unwrap = re.compile(r"(?<=.{%i}[a-z,;:\IA])\s*</(span|p|div)>\s*(</(p|span|div)>)?\s*(?P<up2threeblanks><(p|span|div)[^>]*>\s*(<(p|span|div)[^>]*>\s*</(span|p|div)>\s*)</(span|p|div)>\s*){0,3}\s*<(span|div|p)[^>]*>\s*(<(span|div|p)[^>]*>)?\s*" % length, re.UNICODE)
+		if length < 150:
+			html = unwrap.sub(' ', html)
+        return html
 
