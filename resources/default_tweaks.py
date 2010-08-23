4afad8585592697b83e4795d35581571ae2b5473
Charles Haley <>
2010-08-23 11:56:16 +0100

    Implement enhancement #6577 (Date format) created by chattympc

--- resources/default_tweaks.py
+++ resources/default_tweaks.py
@@ -44,7 +44,7 @@ bool_custom_columns_are_tristate = 'yes'
 # title within authors.
 sort_columns_at_startup = None
 
-# Format to be used for publication date
+# Format to be used for publication date and the timestamp (date).
 #  A string controlling how the publication date is displayed in the GUI
 #  d    the day as number without a leading zero (1 to 31)
 #  dd    the day as number with a leading zero (01 to 31)
@@ -59,8 +59,10 @@ sort_columns_at_startup = None
 #  For example, given the date of 9 Jan 2010, the following formats show
 #  MMM yyyy ==> Jan 2010    yyyy ==> 2010       dd MMM yyyy ==> 09 Jan 2010
 #  MM/yyyy ==> 01/2010      d/M/yy ==> 9/1/10   yy ==> 10
-# default if not set: MMM yyyy
+# publication default if not set: MMM yyyy
+# timestamp default if not set: dd MMM yyyy
 gui_pubdate_display_format = 'MMM yyyy'
+gui_timestamp_display_format = 'dd MMM yyyy'
 
 # Control title and series sorting in the library view.
 # If set to 'library_order', Leading articles such as The and A will be ignored.
