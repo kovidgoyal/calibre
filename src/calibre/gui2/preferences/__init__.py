19bcbb713f98d7150b08ab799348bbdab167b46a
Kovid Goyal <kovid@kovidgoyal.net>
2010-08-24 18:13:13 -0600

    Framework for plugin based preferences dialog

--- src/calibre/gui2/preferences/__init__.py
+++ src/calibre/gui2/preferences/__init__.py
@@ -7,28 +7,67 @@ __docformat__ = 'restructuredtext en'
 
 from PyQt4.Qt import QWidget, pyqtSignal
 
-class PreferenceWidget(QWidget):
+from calibre.customize.ui import preferences_plugins
 
-    category = None
-    name     = None
+class ConfigWidgetInterface(object):
 
-    changed_signal = pyqtSignal()
-
-    def __init__(self, parent=None):
-        QWidget.__init__(self, parent)
-
-        self.has_changed = False
-        self.changed.connect(lambda : setattr(self, 'has_changed', True))
-        self.setupUi(self)
+    changed_signal = None
 
     def genesis(self, gui):
         raise NotImplementedError()
 
-    def reset_to_defaults(self):
+    def restore_defaults(self):
         pass
 
     def commit(self):
         pass
 
-    def add_boolean(self, widget_name, preference_interface, pref_name):
-        pass
+
+class ConfigWidgetBase(QWidget, ConfigWidgetInterface):
+
+    changed_signal = pyqtSignal()
+
+    def __init__(self, parent=None):
+        QWidget.__init__(self, parent)
+        if hasattr(self, 'setupUi'):
+            self.setupUi(self)
+
+def get_plugin(category, name):
+    for plugin in preferences_plugins():
+        if plugin.category == category and plugin.name == name:
+            return plugin
+    raise ValueError(
+            'No Preferences PLugin with category: %s and name: %s found' %
+            (category, name))
+
+def test_widget(category, name, gui=None): # {{{
+    from PyQt4.Qt import QDialog, QVBoxLayout, QDialogButtonBox
+    pl = get_plugin(category, name)
+    d = QDialog()
+    d.resize(750, 550)
+    bb = QDialogButtonBox(d)
+    bb.setStandardButtons(bb.Apply|bb.Cancel|bb.RestoreDefaults)
+    bb.accepted.connect(d.accept)
+    bb.rejected.connect(d.reject)
+    w = pl.create_widget(d)
+    bb.button(bb.RestoreDefaults).clicked.connect(w.restore_defaults)
+    bb.button(bb.Apply).setEnabled(False)
+    w.changed_signal.connect(lambda : bb.button(bb.Apply).setEnable(True))
+    l = QVBoxLayout()
+    pl.setLayout(l)
+    l.addWidget(w)
+    if gui is None:
+        from calibre.gui2.ui import Main
+        from calibre.gui2.main import option_parser
+        from calibre.library.db import db
+        parser = option_parser()
+        opts, args = parser.parse_args([])
+        actions = tuple(Main.create_application_menubar())
+        db = db()
+        gui = Main(opts)
+        gui.initialize(db.library_path, db, None, actions)
+    w.genesis(gui)
+    if d.exec_() == QDialog.Accepted:
+        w.commit()
+# }}}
+
