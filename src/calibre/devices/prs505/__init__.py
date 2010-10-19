5fb294486babed1e2471940a4ebee97cc199601f
Kovid Goyal <kovid@kovidgoyal.net>
2010-10-19 16:40:39 -0600

    SONY driver: Add support for uploading covers and periodicals

--- src/calibre/devices/prs505/__init__.py
+++ src/calibre/devices/prs505/__init__.py
@@ -2,5 +2,11 @@ __license__   = 'GPL v3'
 __copyright__ = '2008, Kovid Goyal <kovid at kovidgoyal.net>'
 
 MEDIA_XML = 'database/cache/media.xml'
+MEDIA_EXT = 'database/cache/cacheExt.xml'
 
 CACHE_XML = 'Sony Reader/database/cache.xml'
+CACHE_EXT = 'Sony Reader/database/cacheExt.xml'
+
+MEDIA_THUMBNAIL = 'database/thumbnail'
+CACHE_THUMBNAIL = 'Sony Reader/database/thumbnail'
+
