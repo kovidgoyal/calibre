8fc411ade65577eef65d21fd52d38251c63a7e86
Kovid Goyal <kovid@kovidgoyal.net>
2010-11-26 09:46:07 -0700

    Implement #7400 (Mass importing by ISBN)

--- src/calibre/gui2/dialogs/add_from_isbn.py
+++ src/calibre/gui2/dialogs/add_from_isbn.py
@@ -5,10 +5,14 @@ __license__   = 'GPL v3'
 __copyright__ = '2010, Kovid Goyal <kovid@kovidgoyal.net>'
 __docformat__ = 'restructuredtext en'
 
+import os
+
 from PyQt4.Qt import QDialog, QApplication
 
 from calibre.gui2.dialogs.add_from_isbn_ui import Ui_Dialog
 from calibre.ebooks.metadata import check_isbn
+from calibre.constants import iswindows
+from calibre.ptempfile import PersistentTemporaryFile
 
 class AddFromISBN(QDialog, Ui_Dialog):
 
@@ -16,7 +20,12 @@ class AddFromISBN(QDialog, Ui_Dialog):
         QDialog.__init__(self, parent)
         self.setupUi(self)
 
+        path = r'C:\Users\kovid\e-books\some_book.epub' if iswindows else \
+                '/Users/kovid/e-books/some_book.epub'
+        self.label.setText(unicode(self.label.text())%path)
+
         self.isbns = []
+        self.books = []
         self.paste_button.clicked.connect(self.paste)
 
     def paste(self, *args):
@@ -30,11 +39,27 @@ class AddFromISBN(QDialog, Ui_Dialog):
 
     def accept(self, *args):
         for line in unicode(self.isbn_box.toPlainText()).strip().splitlines():
-            if line:
-                isbn = check_isbn(line)
-                if isbn is not None:
-                    isbn = isbn.upper()
-                    if isbn not in self.isbns:
-                        self.isbns.append(isbn)
+            line = line.strip()
+            if not line:
+                continue
+            parts = line.split('>>')
+            if len(parts) > 2:
+                parts = [parts[0] + '>>'.join(parts[1:])]
+            parts = [x.strip() for x in parts]
+            if not parts[0]:
+                continue
+            isbn = check_isbn(parts[0])
+            if isbn is not None:
+                isbn = isbn.upper()
+                if isbn not in self.isbns:
+                    self.isbns.append(isbn)
+                    book = {'isbn': isbn, 'path': None}
+                    if len(parts) > 1 and parts[1] and \
+                        os.access(parts[1], os.R_OK) and os.path.isfile(parts[1]):
+                        pt = PersistentTemporaryFile(os.path.splitext(parts[1])[1])
+                        pt.write(open(parts[1], 'rb').read())
+                        pt.close()
+                        book['path'] = pt.name
+                    self.books.append(book)
         QDialog.accept(self, *args)
 
