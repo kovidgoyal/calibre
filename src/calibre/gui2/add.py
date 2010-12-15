54336c28414c693dde23b7eb329b6633b898fd0e
Kovid Goyal <kovid@kovidgoyal.net>
2010-12-15 13:56:50 -0700

    ...

--- src/calibre/gui2/add.py
+++ src/calibre/gui2/add.py
@@ -369,8 +369,13 @@ class Adder(QObject): # {{{
                 except:
                     pass
         self._parent = None
+        self.pd.setParent(None)
+        del self.pd
+        self.pd = None
         if hasattr(self, 'db_adder'):
             self.db_adder.setParent(None)
+            del self.db_adder
+            self.db_adder = None
 
 
     @property
