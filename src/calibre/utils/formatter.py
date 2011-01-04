c483d01b45f25bd1326052c2e352b51cca6613f1
Kovid Goyal <kovid@kovidgoyal.net>
2011-01-03 17:27:37 -0700

    Fix bug where composite custom columns using general_program_mode fields are not evaluated correctly when used in a template.

--- src/calibre/utils/formatter.py
+++ src/calibre/utils/formatter.py
@@ -431,7 +431,10 @@ class TemplateFormatter(string.Formatter):
         return prefix + val + suffix
 
     def vformat(self, fmt, args, kwargs):
-        ans = string.Formatter.vformat(self, fmt, args, kwargs)
+        if fmt.startswith('program:'):
+            ans = self._eval_program(None, fmt[8:])
+        else:
+            ans = string.Formatter.vformat(self, fmt, args, kwargs)
         return self.compress_spaces.sub(' ', ans).strip()
 
     ########## a formatter guaranteed not to throw and exception ############
@@ -441,10 +444,7 @@ class TemplateFormatter(string.Formatter):
         self.book = book
         self.composite_values = {}
         try:
-            if fmt.startswith('program:'):
-                ans = self._eval_program(None, fmt[8:])
-            else:
-                ans = self.vformat(fmt, [], kwargs).strip()
+            ans = self.vformat(fmt, [], kwargs).strip()
         except Exception, e:
             if DEBUG:
                 traceback.print_exc()
