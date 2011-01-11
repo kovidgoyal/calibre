2ea905dffd76f31f860962982a76ca997d219745
Kovid Goyal <kovid@kovidgoyal.net>
2011-01-11 10:38:10 -0700

    Fix bug that caused automatic news removal to remove any book that has a tag that contains the word 'news' instead of only books that have the tag News

--- src/calibre/library/database2.py
+++ src/calibre/library/database2.py
@@ -1373,7 +1373,7 @@ class LibraryDatabase2(LibraryDatabase, SchemaUpgrade, CustomColumns):
             if r is not None:
                 if (now - r[self.FIELD_MAP['timestamp']]) > delta:
                     tags = r[self.FIELD_MAP['tags']]
-                    if tags and tag in tags.lower():
+                    if tags and tag in tags.lower().split(','):
                         yield r[self.FIELD_MAP['id']]
 
     def get_next_series_num_for(self, series):
