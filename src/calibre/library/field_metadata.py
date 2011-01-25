ede45bbc7865b903eb491a4246f4fdc0acd79e5c
Charles Haley <>
2011-01-25 22:40:38 +0000

    Another 'right' fix for the multiple user category problem

--- src/calibre/library/field_metadata.py
+++ src/calibre/library/field_metadata.py
@@ -485,7 +485,6 @@ class FieldMetadata(dict):
         return self._tb_cats[key]['rec_index'] + 1
 
     def add_user_category(self, label, name):
-        label = lower(label)
         if label in self._tb_cats:
             raise ValueError('Duplicate user field [%s]'%(label))
         self._tb_cats[label] = {'table':None,          'column':None,
@@ -525,7 +524,6 @@ class FieldMetadata(dict):
     def _add_search_terms_to_map(self, key, terms):
         if terms is not None:
             for t in terms:
-                t = t.lower()
                 if t in self._search_term_map:
                     raise ValueError('Attempt to add duplicate search term "%s"'%t)
                 self._search_term_map[t] = key
