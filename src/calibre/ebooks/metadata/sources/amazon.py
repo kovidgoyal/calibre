1edc4f5a800473d80196139b502ce63bb7d70c1a
Kovid Goyal <kovid@kovidgoyal.net>
2011-03-16 10:49:10 -0600

    More work on the new Amazon metadata download plugin

--- src/calibre/ebooks/metadata/sources/amazon.py
+++ src/calibre/ebooks/metadata/sources/amazon.py
@@ -7,7 +7,7 @@ __license__   = 'GPL v3'
 __copyright__ = '2011, Kovid Goyal <kovid@kovidgoyal.net>'
 __docformat__ = 'restructuredtext en'
 
-import socket, time
+import socket, time, re
 from urllib import urlencode
 from threading import Thread
 
@@ -18,8 +18,14 @@ from calibre.ebooks.metadata import check_isbn
 from calibre.ebooks.metadata.sources.base import Source
 from calibre.utils.cleantext import clean_ascii_chars
 from calibre.ebooks.chardet import xml_to_unicode
+from calibre.ebooks.metadata.book.base import Metadata
+from calibre.library.comments import sanitize_comments_html
 
 class Worker(Thread):
+
+    '''
+    Get book details from amazons book page in a separate thread
+    '''
 
     def __init__(self, url, result_queue, browser, log, timeout=20):
         self.url, self.result_queue = url, result_queue
@@ -75,7 +81,117 @@ class Worker(Thread):
         self.parse_details(root)
 
     def parse_details(self, root):
-        pass
+        try:
+            asin = self.parse_asin(root)
+        except:
+            self.log.exception('Error parsing asin for url: %r'%self.url)
+            asin = None
+
+        try:
+            title = self.parse_title(root)
+        except:
+            self.log.exception('Error parsing title for url: %r'%self.url)
+            title = None
+
+        try:
+            authors = self.parse_authors(root)
+        except:
+            self.log.exception('Error parsing authors for url: %r'%self.url)
+            authors = []
+
+
+        if not title or not authors or not asin:
+            self.log.error('Could not find title/authors/asin for %r'%self.url)
+            self.log.error('ASIN: %r Title: %r Authors: %r'%(asin, title,
+                authors))
+            return
+
+        mi = Metadata(title, authors)
+        mi.set_identifier('amazon', asin)
+        self.amazon_id = asin
+
+        try:
+            mi.rating = self.parse_ratings(root)
+        except:
+            self.log.exception('Error parsing ratings for url: %r'%self.url)
+
+        try:
+            mi.comments = self.parse_comments(root)
+        except:
+            self.log.exception('Error parsing comments for url: %r'%self.url)
+
+        try:
+            self.cover_url = self.parse_cover(root)
+        except:
+            self.log.exception('Error parsing cover for url: %r'%self.url)
+
+        self.result_queue.put(mi)
+
+    def parse_asin(self, root):
+        link = root.xpath('//link[@rel="canonical" and @href]')
+        for l in link:
+            return l.get('href').rpartition('/')[-1]
+
+    def parse_title(self, root):
+        tdiv = root.xpath('//h1[@class="parseasinTitle"]')[0]
+        actual_title = tdiv.xpath('descendant::*[@id="btAsinTitle"]')
+        if actual_title:
+            title = tostring(actual_title[0], encoding=unicode,
+                    method='text').strip()
+        else:
+            title = tostring(tdiv, encoding=unicode, method='text').strip()
+        return re.sub(r'[([].*[)]]', '', title).strip()
+
+    def parse_authors(self, root):
+        bdiv = root.xpath('//div[@class="buying"]')[0]
+        aname = bdiv.xpath('descendant::span[@class="contributorNameTrigger"]')
+        authors = [tostring(x, encoding=unicode, method='text').strip() for x
+                in aname]
+        return authors
+
+    def parse_ratings(self, root):
+        ratings = root.xpath('//form[@id="handleBuy"]/descendant::*[@class="asinReviewsSummary"]')
+        pat = re.compile(r'([0-9.]+) out of (\d+) stars')
+        if ratings:
+            for elem in ratings[0].xpath('descendant::*[@title]'):
+                t = elem.get('title')
+                m = pat.match(t)
+                if m is not None:
+                    try:
+                        return float(m.group(1))/float(m.group(2)) * 5
+                    except:
+                        pass
+
+    def parse_comments(self, root):
+        desc = root.xpath('//div[@id="productDescription"]/*[@class="content"]')
+        if desc:
+            desc = desc[0]
+            for c in desc.xpath('descendant::*[@class="seeAll" or'
+                    ' @class="emptyClear" or @href]'):
+                c.getparent().remove(c)
+            desc = tostring(desc, method='html', encoding=unicode).strip()
+            # remove all attributes from tags
+            desc = re.sub(r'<([a-zA-Z0-9]+)\s[^>]+>', r'<\1>', desc)
+            # Collapse whitespace
+            #desc = re.sub('\n+', '\n', desc)
+            #desc = re.sub(' +', ' ', desc)
+            # Remove the notice about text referring to out of print editions
+            desc = re.sub(r'(?s)<em>--This text ref.*?</em>', '', desc)
+            # Remove comments
+            desc = re.sub(r'(?s)<!--.*?-->', '', desc)
+            return sanitize_comments_html(desc)
+
+    def parse_cover(self, root):
+        imgs = root.xpath('//img[@id="prodImage" and @src]')
+        if imgs:
+            src = imgs[0].get('src')
+            parts = src.split('/')
+            if len(parts) > 3:
+                bn = parts[-1]
+                sparts = bn.split('_')
+                if len(sparts) > 2:
+                    bn = sparts[0] + sparts[-1]
+                    return ('/'.join(parts[:-1]))+'/'+bn
 
 
 class Amazon(Source):
