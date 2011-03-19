0fbc8d717b186abbe87d47e09a3bf699ef7c6bc8
Kovid Goyal <kovid@kovidgoyal.net>
2011-03-19 17:52:35 -0600

    ...

--- setup/installer/__init__.py
+++ setup/installer/__init__.py
@@ -138,7 +138,7 @@ class VMInstaller(Command):
             self.vm = self.VM
         if not self.vmware_started():
             self.start_vmware()
-        subprocess.call(['chmod', '-R', '+r', 'resources/recipes'])
+        subprocess.call(['chmod', '-R', '+r', 'recipes'])
         self.start_vm()
         self.download_installer()
         if not self.dont_shutdown:
