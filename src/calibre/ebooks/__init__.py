18d0f6a6ef79bc80b23a3a58a1c7c607169fe662
John Schember <john@nachtimwald.com>
2011-04-12 18:58:54 -0400

    Add HTMLZ as a book extension. Use HTML icon for HTMLZ.

--- src/calibre/ebooks/__init__.py
+++ src/calibre/ebooks/__init__.py
@@ -26,7 +26,7 @@ class ParserError(ValueError):
     pass
 
 BOOK_EXTENSIONS = ['lrf', 'rar', 'zip', 'rtf', 'lit', 'txt', 'txtz', 'text', 'htm', 'xhtm',
-                   'html', 'xhtml', 'pdf', 'pdb', 'pdr', 'prc', 'mobi', 'azw', 'doc',
+                   'html', 'htmlz', 'xhtml', 'pdf', 'pdb', 'pdr', 'prc', 'mobi', 'azw', 'doc',
                    'epub', 'fb2', 'djvu', 'lrx', 'cbr', 'cbz', 'cbc', 'oebzip',
                    'rb', 'imp', 'odt', 'chm', 'tpz', 'azw1', 'pml', 'pmlz', 'mbp', 'tan', 'snb']
 
