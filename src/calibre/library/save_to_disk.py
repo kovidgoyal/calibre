c68f6162f9dc661882a1ea128985ccc1c74b04dc
Charles Haley <>
2011-05-19 13:16:58 +0100

    Fixes for plugboards: make matching hierarchical and improve error messages.

--- src/calibre/library/save_to_disk.py
+++ src/calibre/library/save_to_disk.py
@@ -56,17 +56,18 @@ for x in FORMAT_ARG_DESCS:
 def find_plugboard(device_name, format, plugboards):
     cpb = None
     if format in plugboards:
-        cpb = plugboards[format]
-    elif plugboard_any_format_value in plugboards:
-        cpb = plugboards[plugboard_any_format_value]
-    if cpb is not None:
-        if device_name in cpb:
-            cpb = cpb[device_name]
-        elif plugboard_any_device_value in cpb:
-            cpb = cpb[plugboard_any_device_value]
-        else:
-            cpb = None
-    if DEBUG:
+        pb = plugboards[format]
+        if device_name in pb:
+            cpb = pb[device_name]
+        elif plugboard_any_device_value in pb:
+            cpb = pb[plugboard_any_device_value]
+    if not cpb and plugboard_any_format_value in plugboards:
+        pb = plugboards[plugboard_any_format_value]
+        if device_name in pb:
+            cpb = pb[device_name]
+        elif plugboard_any_device_value in pb:
+            cpb = pb[plugboard_any_device_value]
+    if True or DEBUG:
         prints('Device using plugboard', format, device_name, cpb)
     return cpb
 
