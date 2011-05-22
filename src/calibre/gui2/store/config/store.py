ab55a25674d64426fa06877ec6b9c6a1bb87b8a9
John Schember <john@nachtimwald.com>
2011-05-21 21:17:27 -0400

    Store: add missed files. Add more quick functions for listing stores and which ones are enabled.

---
+++ src/calibre/gui2/store/config/store.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import (unicode_literals, division, absolute_import, print_function)
+
+__license__ = 'GPL 3'
+__copyright__ = '2011, John Schember <john@nachtimwald.com>'
+__docformat__ = 'restructuredtext en'
+
+def config_widget():
+    from calibre.gui2.store.config.search_widget import StoreConfigWidget
+    return StoreConfigWidget()
+
+def save_settings(config_widget):
+    config_widget.save_settings()
