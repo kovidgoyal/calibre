52edcbe991aa8bb763b2815d39f678fbd6f069b5
Charles Haley <>
2011-06-22 10:48:31 +0100

    Small fixes to Quickview

--- src/calibre/gui2/actions/show_quickview.py
+++ src/calibre/gui2/actions/show_quickview.py
@@ -13,8 +13,7 @@ from calibre.gui2 import error_dialog
 class ShowQuickviewAction(InterfaceAction):
 
     name = 'Show quickview'
-    action_spec = (_('Show quickview'), 'user_profile.png', None,
-            _('Q'))
+    action_spec = (_('Show quickview'), 'search.png', None, _('Q'))
     dont_add_to = frozenset(['menubar-device', 'toolbar-device', 'context-menu-device'])
     action_type = 'current'
 
