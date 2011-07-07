dd62e49a87fa1fe307b88edb928ac408ee93377e
Charles Haley <>
2011-07-07 19:18:15 +0100

    Fix for device_db

--- src/calibre/devices/prs505/driver.py
+++ src/calibre/devices/prs505/driver.py
@@ -28,6 +28,7 @@ class PRS505(USBMS):
 
     FORMATS      = ['epub', 'lrf', 'lrx', 'rtf', 'pdf', 'txt']
     CAN_SET_METADATA = ['title', 'authors', 'collections']
+    CAN_DO_DEVICE_DB_PLUGBOARD = True
 
     VENDOR_ID    = [0x054c]   #: SONY Vendor Id
     PRODUCT_ID   = [0x031e]
