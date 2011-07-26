586aa592459ea7c3279166393218ceb24463a649
Charles Haley <>
2011-07-26 13:53:45 +0100

    Fix 815573: Series number Tweak will not accept constant value

--- src/calibre/library/database2.py
+++ src/calibre/library/database2.py
@@ -3023,8 +3023,8 @@ class LibraryDatabase2(LibraryDatabase, SchemaUpgrade, CustomColumns):
         stream.seek(0)
         mi = get_metadata(stream, format, use_libprs_metadata=False)
         stream.seek(0)
-        if not mi.series_index:
-            mi.series_index = 1.0
+        if mi.series_index is None:
+            mi.series_index = self.get_next_series_num_for(mi.series)
         mi.tags = [_('News')]
         if arg['add_title_tag']:
             mi.tags += [arg['title']]
@@ -3076,7 +3076,8 @@ class LibraryDatabase2(LibraryDatabase, SchemaUpgrade, CustomColumns):
         self._add_newbook_tag(mi)
         if not add_duplicates and self.has_book(mi):
             return None
-        series_index = 1.0 if mi.series_index is None else mi.series_index
+        series_index = self.get_next_series_num_for(mi.series) \
+                    if mi.series_index is None else mi.series_index
         aus = mi.author_sort if mi.author_sort else self.author_sort_from_authors(mi.authors)
         title = mi.title
         if isbytestring(aus):
@@ -3123,7 +3124,8 @@ class LibraryDatabase2(LibraryDatabase, SchemaUpgrade, CustomColumns):
             if not add_duplicates and self.has_book(mi):
                 duplicates.append((path, format, mi))
                 continue
-            series_index = 1.0 if mi.series_index is None else mi.series_index
+            series_index = self.get_next_series_num_for(mi.series) \
+                            if mi.series_index is None else mi.series_index
             aus = mi.author_sort if mi.author_sort else self.author_sort_from_authors(mi.authors)
             title = mi.title
             if isinstance(aus, str):
@@ -3157,7 +3159,8 @@ class LibraryDatabase2(LibraryDatabase, SchemaUpgrade, CustomColumns):
 
     def import_book(self, mi, formats, notify=True, import_hooks=True,
             apply_import_tags=True, preserve_uuid=False):
-        series_index = 1.0 if mi.series_index is None else mi.series_index
+        series_index = self.get_next_series_num_for(mi.series) \
+                        if mi.series_index is None else mi.series_index
         if apply_import_tags:
             self._add_newbook_tag(mi)
         if not mi.title:
