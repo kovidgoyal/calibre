4374e16bc8f6b90e7fc3bc9c8e2478a354e6a1b4
Kovid Goyal <kovid@kovidgoyal.net>
2012-04-23 23:48:14 +0530

    tighten format_field_extended

--- src/calibre/ebooks/metadata/book/base.py
+++ src/calibre/ebooks/metadata/book/base.py
@@ -650,11 +650,7 @@ class Metadata(object):
             res = self.get(key, None)
             cmeta = self.get_user_metadata(key, make_copy=False)
             name = unicode(cmeta['name'])
-            if cmeta['datatype'] != 'composite' and (res is None or res == ''):
-                return (name, res, None, None)
-            orig_res = res
-            cmeta = self.get_user_metadata(key, make_copy=False)
-            if res is None or res == '':
+            if res in {None, ''}:
                 return (name, res, None, None)
             orig_res = res
             datatype = cmeta['datatype']
