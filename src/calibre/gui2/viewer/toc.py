6d0b2ec5536e02124bad8cbc0ca47116c011cae9
Kovid Goyal <kovid@kovidgoyal.net>
2012-05-06 23:10:25 +0530

    Cleanup ebook viewer code

---
+++ src/calibre/gui2/viewer/toc.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__   = 'GPL v3'
+__copyright__ = '2012, Kovid Goyal <kovid@kovidgoyal.net>'
+__docformat__ = 'restructuredtext en'
+
+import re
+
+from PyQt4.Qt import QStandardItem, QStandardItemModel, Qt
+
+class TOCItem(QStandardItem):
+
+    def __init__(self, toc):
+        text = toc.text
+        if text:
+            text = re.sub(r'\s', ' ', text)
+        QStandardItem.__init__(self, text if text else '')
+        self.abspath = toc.abspath
+        self.fragment = toc.fragment
+        for t in toc:
+            self.appendRow(TOCItem(t))
+        self.setFlags(Qt.ItemIsEnabled|Qt.ItemIsSelectable)
+
+    @classmethod
+    def type(cls):
+        return QStandardItem.UserType+10
+
+class TOC(QStandardItemModel):
+
+    def __init__(self, toc):
+        QStandardItemModel.__init__(self)
+        for t in toc:
+            self.appendRow(TOCItem(t))
+        self.setHorizontalHeaderItem(0, QStandardItem(_('Table of Contents')))
+
+
