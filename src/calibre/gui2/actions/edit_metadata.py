b4d6e46057b1f24a20d453df0f714512e932ec12
Kovid Goyal <kovid@kovidgoyal.net>
2012-05-28 21:16:53 +0530

    When bulk downloading metadata and the user deletes one of the books for which metadata is being downloaded, just ignore it, instead of erroring out

--- src/calibre/gui2/actions/edit_metadata.py
+++ src/calibre/gui2/actions/edit_metadata.py
@@ -132,7 +132,7 @@ class EditMetadataAction(InterfaceAction):
 
         for i in good_ids:
             lm = db.metadata_last_modified(i, index_is_id=True)
-            if lm > lm_map[i]:
+            if lm is not None and lm_map[i] is not None and lm > lm_map[i]:
                 title = db.title(i, index_is_id=True)
                 authors = db.authors(i, index_is_id=True)
                 if authors:
@@ -516,18 +516,19 @@ class EditMetadataAction(InterfaceAction):
             return self.finalize_apply()
 
         i, mi = self.apply_id_map[self.apply_current_idx]
-        if isinstance(mi, tuple):
-            opf, cover = mi
-            if opf:
-                mi = OPF(open(opf, 'rb'), basedir=os.path.dirname(opf),
-                        populate_spine=False).to_book_metadata()
+        if self.gui.current_db.has_id(i):
+            if isinstance(mi, tuple):
+                opf, cover = mi
+                if opf:
+                    mi = OPF(open(opf, 'rb'), basedir=os.path.dirname(opf),
+                            populate_spine=False).to_book_metadata()
+                    self.apply_mi(i, mi)
+                if cover:
+                    self.gui.current_db.set_cover(i, open(cover, 'rb'),
+                            notify=False, commit=False)
+                    self.applied_ids.add(i)
+            else:
                 self.apply_mi(i, mi)
-            if cover:
-                self.gui.current_db.set_cover(i, open(cover, 'rb'),
-                        notify=False, commit=False)
-                self.applied_ids.add(i)
-        else:
-            self.apply_mi(i, mi)
 
         self.apply_current_idx += 1
         if self.apply_pd is not None:
