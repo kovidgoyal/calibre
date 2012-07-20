c8bf6a66f1eaa561f1718548eaa188e6ba49a47f
John Schember <john@nachtimwald.com>
2012-07-19 21:31:21 -0400

    Fix for Bug #1026541: non-visibile element's tail text (which should be visible) is being ignored when it shouldn't.

--- src/calibre/ebooks/rb/rbml.py
+++ src/calibre/ebooks/rb/rbml.py
@@ -142,8 +142,11 @@ class RBMLizer(object):
     def dump_text(self, elem, stylizer, page, tag_stack=[]):
         from calibre.ebooks.oeb.base import XHTML_NS, barename, namespace
 
-        if not isinstance(elem.tag, basestring) \
-           or namespace(elem.tag) != XHTML_NS:
+        if not isinstance(elem_tree.tag, basestring) or namespace(elem_tree.tag) != XHTML_NS:
+            p = elem.getparent()
+            if p is not None and isinstance(p.tag, basestring) and namespace(p.tag) == XHTML_NS \
+                    and elem.tail:
+                return [elem.tail]
             return [u'']
 
         text = [u'']
@@ -151,6 +154,8 @@ class RBMLizer(object):
 
         if style['display'] in ('none', 'oeb-page-head', 'oeb-page-foot') \
            or style['visibility'] == 'hidden':
+            if hasattr(elem, 'tail') and elem.tail:
+                return [elem.tail]
             return [u'']
 
         tag = barename(elem.tag)
