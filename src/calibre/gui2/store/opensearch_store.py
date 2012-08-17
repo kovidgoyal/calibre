29f58de5c0d8ba4aa85eb686d00d0e35ece77b12
Kovid Goyal <kovid@kovidgoyal.net>
2012-08-17 09:47:27 +0530

    ...

--- src/calibre/gui2/store/opensearch_store.py
+++ src/calibre/gui2/store/opensearch_store.py
@@ -6,7 +6,6 @@ __license__ = 'GPL 3'
 __copyright__ = '2011, John Schember <john@nachtimwald.com>'
 __docformat__ = 'restructuredtext en'
 
-import mimetypes
 from contextlib import closing
 
 from lxml import etree
