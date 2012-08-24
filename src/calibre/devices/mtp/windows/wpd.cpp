b16ca1e038bde08afe36b2e94ceb8ce18921dd74
Kovid Goyal <kovid@kovidgoyal.net>
2012-08-24 10:46:56 +0530

    ...

--- src/calibre/devices/mtp/windows/wpd.cpp
+++ src/calibre/devices/mtp/windows/wpd.cpp
@@ -195,15 +195,15 @@ initwpd(void) {
 
     WPDError = PyErr_NewException("wpd.WPDError", NULL, NULL);
     if (WPDError == NULL) return;
-    PyModule_AddObject(m, "WPDError", MTPError);
+    PyModule_AddObject(m, "WPDError", WPDError);
 
     NoWPD = PyErr_NewException("wpd.NoWPD", NULL, NULL);
     if (NoWPD == NULL) return;
-    PyModule_AddObject(m, "NoWPD", MTPError);
+    PyModule_AddObject(m, "NoWPD", NoWPD);
 
     WPDFileBusy = PyErr_NewException("wpd.WPDFileBusy", NULL, NULL);
     if (WPDFileBusy == NULL) return;
-    PyModule_AddObject(m, "WPDFileBusy", MTPError);
+    PyModule_AddObject(m, "WPDFileBusy", WPDFileBusy);
 
     Py_INCREF(&DeviceType);
     PyModule_AddObject(m, "Device", (PyObject *)&DeviceType);
