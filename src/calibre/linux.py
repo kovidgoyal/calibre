6e93e8fada9735a838f0b16c81536a91563f54f2
NiLuJe <ninuje@gmail.com>
2012-09-14 18:04:44 +0200

    Add an icon for AZW3, AZW2 & TOPAZ file formats. Use the same mimetypes as Amazon (on the Kindle Touch) for a bunch of Amazon formats Tweak the xdg mimetypes xml file to actually be taken into account (apparently really needs to be a .xml)

--- src/calibre/linux.py
+++ src/calibre/linux.py
@@ -363,6 +363,18 @@ class PostInstall:
                 cc('xdg-icon-resource install --noupdate --context mimetypes --size 128 calibre-lrf.png text-lrs', shell=True)
                 self.icon_resources.append(('mimetypes', 'application-lrs',
                 '128'))
+                render_img('mimetypes/mobi.png', 'calibre-mobi.png')
+                cc('xdg-icon-resource install --noupdate --context mimetypes --size 128 calibre-mobi.png application-x-mobipocket-ebook', shell=True)
+                self.icon_resources.append(('mimetypes', 'application-x-mobipocket-ebook', '128'))
+                render_img('mimetypes/tpz.png', 'calibre-tpz.png')
+                cc('xdg-icon-resource install --noupdate --context mimetypes --size 128 calibre-tpz.png application-x-topaz-ebook', shell=True)
+                self.icon_resources.append(('mimetypes', 'application-x-topaz-ebook', '128'))
+                render_img('mimetypes/azw2.png', 'calibre-azw2.png')
+                cc('xdg-icon-resource install --noupdate --context mimetypes --size 128 calibre-azw2.png application-x-kindle-application', shell=True)
+                self.icon_resources.append(('mimetypes', 'application-x-kindle-application', '128'))
+                render_img('mimetypes/azw3.png', 'calibre-azw3.png')
+                cc('xdg-icon-resource install --noupdate --context mimetypes --size 128 calibre-azw3.png application-x-mobi8-ebook', shell=True)
+                self.icon_resources.append(('mimetypes', 'application-x-mobi8-ebook', '128'))
                 render_img('lt.png', 'calibre-gui.png', width=256, height=256)
                 cc('xdg-icon-resource install --noupdate --size 256 calibre-gui.png calibre-gui', shell=True)
                 self.icon_resources.append(('apps', 'calibre-gui', '128'))
@@ -397,11 +409,11 @@ class PostInstall:
                     cc(' '.join(cmd), shell=True)
                     self.menu_resources.append(x)
                 cc(['xdg-desktop-menu', 'forceupdate'])
-                f = open('calibre-mimetypes', 'wb')
+                f = open('calibre-mimetypes.xml', 'wb')
                 f.write(MIME)
                 f.close()
-                self.mime_resources.append('calibre-mimetypes')
-                cc('xdg-mime install ./calibre-mimetypes', shell=True)
+                self.mime_resources.append('calibre-mimetypes.xml')
+                cc('xdg-mime install ./calibre-mimetypes.xml', shell=True)
         except Exception:
             if self.opts.fatal_errors:
                 raise
@@ -562,6 +574,33 @@ MIME = '''\
     <mime-type type="text/lrs">
         <comment>SONY E-book source format</comment>
         <glob pattern="*.lrs"/>
+    </mime-type>
+    <mime-type type="application/x-mobipocket-ebook">
+        <comment>Amazon Mobipocket e-book format</comment>
+        <sub-class-of type="application/x-palm-database"/>
+        <glob pattern="*.azw"/>
+    </mime-type>
+    <mime-type type="application/x-topaz-ebook">
+        <comment>Amazon Topaz ebook format</comment>
+        <glob pattern="*.tpz"/>
+        <glob pattern="*.azw1"/>
+    </mime-type>
+    <mime-type type="application/x-kindle-application">
+        <comment>Amazon Kindle Application (Kindlet)</comment>
+        <sub-class-of type="application/x-java-archive"/>
+        <glob pattern="*.azw2"/>
+    </mime-type>
+    <mime-type type="application/x-mobipocket-subscription">
+        <comment>Amazon Mobipocket ebook newspaper format</comment>
+        <sub-class-of type="application/x-mobipocket-ebook"/>
+        <!-- Technically, this depends on the cdeType (NWPR or MAGZ), but since EXTH headers have a variable length, it's tricky to probe via magic... -->
+        <alias type="application/x-mobipocket-subscription-magazine"/>
+        <glob pattern="*.pobi"/>
+    </mime-type>
+    <mime-type type="application/x-mobi8-ebook">
+        <comment>Amazon KF8 ebook format</comment>
+        <sub-class-of type="application/x-palm-database"/>
+        <glob pattern="*.azw3"/>
     </mime-type>
 </mime-info>
 '''
