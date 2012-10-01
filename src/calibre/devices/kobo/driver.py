ee7c2ca0ecffd6fa6747000a32db74a5d04f8e9d
Kovid Goyal <kovid@kovidgoyal.net>
2012-10-01 19:10:21 +0530

    Fix #1059585 (Kobo Glo not recognised as device)

--- src/calibre/devices/kobo/driver.py
+++ src/calibre/devices/kobo/driver.py
@@ -39,7 +39,7 @@ class KOBO(USBMS):
     CAN_SET_METADATA = ['collections']
 
     VENDOR_ID   = [0x2237]
-    PRODUCT_ID  = [0x4161, 0x4163, 0x4165]
+    PRODUCT_ID  = [0x4161, 0x4163, 0x4165, 0x4173]
     BCD         = [0x0110, 0x0323, 0x0326]
 
     VENDOR_NAME = ['KOBO_INC', 'KOBO']
