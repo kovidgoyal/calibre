82472c1a9e859bdf45d946e176fb0884b25c4032
Kovid Goyal <kovid@kovidgoyal.net>
2012-10-06 13:13:40 +0530

    turn off use of worldcat for earliest edition dates by default

--- src/calibre/ebooks/metadata/sources/base.py
+++ src/calibre/ebooks/metadata/sources/base.py
@@ -26,7 +26,7 @@ msprefs.defaults['wait_after_first_identify_result'] = 30 # seconds
 msprefs.defaults['wait_after_first_cover_result'] = 60 # seconds
 msprefs.defaults['swap_author_names'] = False
 msprefs.defaults['fewer_tags'] = True
-msprefs.defaults['find_first_edition_date'] = True
+msprefs.defaults['find_first_edition_date'] = False
 
 # Google covers are often poor quality (scans/errors) but they have high
 # resolution, so they trump covers from better sources. So make sure they
