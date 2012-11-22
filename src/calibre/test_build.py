b2b640e24ab3c5eeafb793940ac9c2632d7bb5ab
Kovid Goyal <kovid@kovidgoyal.net>
2012-11-22 16:39:44 +0530

    More 64-bit windows build progress

--- src/calibre/test_build.py
+++ src/calibre/test_build.py
@@ -74,8 +74,9 @@ def test_imaging():
         print ('ImageMagick OK!')
     else:
         raise RuntimeError('ImageMagick choked!')
-    from PIL import Image, _imaging, _imagingmath, _imagingft, _imagingcms
-    _imaging, _imagingmath, _imagingft, _imagingcms
+    from PIL import Image
+    import _imaging, _imagingmath, _imagingft
+    _imaging, _imagingmath, _imagingft
     i = Image.open(cStringIO.StringIO(data))
     if i.size < (20, 20):
         raise RuntimeError('PIL choked!')
