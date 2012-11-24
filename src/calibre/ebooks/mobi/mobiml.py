0d5366ecc16bb6b1155d90f95cbe5dc7f44bca78
Kovid Goyal <kovid@kovidgoyal.net>
2012-11-24 09:44:20 +0530

    ...

--- src/calibre/ebooks/mobi/mobiml.py
+++ src/calibre/ebooks/mobi/mobiml.py
@@ -214,7 +214,11 @@ class MobiMLizer(object):
         if tag in CONTENT_TAGS:
             bstate.inline = para
             pstate = bstate.istate = None
-            etree.SubElement(para, XHTML(tag), attrib=istate.attrib)
+            try:
+                etree.SubElement(para, XHTML(tag), attrib=istate.attrib)
+            except:
+                print 'Invalid subelement:', para, tag, istate.attrib
+                raise
         elif tag in TABLE_TAGS:
             para.attrib['valign'] = 'top'
         if istate.ids:
