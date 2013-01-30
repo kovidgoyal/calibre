716d55e869273304bff4ca73a0364e23d4523516
Kovid Goyal <kovid@kovidgoyal.net>
2013-01-30 14:49:48 +0530

    ...

--- src/calibre/gui2/metadata/pdf_covers.py
+++ src/calibre/gui2/metadata/pdf_covers.py
@@ -11,6 +11,7 @@ import sys, shutil, os
 from threading import Thread
 from glob import glob
 
+import sip
 from PyQt4.Qt import (QDialog, QApplication, QLabel, QGridLayout,
                       QDialogButtonBox, Qt, pyqtSignal, QListWidget,
                       QListWidgetItem, QSize, QIcon)
@@ -76,7 +77,7 @@ class PDFCovers(QDialog):
             page_images(self.pdfpath, self.tdir, last=10)
         except Exception as e:
             self.error = as_unicode(e)
-        if self.isVisible():
+        if not sip.isdeleted(self) and self.isVisible():
             self.rendering_done.emit()
 
     def show_pages(self):
