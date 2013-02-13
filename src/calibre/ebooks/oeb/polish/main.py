0cce42a313e267305c90a57dac9ff31a75579ab6
Kovid Goyal <kovid@kovidgoyal.net>
2013-02-13 16:37:31 +0530

    ...

--- src/calibre/ebooks/oeb/polish/main.py
+++ src/calibre/ebooks/oeb/polish/main.py
@@ -114,6 +114,7 @@ def polish(file_map, opts, log, report):
             report('')
 
         ebook.commit(outbook)
+        report('-'*70)
     report('Polishing took: %.1f seconds'%(time.time()-st))
 
 REPORT = '{0} REPORT {0}'.format('-'*30)
