a244dca3b2f5cfbb95c42d5dd66316c4af5b2df5
Kovid Goyal <kovid@kovidgoyal.net>
2013-02-13 19:16:02 +0530

    Driver for the Trekstor Pyrus Mini. Fixes #1124120 (Trekstor Pyrus mini not detected)

--- src/calibre/devices/misc.py
+++ src/calibre/devices/misc.py
@@ -225,6 +225,7 @@ class TREKSTOR(USBMS):
 
     VENDOR_ID   = [0x1e68]
     PRODUCT_ID  = [0x0041, 0x0042, 0x0052, 0x004e, 0x0056,
+            0x0067, # This is for the Pyrus Mini
             0x003e, # This is for the EBOOK_PLAYER_5M https://bugs.launchpad.net/bugs/792091
             0x5cL, # This is for the 4ink http://www.mobileread.com/forums/showthread.php?t=191318
             ]
@@ -234,7 +235,7 @@ class TREKSTOR(USBMS):
 
     VENDOR_NAME = 'TREKSTOR'
     WINDOWS_MAIN_MEM = WINDOWS_CARD_A_MEM = ['EBOOK_PLAYER_7',
-            'EBOOK_PLAYER_5M', 'EBOOK-READER_3.0', 'EREADER_PYRUS']
+            'EBOOK_PLAYER_5M', 'EBOOK-READER_3.0', 'EREADER_PYRUS', 'PYRUS_MINI']
     SUPPORTS_SUB_DIRS = True
     SUPPORTS_SUB_DIRS_DEFAULT = False
 
