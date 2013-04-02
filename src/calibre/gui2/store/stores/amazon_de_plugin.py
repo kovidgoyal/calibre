7b864ff15b0bf9730ba06e2bc7ba215f615b2514
Charles Haley <>
2013-04-02 10:27:41 +0200

    Remove subclass that seems not to work in some cases when the plugin is dynamically loaded

--- src/calibre/gui2/store/stores/amazon_de_plugin.py
+++ src/calibre/gui2/store/stores/amazon_de_plugin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import (unicode_literals, division, absolute_import, print_function)
-store_version = 2 # Needed for dynamic plugin loading
+store_version = 3 # Needed for dynamic plugin loading
 
 __license__ = 'GPL 3'
 __copyright__ = '2011, John Schember <john@nachtimwald.com>'
@@ -18,13 +18,26 @@ from calibre import browser
 from calibre.gui2 import open_url
 from calibre.gui2.store.search_result import SearchResult
 
+class AmazonDEKindleStore(StorePlugin):
+    '''
+    For comments on the implementation, please see amazon_plugin.py
+    '''
 
+    aff_id = {'tag': 'charhale0a-21'}
+    store_link = ('http://www.amazon.de/gp/redirect.html?ie=UTF8&site-redirect=de'
+                 '&tag=%(tag)s&linkCode=ur2&camp=1638&creative=19454'
+                 '&location=http://www.amazon.de/ebooks-kindle/b?node=530886031')
+    store_link_details = ('http://www.amazon.de/gp/redirect.html?ie=UTF8'
+                          '&location=http://www.amazon.de/dp/%(asin)s&site-redirect=de'
+                          '&tag=%(tag)s&linkCode=ur2&camp=1638&creative=6742')
+    search_url = 'http://www.amazon.de/s/?url=search-alias%3Ddigital-text&field-keywords='
 
-# This class is copy/pasted from amason_uk_plugin. Do not modify it in any
-# other amazon EU plugin. Be sure to paste it into all other amazon EU plugins
-# when modified.
+    author_article = 'von '
 
-class AmazonEUBase(StorePlugin):
+    and_word = ' und '
+
+    # ---- Copy from here to end
+
     '''
     For comments on the implementation, please see amazon_plugin.py
     '''
@@ -108,20 +121,3 @@ class AmazonEUBase(StorePlugin):
     def get_details(self, search_result, timeout):
         pass
 
-class AmazonDEKindleStore(AmazonEUBase):
-    '''
-    For comments on the implementation, please see amazon_plugin.py
-    '''
-
-    aff_id = {'tag': 'charhale0a-21'}
-    store_link = ('http://www.amazon.de/gp/redirect.html?ie=UTF8&site-redirect=de'
-                 '&tag=%(tag)s&linkCode=ur2&camp=1638&creative=19454'
-                 '&location=http://www.amazon.de/ebooks-kindle/b?node=530886031')
-    store_link_details = ('http://www.amazon.de/gp/redirect.html?ie=UTF8'
-                          '&location=http://www.amazon.de/dp/%(asin)s&site-redirect=de'
-                          '&tag=%(tag)s&linkCode=ur2&camp=1638&creative=6742')
-    search_url = 'http://www.amazon.de/s/?url=search-alias%3Ddigital-text&field-keywords='
-
-    author_article = 'von '
-
-    and_word = ' und '
