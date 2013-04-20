01b125350dc881d348d9380fa699c9d31ef27754
Kovid Goyal <kovid@kovidgoyal.net>
2013-04-20 15:11:30 +0530

    Allow importing of PArseException from pyparsing for backwards compatibility with plugins

---
+++ src/calibre/utils/pyparsing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__ = 'GPL v3'
+__copyright__ = '2013, Kovid Goyal <kovid at kovidgoyal.net>'
+
+# Dummy file for backwards compatibility with older plugins
+from calibre.utils.search_query_parser import ParseException  # noqa
+
