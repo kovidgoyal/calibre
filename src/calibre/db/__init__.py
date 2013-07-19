f0ef5f0387049a96108298c3804f41bcb1a54192
Kovid Goyal <kovid@kovidgoyal.net>
2013-07-19 21:51:48 +0530

    ...

--- src/calibre/db/__init__.py
+++ src/calibre/db/__init__.py
@@ -139,4 +139,5 @@ Various things that require other things before they can be migrated:
     3. Check that content server reloading on metadata,db change, metadata
     backup, refresh gui on calibredb add and moving libraries all work (check
     them on windows as well for file locking issues)
+    4. Check for mem leaks when switching libraries
 '''
