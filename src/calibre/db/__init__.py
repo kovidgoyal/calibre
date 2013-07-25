6975543207a0b8eae74fa74428815e34061f6fe0
Kovid Goyal <kovid@kovidgoyal.net>
2013-07-25 12:39:03 +0530

    newdb: calibredb add calibre file.ebook tested

--- src/calibre/db/__init__.py
+++ src/calibre/db/__init__.py
@@ -137,7 +137,6 @@ def get_db_loader():
 
 '''
 Various things that require other things before they can be migrated:
-    1. Check that metadata
-    backup, refresh gui on calibredb add and moving libraries all work (check
+    1. Check that metadata backup, and moving libraries all work (check
     them on windows as well for file locking issues)
 '''
