0fd842dd9a3cc37d1622c857e03a19d8d8587b66
Kovid Goyal <kovid@kovidgoyal.net>
2013-09-03 11:06:53 +0530

    Expand ~ in content-server --with-library

--- src/calibre/library/server/main.py
+++ src/calibre/library/server/main.py
@@ -116,7 +116,7 @@ def main(args=sys.argv):
         print('No saved library path. Use the --with-library option'
                 ' to specify the path to the library you want to use.')
         return 1
-    db = LibraryDatabase(opts.with_library)
+    db = LibraryDatabase(os.path.expanduser(opts.with_library))
     server = LibraryServer(db, opts, show_tracebacks=opts.develop)
     server.start()
     return 0
