e58244f87d232ac0860c24573be00f7afeb2f016
Kovid Goyal <kovid@kovidgoyal.net>
2013-10-06 13:03:06 +0530

    Refactor for test run re-use

--- src/calibre/db/tests/main.py
+++ src/calibre/db/tests/main.py
@@ -61,11 +61,7 @@ class TestResult(unittest.TextTestResult):
 def find_tests():
     return unittest.defaultTestLoader.discover(os.path.dirname(os.path.abspath(__file__)), pattern='*.py')
 
-if __name__ == '__main__':
-    from calibre.utils.config_base import reset_tweaks_to_default
-    from calibre.ebooks.metadata.book.base import reset_field_metadata
-    reset_tweaks_to_default()
-    reset_field_metadata()
+def run_tests(find_tests=find_tests):
     parser = argparse.ArgumentParser()
     parser.add_argument('name', nargs='?', default=None,
                         help='The name of the test to run, for e.g. writing.WritingTest.many_many_basic or .many_many_basic for a shortcut')
@@ -94,4 +90,11 @@ if __name__ == '__main__':
     r = unittest.TextTestRunner
     r.resultclass = TestResult
     r(verbosity=4).run(tests)
+
+if __name__ == '__main__':
+    from calibre.utils.config_base import reset_tweaks_to_default
+    from calibre.ebooks.metadata.book.base import reset_field_metadata
+    reset_tweaks_to_default()
+    reset_field_metadata()
+    run_tests()
 
