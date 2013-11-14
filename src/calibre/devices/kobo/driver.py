a4c35af27d7d6a8b7d11c59613380917cfeafed7
David Forrester <davidfor@internode.on.net>
2013-11-14 13:12:53 +1100

    Kobo firmware updated to 3.0.0
    Kobo have updated firmware to 3.0.0.  No changes needed for the driver
    except the version number. Fixes #1251099 [Update to support Kobo firmware 3.0.0](https://bugs.launchpad.net/calibre/+bug/1251099)

--- src/calibre/devices/kobo/driver.py
+++ src/calibre/devices/kobo/driver.py
@@ -1242,7 +1242,7 @@ class KOBOTOUCH(KOBO):
     description = 'Communicate with the Kobo Touch, Glo, Mini and Aura HD ereaders. Based on the existing Kobo driver by %s.' % (KOBO.author)
 #    icon        = I('devices/kobotouch.jpg')
 
-    supported_dbversion             = 90
+    supported_dbversion             = 95
     min_supported_dbversion         = 53
     min_dbversion_series            = 65
     min_dbversion_externalid        = 65
@@ -1251,7 +1251,7 @@ class KOBOTOUCH(KOBO):
     min_dbversion_activiy           = 77
     min_dbversion_keywords          = 82
 
-    max_supported_fwversion         = (2,10,1)
+    max_supported_fwversion         = (3, 0, 0)
     min_fwversion_images_on_sdcard  = (2,4,1)
     min_fwversion_images_tree       = (2,9,0)  # Cover images stored in tree under .kobo-images
 
