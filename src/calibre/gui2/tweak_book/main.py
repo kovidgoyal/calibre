3ab2f07a7da41016e6331f572a8885cfdd006a59
Kovid Goyal <kovid@kovidgoyal.net>
2013-11-25 12:49:10 +0530

    Fix plugins that use the polish infrastructure causing circular imports when start tweak book

--- src/calibre/gui2/tweak_book/main.py
+++ src/calibre/gui2/tweak_book/main.py
@@ -6,7 +6,7 @@ from __future__ import (unicode_literals, division, absolute_import,
 __license__ = 'GPL v3'
 __copyright__ = '2013, Kovid Goyal <kovid at kovidgoyal.net>'
 
-import sys, os
+import sys, os, importlib
 
 from PyQt4.Qt import QIcon
 
@@ -14,7 +14,6 @@ from calibre.constants import islinux
 from calibre.gui2 import Application, ORG_NAME, APP_UID, setup_gui_option_parser, detach_gui
 from calibre.ptempfile import reset_base_dir
 from calibre.utils.config import OptionParser
-from calibre.gui2.tweak_book.ui import Main
 
 def option_parser():
     parser =  OptionParser('''\
@@ -30,6 +29,12 @@ def main(args=sys.argv):
     # Ensure we can continue to function if GUI is closed
     os.environ.pop('CALIBRE_WORKER_TEMP_DIR', None)
     reset_base_dir()
+
+    # The following two lines are needed to prevent circular imports causing
+    # errors during initialization of plugins that use the polish container
+    # infrastructure.
+    importlib.import_module('calibre.customize.ui')
+    from calibre.gui2.tweak_book.ui import Main
 
     parser = option_parser()
     opts, args = parser.parse_args(args)
