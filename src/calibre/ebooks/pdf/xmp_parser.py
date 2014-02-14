bc8f79671619d8ec989b744d06e694a648fe71ca
sengian <sengian1@gmail.com>
2014-02-14 18:42:43 +0100

    Remove unneeded class in xmp_parser

--- src/calibre/ebooks/pdf/xmp_parser.py
+++ src/calibre/ebooks/pdf/xmp_parser.py
@@ -22,56 +22,40 @@ NS_MAP = {
     'http://www.w3.org/XML/1998/namespace'           : 'xml'
 }
 
-class XmpParser(object):
-    """
-    Parses an XMP string into a dictionary.
+def _parse_tag(el):
+    """ Extract the namespace and tag from an element. """
+    ns = None
+    tag = el.tag
+    if tag[0] == "{":
+        ns, tag = tag[1:].split('}',1)
+        if ns in NS_MAP:
+            ns = NS_MAP[ns]
+    return ns, tag
 
-    Usage:
-
-        parser = XmpParser(xmpstring)
-        meta = parser.meta
-    """
-
-    meta = None
-
-    def __init__(self, xmp):
-        self.tree = ET.XML(xmp)
-        self.rdftree = self.tree.find(RDF_NS+'RDF')
-        self.meta = self._meta()
-
-    def _meta(self):
-        """ Return a dictionary of all the parsed metadata. """
-        meta = defaultdict(dict)
-        for desc in self.rdftree.findall(RDF_NS+'Description'):
-            for el in desc.getchildren():
-                ns, tag =  self._parse_tag(el)
-                value = self._parse_value(el)
-                meta[ns][tag] = value
-        return dict(meta)
-
-    def _parse_tag(self, el):
-        """ Extract the namespace and tag from an element. """
-        ns = None
-        tag = el.tag
-        if tag[0] == "{":
-            ns, tag = tag[1:].split('}',1)
-            if ns in NS_MAP:
-                ns = NS_MAP[ns]
-        return ns, tag
-
-    def _parse_value(self, el):
-        """ Extract the metadata value from an element. """
-        if el.find(RDF_NS+'Bag') is not None:
-            value = [li.text for li in el.findall(RDF_NS+'Bag/'+RDF_NS+'li')]
-        elif el.find(RDF_NS+'Seq') is not None:
-            value = [li.text for li in el.findall(RDF_NS+'Seq/'+RDF_NS+'li')]
-        elif el.find(RDF_NS+'Alt') is not None:
-            value = {li.get(XML_NS+'lang'):li.text \
-                        for li in el.findall(RDF_NS+'Alt/'+RDF_NS+'li')}
-        else:
-            value = el.text
-        return value
+def _parse_value(el):
+    """ Extract the metadata value from an element. """
+    if el.find(RDF_NS+'Bag') is not None:
+        value = [li.text for li in el.findall(RDF_NS+'Bag/'+RDF_NS+'li')]
+    elif el.find(RDF_NS+'Seq') is not None:
+        value = [li.text for li in el.findall(RDF_NS+'Seq/'+RDF_NS+'li')]
+    elif el.find(RDF_NS+'Alt') is not None:
+        value = {li.get(XML_NS+'lang'):li.text \
+                    for li in el.findall(RDF_NS+'Alt/'+RDF_NS+'li')}
+    else:
+        value = el.text
+    return value
 
 def xmp_to_dict(xmp):
-    """ Shorthand function for parsing an XMP string into a python dictionary. """
-    return XmpParser(xmp).meta
+    """ Parsing an XMP string into a python dictionary. """
+
+    tree = ET.XML(xmp)
+    rdftree = tree.find(RDF_NS+'RDF')
+    meta = defaultdict(dict)
+
+    for desc in rdftree.findall(RDF_NS+'Description'):
+        for el in desc.getchildren():
+            ns, tag =  _parse_tag(el)
+            value = _parse_value(el)
+            meta[ns][tag] = value
+
+    return dict(meta)
