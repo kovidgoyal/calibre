7d27ee58034dfdbbd4fc2fb8755bf4e1448bc19f
John Schember <john@nachtimwald.com>
2014-02-23 20:42:18 -0500

    Update store versions.

--- src/calibre/gui2/store/stores/smashwords_plugin.py
+++ src/calibre/gui2/store/stores/smashwords_plugin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import (unicode_literals, division, absolute_import, print_function)
-store_version = 1 # Needed for dynamic plugin loading
+store_version = 2 # Needed for dynamic plugin loading
 
 __license__ = 'GPL 3'
 __copyright__ = '2011, John Schember <john@nachtimwald.com>'
