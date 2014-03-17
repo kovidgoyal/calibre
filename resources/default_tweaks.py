ca1fdf47b8b5de49d442c4e392625260af7317d0
Kovid Goyal <kovid@kovidgoyal.net>
2014-03-17 19:36:50 +0530

    ...

--- resources/default_tweaks.py
+++ resources/default_tweaks.py
@@ -147,7 +147,7 @@ sort_columns_at_startup = None
 #  d     the day as number without a leading zero (1 to 31)
 #  dd    the day as number with a leading zero (01 to 31)
 #  ddd   the abbreviated localized day name (e.g. 'Mon' to 'Sun').
-#  dddd  the long localized day name (e.g. 'Monday' to 'Qt::Sunday').
+#  dddd  the long localized day name (e.g. 'Monday' to 'Sunday').
 #  M     the month as number without a leading zero (1-12)
 #  MM    the month as number with a leading zero (01-12)
 #  MMM   the abbreviated localized month name (e.g. 'Jan' to 'Dec').
