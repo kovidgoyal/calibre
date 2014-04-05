6c77678f57a64fc4e5af053d6742c87cecf50c12
Kovid Goyal <kovid@kovidgoyal.net>
2014-04-05 09:24:42 +0530

    Send books to the Books folder for the Tolino Vision

--- src/calibre/devices/eb600/driver.py
+++ src/calibre/devices/eb600/driver.py
@@ -96,6 +96,16 @@ class TOLINO(EB600):
             drives['carda'] = main
         return drives
 
+    def post_open_callback(self):
+        # The Tolino Vision only handles books inside the Books folder
+        product_id = self.device_being_opened[1]
+        self.ebook_dir_for_upload = 'Books' if product_id == 0x6033 else ''
+
+    def get_main_ebook_dir(self, for_upload=False):
+        if for_upload:
+            return getattr(self, 'ebook_dir_for_upload', self.EBOOK_DIR_MAIN)
+        return self.EBOOK_DIR_MAIN
+
 class COOL_ER(EB600):
 
     name = 'Cool-er device interface'
