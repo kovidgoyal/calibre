c329654ebea8aac476200b83582ea8a58bb0a6e4
Kovid Goyal <kovid@kovidgoyal.net>
2014-04-25 15:04:14 +0530

    Add a hook to rebuild ui modules after a rebase

---
+++ setup/git_post_rewrite_hook.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+from __future__ import (unicode_literals, division, absolute_import,
+                        print_function)
+
+__license__ = 'GPL v3'
+__copyright__ = '2014, Kovid Goyal <kovid at kovidgoyal.net>'
+
+import subprocess, os, sys
+
+base = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+os.chdir(base)
+
+action = [x.decode('utf-8') if isinstance(x, bytes) else x for x in sys.argv[1:]][0]
+
+if action == 'rebase':
+    subprocess.check_call(['python', 'setup.py', 'gui'])
+
+
