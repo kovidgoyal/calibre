5736f177351e3f77c115fe7c19d45827264c35ba
Kovid Goyal <kovid@kovidgoyal.net>
2014-04-28 16:37:23 +0530

    pep8

--- src/calibre/gui2/tweak_book/search.py
+++ src/calibre/gui2/tweak_book/search.py
@@ -771,7 +771,7 @@ class SavedSearches(Dialog):
             def err():
                 error_dialog(self, _('Invalid data'), _(
                     'The file %s does not contain valid saved searches') % path, show=True)
-            if not isinstance(obj, dict) or not 'version' in obj or not 'searches' in obj or obj['version'] not in (1,):
+            if not isinstance(obj, dict) or 'version' not in obj or 'searches' not in obj or obj['version'] not in (1,):
                 return err()
             searches = []
             for item in obj['searches']:
