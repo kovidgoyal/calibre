b381966b79e98a36566b289ce211bdad02614dd9
Kovid Goyal <kovid@kovidgoyal.net>
2014-04-30 14:55:11 +0530

    Easier debugging of syntax highlighters

--- src/calibre/gui2/tweak_book/editor/__init__.py
+++ src/calibre/gui2/tweak_book/editor/__init__.py
@@ -6,7 +6,7 @@ from __future__ import (unicode_literals, division, absolute_import,
 __license__ = 'GPL v3'
 __copyright__ = '2013, Kovid Goyal <kovid at kovidgoyal.net>'
 
-from PyQt4.Qt import QTextCharFormat
+from PyQt4.Qt import QTextCharFormat, QFont
 
 from calibre.ebooks.oeb.base import OEB_DOCS, OEB_STYLES
 from calibre.ebooks.oeb.polish.container import guess_type
@@ -40,4 +40,10 @@ class SyntaxTextCharFormat(QTextCharFormat):
     def __init__(self, *args):
         QTextCharFormat.__init__(self, *args)
         self.setProperty(SYNTAX_PROPERTY, True)
+
+    def __repr__(self):
+        return 'SyntaxFormat(id=%s, color=%s, italic=%s, bold=%s)' % (
+            id(self), self.foreground().color().name(), self.fontItalic(), self.fontWeight() >= QFont.DemiBold)
+    __str__ = __repr__
+
 
