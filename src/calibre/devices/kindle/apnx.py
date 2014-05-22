ba61122d1ff3c6220207430f5f15a92679794c6d
John Schember <john@nachtimwald.com>
2014-05-22 14:04:53 -0400

    fix fiding pagebreak tags.

--- src/calibre/devices/kindle/apnx.py
+++ src/calibre/devices/kindle/apnx.py
@@ -280,7 +280,7 @@ class APNXBuilder(object):
         mr.extract_text()
 
         html = mr.mobi_html.lower()
-        for m in re.finditer('<\s*(mbp:)?pagebreak[^>]*>'):
+        for m in re.finditer('<\s*(mbp:)?pagebreak[^>]*>', html):
             pages.append(m.end())
 
         return pages
