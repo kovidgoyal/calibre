e72325ab99c2f3d4c82b781245f88c530d07e35f
Kovid Goyal <kovid@kovidgoyal.net>
2014-08-07 09:46:52 +0530

    ...

--- src/calibre/gui2/viewer/bookmarkmanager.py
+++ src/calibre/gui2/viewer/bookmarkmanager.py
@@ -98,12 +98,12 @@ class BookmarkManager(QWidget):
         b.clicked.connect(self.delete_bookmark)
         l.addWidget(b, l.rowCount() - 1, 1)
 
-        self.button_delete = b = QPushButton(_('Sort &name'), self)
+        self.button_delete = b = QPushButton(_('Sort by &name'), self)
         b.setToolTip(_('Sort bookmarks by name'))
         b.clicked.connect(self.sort_by_name)
         l.addWidget(b)
 
-        self.button_delete = b = QPushButton(_('Sort &position'), self)
+        self.button_delete = b = QPushButton(_('Sort by &position'), self)
         b.setToolTip(_('Sort bookmarks by position in book'))
         b.clicked.connect(self.sort_by_pos)
         l.addWidget(b, l.rowCount() - 1, 1)
