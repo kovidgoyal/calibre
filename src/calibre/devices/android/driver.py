1e85a69401a54ff2f06b99c86b231d02199464b8
Kovid Goyal <kovid@kovidgoyal.net>
2014-09-03 09:13:27 +0530

    Additional product ID for the Onyx T68

--- src/calibre/devices/android/driver.py
+++ src/calibre/devices/android/driver.py
@@ -113,6 +113,7 @@ class ANDROID(USBMS):
                 0x0ff9 : [0x0226],
                 0xc91  : HTC_BCDS,
                 0xdddd : [0x216],
+                0x0d01 : [0x9999],
                 0x0d02 : [0x9999],
                 0xdeed : [0x231, 0x226],
             },
