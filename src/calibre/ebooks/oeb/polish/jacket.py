124629dd7e25a56a2e2496a3260f8434061d7799
Kovid Goyal <kovid@kovidgoyal.net>
2014-11-03 08:43:58 +0530

    Fix #1388646 [Little typo](https://bugs.launchpad.net/calibre/+bug/1388646)

--- src/calibre/ebooks/oeb/polish/jacket.py
+++ src/calibre/ebooks/oeb/polish/jacket.py
@@ -59,7 +59,7 @@ def replace_jacket(container, name):
     container.dirty(name)
 
 def remove_jacket(container):
-    ' Remove an existing jacket, if ant. Returns False if no existing jacket was found. '
+    ' Remove an existing jacket, if any. Returns False if no existing jacket was found. '
     name = find_existing_jacket(container)
     if name is not None:
         remove_jacket_images(container, name)
