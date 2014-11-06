e59b97d1bc034ba99a9afaa1c6d094f8c37d451b
Kovid Goyal <kovid@kovidgoyal.net>
2014-11-06 09:57:32 +0530

    ...

--- src/calibre/web/feeds/news.py
+++ src/calibre/web/feeds/news.py
@@ -326,12 +326,15 @@ class BasicNewsRecipe(Recipe):
     #: Ignore duplicates of articles that are present in more than one section.
     #: A duplicate article is an article that has the same title and/or URL.
     #: To ignore articles with the same title, set this to::
+    #:
     #:   ignore_duplicate_articles = {'title'}
     #:
     #: To use URLs instead, set it to::
+    #:
     #:   ignore_duplicate_articles = {'url'}
     #:
     #: To match on title or URL, set it to::
+    #:
     #:   ignore_duplicate_articles = {'title', 'url'}
     ignore_duplicate_articles = None
 
