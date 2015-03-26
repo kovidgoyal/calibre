3c75dae2d5c28c820b082339db951276843f24a9
Kovid Goyal <kovid@kovidgoyal.net>
2015-03-26 18:34:23 +0530

    GUI config widget for DOCX output

---
+++ src/calibre/gui2/convert/docx_output.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+__license__ = 'GPL 3'
+__copyright__ = '2009, John Schember <john@nachtimwald.com>'
+__docformat__ = 'restructuredtext en'
+
+from calibre.gui2.convert.docx_output_ui import Ui_Form
+from calibre.gui2.convert import Widget
+
+paper_size_model = None
+orientation_model = None
+
+class PluginWidget(Widget, Ui_Form):
+
+    TITLE = _('DOCX Output')
+    HELP = _('Options specific to')+' DOCX '+_('output')
+    COMMIT_NAME = 'docx_output'
+    ICON = I('mimetypes/docx.png')
+
+    def __init__(self, parent, get_option, get_help, db=None, book_id=None):
+        Widget.__init__(self, parent, [
+            'docx_page_size', 'docx_custom_page_size',
+        ])
+        for x in get_option('docx_page_size').option.choices:
+            self.opt_docx_page_size.addItem(x)
+
+        self.initialize_options(get_option, get_help, db, book_id)
+        self.layout().setFieldGrowthPolicy(self.layout().ExpandingFieldsGrow)
