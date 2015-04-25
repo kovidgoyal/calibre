b0e51635f5d1ba29f43cbf93a6a7b4dc18764ef4
Kovid Goyal <kovid@kovidgoyal.net>
2015-04-25 18:49:11 +0530

    Fix #1448433 [New option for footnotes in docx input](https://bugs.launchpad.net/calibre/+bug/1448433)

--- src/calibre/ebooks/conversion/plugins/docx_input.py
+++ src/calibre/ebooks/conversion/plugins/docx_input.py
@@ -19,9 +19,8 @@ class DOCXInput(InputFormatPlugin):
             help=_('Normally, if a large image is present at the start of the document that looks like a cover, '
                    'it will be removed from the document and used as the cover for created ebook. This option '
                    'turns off that behavior.')),
-        OptionRecommendation(name='docx_notes_nopb', recommended_value=False,
-            help=_('Normally, a page break is added after every footnote, '
-                   'This option turns off that behavior.')),
+        OptionRecommendation(name='docx_no_pagebreaks_between_notes', recommended_value=False,
+            help=_('Do not insert a page break after every endnote.')),
 
     }
 
@@ -29,5 +28,5 @@ class DOCXInput(InputFormatPlugin):
 
     def convert(self, stream, options, file_ext, log, accelerators):
         from calibre.ebooks.docx.to_html import Convert
-        return Convert(stream, detect_cover=not options.docx_no_cover, log=log, notes_nopb=options.docx_notes_nopb)()
+        return Convert(stream, detect_cover=not options.docx_no_cover, log=log, notes_nopb=options.docx_no_pagebreaks_between_notes)()
 
