c90d3738c0342fe3a58b7729381dde10a66a5669
Kovid Goyal <kovid@kovidgoyal.net>
2015-05-13 09:15:22 +0530

    Fix #1454411 [Bulk edit metadata date field pull down menu](https://bugs.launchpad.net/calibre/+bug/1454411)

--- src/calibre/gui2/dialogs/metadata_bulk.py
+++ src/calibre/gui2/dialogs/metadata_bulk.py
@@ -352,6 +352,8 @@ class MetadataBulkDialog(ResizableDialog, Ui_MetadataBulkDialog):
         self.pubdate.dateTimeChanged.connect(self.do_apply_pubdate)
         self.adddate.setDateTime(QDateTime.currentDateTime())
         self.adddate.setMinimumDateTime(UNDEFINED_QDATETIME)
+        self.adddate_cw = CalendarWidget(self.adddate)
+        self.adddate.setCalendarWidget(self.adddate_cw)
         adddate_format = tweaks['gui_timestamp_display_format']
         if adddate_format is not None:
             self.adddate.setDisplayFormat(adddate_format)
