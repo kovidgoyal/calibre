694b457a1af1aa6003b2ff727e9fb225d495656e
Kovid Goyal <kovid@kovidgoyal.net>
2015-05-28 09:53:47 +0530

    DOCX Output: Detect the common idiom of placing centered images as inline images inside a containing centered block and render the resulting image as a floating centered image in the DOCX file. See #1455502 (EPUB to DOCX Conversation)

--- src/calibre/ebooks/docx/writer/images.py
+++ src/calibre/ebooks/docx/writer/images.py
@@ -81,6 +81,13 @@ class ImagesManager(object):
                 floating = 'center' if mr == 'auto' else 'right'
             if mr == 'auto':
                 floating = 'center' if ml == 'auto' else 'right'
+        else:
+            parent = html_img.getparent()
+            if len(parent) == 1 and not (parent.text or '').strip() and not (html_img.tail or '').strip():
+                # We have an inline image alone inside a block
+                pstyle = stylizer.style(parent)
+                if pstyle['text-align'] in ('center', 'right'):
+                    floating = pstyle['text-align']
         fake_margins = floating is None
         self.count += 1
         img = self.images[href]
