4d87cb01ff43351acf2cf067569e69fd4e552a92
David <davidfor@internode.on.net>
2015-06-04 23:50:53 +1000

    Bump Kobo firmware version for release of 3.16.1
    Bumping firmware version in preparation for release of firmware version
    3.16.0 with an allowance for 3.16.1. Fixes #1461964 [Bump Kobo firmware version for release of 3.16.1](https://bugs.launchpad.net/calibre/+bug/1461964)

--- src/calibre/devices/kobo/driver.py
+++ src/calibre/devices/kobo/driver.py
@@ -1265,7 +1265,7 @@ class KOBOTOUCH(KOBO):
     min_dbversion_activity          = 77
     min_dbversion_keywords          = 82
 
-    max_supported_fwversion         = (3, 15, 1)
+    max_supported_fwversion         = (3, 16, 1)
     # The following document firwmare versions where new function or devices were added.
     # Not all are used, but this feels a good place to record it.
     min_fwversion_shelves           = (2, 0, 0)
