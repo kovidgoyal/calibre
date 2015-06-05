9a6fba244a5ebdf71fa1d756fa91e26f7b80f525
Kovid Goyal <kovid@kovidgoyal.net>
2015-06-05 14:20:31 +0530

    Similarly do a clean shutdown on SIGHUP unless daemonized

--- src/calibre/srv/standalone.py
+++ src/calibre/srv/standalone.py
@@ -129,4 +129,6 @@ def main(args=sys.argv):
         with lopen(opts.pidfile, 'wb') as f:
             f.write(str(os.getpid()))
     signal.signal(signal.SIGTERM, lambda s,f: server.stop())
+    if not opts.daemonize:
+        signal.signal(signal.SIGHUP, lambda s,f: server.stop())
     server.serve_forever()
