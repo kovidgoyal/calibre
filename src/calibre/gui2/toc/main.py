eb8c761633091566e8925d409d6344b0bf66c75c
Kovid Goyal <kovid@kovidgoyal.net>
2015-10-29 20:00:51 +0530

    ...

--- src/calibre/gui2/toc/main.py
+++ src/calibre/gui2/toc/main.py
@@ -624,7 +624,7 @@ class TOCView(QWidget):  # {{{
         l.addWidget(b, col, 1)
         self.default_msg = _('Double click on an entry to change the text')
         self.hl = hl = QLabel(self.default_msg)
-        hl.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Preferred)
+        hl.setSizePolicy(QSizePolicy.Ignored, QSizePolicy.Ignored)
         l.addWidget(hl, col, 2, 1, -1)
         self.item_view = i = ItemView(self, self.prefs)
         self.item_view.delete_item.connect(self.delete_current_item)
