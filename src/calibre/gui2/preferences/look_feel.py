38ed56dc62ab9c66558bfd7c35a94f9ee24c508e
Kovid Goyal <kovid@kovidgoyal.net>
2016-04-15 10:58:50 +0530

    ...

--- src/calibre/gui2/preferences/look_feel.py
+++ src/calibre/gui2/preferences/look_feel.py
@@ -58,7 +58,7 @@ class IdLinksRuleEdit(Dialog):
         self.l = l = QFormLayout(self)
         l.setFieldGrowthPolicy(l.AllNonFixedFieldsGrow)
         l.addRow(QLabel(_(
-            'The key of the identifier, for example, in isbn:XXX, they key is isbn')))
+            'The key of the identifier, for example, in isbn:XXX, the key is isbn')))
         self.key = k = QLineEdit(self)
         l.addRow(_('&Key:'), k)
         l.addRow(QLabel(_(
