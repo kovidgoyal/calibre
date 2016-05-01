576d96f1da908af7722dce7250de00bbf7b4d7b3
Kovid Goyal <kovid@kovidgoyal.net>
2016-05-01 11:33:57 +0530

    Update duktape

--- src/duktape/duktape/duktape.h
+++ src/duktape/duktape/duktape.h
@@ -1,12 +1,12 @@
 /*
- *  Duktape public API for Duktape 1.4.0.
+ *  Duktape public API for Duktape 1.4.1.
  *  See the API reference for documentation on call semantics.
  *  The exposed API is inside the DUK_API_PUBLIC_H_INCLUDED
  *  include guard.  Other parts of the header are Duktape
  *  internal and related to platform/compiler/feature detection.
  *
- *  Git commit cad6f595382a0cc1a7e4207794ade5be11b3e397 (v1.4.0).
- *  Git branch master.
+ *  Git commit 70315eded64ade7852680433e4becbfa28afe8e2 (v1.4.1).
+ *  Git branch HEAD.
  *
  *  See Duktape AUTHORS.rst and LICENSE.txt for copyright and
  *  licensing information.
@@ -213,16 +213,16 @@ struct duk_number_list_entry {
  * have 99 for patch level (e.g. 0.10.99 would be a development version
  * after 0.10.0 but before the next official release).
  */
-#define DUK_VERSION                       10400L
+#define DUK_VERSION                       10401L
 
 /* Git commit, describe, and branch for Duktape build.  Useful for
  * non-official snapshot builds so that application code can easily log
  * which Duktape snapshot was used.  Not available in the Ecmascript
  * environment.
  */
-#define DUK_GIT_COMMIT                    "cad6f595382a0cc1a7e4207794ade5be11b3e397"
-#define DUK_GIT_DESCRIBE                  "v1.4.0"
-#define DUK_GIT_BRANCH                    "master"
+#define DUK_GIT_COMMIT                    "70315eded64ade7852680433e4becbfa28afe8e2"
+#define DUK_GIT_DESCRIBE                  "v1.4.1"
+#define DUK_GIT_BRANCH                    "HEAD"
 
 /* Duktape debug protocol version used by this build. */
 #define DUK_DEBUG_PROTOCOL_VERSION        1
