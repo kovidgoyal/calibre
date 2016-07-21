051d9e5d3568c96271af7483b307d0bc81691b25
Kovid Goyal <kovid@kovidgoyal.net>
2016-07-21 20:43:41 +0530

    Fix #1569388 [tags from s to z can not be saved](https://bugs.launchpad.net/calibre/+bug/1569388)

--- src/calibre/gui2/metadata/basic_widgets.py
+++ src/calibre/gui2/metadata/basic_widgets.py
@@ -1286,6 +1286,7 @@ class TagsEdit(EditWithComplete, ToMetadataMixin):  # {{{
 
     def __init__(self, parent):
         EditWithComplete.__init__(self, parent)
+        self.lineEdit().setMaxLength(65536)  # see https://bugs.launchpad.net/bugs/1569388
         self.books_to_refresh = set([])
         self.setToolTip(self.TOOLTIP)
         self.setWhatsThis(self.TOOLTIP)
