2693a2c61479efa7bf987e57024b370561f00eab
Kovid Goyal <kovid@kovidgoyal.net>
2017-01-25 20:38:26 +0530

    Move load(qt_plugin) into the < 5.8 block

--- setup/build.py
+++ setup/build.py
@@ -364,11 +364,11 @@ class Build(Command):
             TARGET = headless
             PLUGIN_TYPE = platforms
             PLUGIN_CLASS_NAME = HeadlessIntegrationPlugin
-            load(qt_plugin)
             QT += core-private gui-private
             greaterThan(QT_MAJOR_VERSION, 5)|greaterThan(QT_MINOR_VERSION, 7): {{
                 QT += theme_support-private
             }} else {{
+                load(qt_plugin)
                 QT += platformsupport-private
             }}
             HEADERS = {headers}
