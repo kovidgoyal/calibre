a2578eac7cc67fab6c8237392e1fbbaecb030176
Kovid Goyal <kovid@kovidgoyal.net>
2017-02-26 23:53:02 +0530

    Add a couple more headers to the browser instance used for amazon metadata download

--- src/calibre/ebooks/metadata/sources/amazon.py
+++ src/calibre/ebooks/metadata/sources/amazon.py
@@ -834,10 +834,10 @@ class Amazon(Source):
         if self._browser is None:
             self._browser = br = browser(user_agent=random_user_agent(allow_ie=False))
             br.set_handle_gzip(True)
-            # br.addheaders += [
-            #     ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
-            #     ('Upgrade-Insecure-Requests', '1'),
-            # ]
+            br.addheaders += [
+                ('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'),
+                ('Upgrade-Insecure-Requests', '1'),
+            ]
         return self._browser.clone_browser()
 
     def save_settings(self, *args, **kwargs):
