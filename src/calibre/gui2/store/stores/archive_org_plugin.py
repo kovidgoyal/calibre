c5845f511bef3007f545dfb1ffb970fe95fe7872
Kovid Goyal <kovid@kovidgoyal.net>
2017-03-06 11:30:05 +0530

    Apparently https is not fully supported on archive.org

--- src/calibre/gui2/store/stores/archive_org_plugin.py
+++ src/calibre/gui2/store/stores/archive_org_plugin.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import (unicode_literals, division, absolute_import, print_function)
-store_version = 3  # Needed for dynamic plugin loading
+store_version = 4  # Needed for dynamic plugin loading
 
 __license__ = 'GPL 3'
 __copyright__ = '2011, John Schember <john@nachtimwald.com>'
@@ -14,14 +14,14 @@ from calibre.gui2.store.search_result import SearchResult
 
 class ArchiveOrgStore(BasicStoreConfig, OpenSearchOPDSStore):
 
-    open_search_url = 'https://bookserver.archive.org/catalog/opensearch.xml'
-    web_url = 'https://www.archive.org/details/texts'
+    open_search_url = 'http://bookserver.archive.org/catalog/opensearch.xml'
+    web_url = 'http://www.archive.org/details/texts'
 
-    # https://bookserver.archive.org/catalog/
+    # http://bookserver.archive.org/catalog/
 
     def search(self, query, max_results=10, timeout=60):
         for s in OpenSearchOPDSStore.search(self, query, max_results, timeout):
-            s.detail_item = 'https://www.archive.org/details/' + s.detail_item.split(':')[-1]
+            s.detail_item = 'http://www.archive.org/details/' + s.detail_item.split(':')[-1]
             s.price = '$0.00'
             s.drm = SearchResult.DRM_UNLOCKED
             yield s
