e5a4d12d4092c8b8e534c88579a9f8f4cdfd68c5
Kovid Goyal <kovid@kovidgoyal.net>
2017-04-07 09:43:35 +0530

    Remove no longer needed cherrypy/routes bundled packages

--- src/cherrypy/lib/http.py
+++
@@ -1,7 +0,0 @@
-import warnings
-warnings.warn('cherrypy.lib.http has been deprecated and will be removed '
-              'in CherryPy 3.3 use cherrypy.lib.httputil instead.',
-              DeprecationWarning)
-
-from cherrypy.lib.httputil import *
-
