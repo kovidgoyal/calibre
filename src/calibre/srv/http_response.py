147b16e4deb6d974ff9c233b7924f659b2438094
Kovid Goyal <kovid@kovidgoyal.net>
2017-04-23 15:54:54 +0530

    Remove the allowed_books_id API from the server
    Better performance and also allows implementing virtual library support
    in the future.

--- src/calibre/srv/http_response.py
+++ src/calibre/srv/http_response.py
@@ -224,7 +224,6 @@ class RequestData(object):  # {{{
         self.lang_code = self.gettext_func = self.ngettext_func = None
         self.set_translator(self.get_preferred_language())
         self.tdir = tdir
-        self.allowed_book_ids = {}
 
     def generate_static_output(self, name, generator, content_type='text/html; charset=UTF-8'):
         ans = self.static_cache.get(name)
