ae38edc6adeccb7bc42153658c9c806ebe65ccd5
Kovid Goyal <kovid@kovidgoyal.net>
2017-04-30 09:26:07 +0530

    Port calibredb add

---
+++ src/calibre/srv/changes.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+# License: GPLv3 Copyright: 2017, Kovid Goyal <kovid at kovidgoyal.net>
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+
+def books_added(book_ids):
+    pass
+
+
+def formats_added(formats_map):
+    pass
