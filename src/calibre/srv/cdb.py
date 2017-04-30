701e0c0c4f741f3d30760f643f8653851ab19fce
Kovid Goyal <kovid@kovidgoyal.net>
2017-04-30 10:39:38 +0530

    Implement versioning for the cdb API

--- src/calibre/srv/cdb.py
+++ src/calibre/srv/cdb.py
@@ -14,14 +14,18 @@ from calibre.utils.serialize import MSGPACK_MIME, msgpack_loads, json_loads
 receive_data_methods = {'GET', 'POST'}
 
 
-@endpoint('/cdb/run/{which}', postprocess=msgpack_or_json, methods=receive_data_methods)
-def cdb_run(ctx, rd, which):
+@endpoint('/cdb/run/{which}/{version=0}', postprocess=msgpack_or_json, methods=receive_data_methods)
+def cdb_run(ctx, rd, which, version):
     try:
         m = module_for_cmd(which)
     except ImportError:
         raise HTTPNotFound('No module named: {}'.format(which))
     if not getattr(m, 'readonly', False):
         ctx.check_for_write_access(rd)
+    if getattr(m, 'version', 0) != int(version):
+        raise HTTPNotFound(('The module {} is not available in version: {}.'
+                           'Make sure the version of calibre used for the'
+                            ' server and calibredb match').format(which, version))
     raw = rd.read()
     ct = rd.inheaders.get('Content-Type', all=True)
     try:
