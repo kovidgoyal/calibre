1b00c3af88a173a748764811b0af2720b5613c12
Kovid Goyal <kovid@kovidgoyal.net>
2017-05-03 17:21:57 +0530

    See if unbufferring and an explicit flush fix test on OS X

--- src/calibre/utils/test_lock.py
+++ src/calibre/utils/test_lock.py
@@ -36,6 +36,7 @@ def run_worker(mod, func, **kw):
     exe = [sys.executable, os.path.join(sys.setup_dir, 'run-calibre-worker.py')]
     env = kw.get('env', os.environ.copy())
     env['CALIBRE_SIMPLE_WORKER'] = mod + ':' + func
+    env['PYTHONUNBUFFERED'] = '1'
     if iswindows:
         import win32process
         kw['creationflags'] = win32process.CREATE_NO_WINDOW
@@ -69,7 +70,9 @@ class IPCLockTest(unittest.TestCase):
 
     def test_exclusive_file_other_process(self):
         child = run_worker('calibre.utils.test_lock', 'other1', stdout=subprocess.PIPE)
+        print(1111111111)
         ready = child.stdout.readline()
+        print(2222222222)
         self.assertEqual(ready.strip(), b'ready')
         ef = FastFailEF('test')
         self.assertRaises(EnvironmentError, ef.__enter__)
@@ -84,10 +87,14 @@ def other1():
     e = ExclusiveFile('test')
     with e:
         print('ready')
-        sys.stdout.close()
-        sys.stderr.close()
+        sys.stdout.flush()
         time.sleep(30)
 
 
 def find_tests():
     return unittest.defaultTestLoader.loadTestsFromTestCase(IPCLockTest)
+
+
+if __name__ == '__main__':
+    suite = find_tests()
+    unittest.TextTestRunner(verbosity=4).run(suite)
