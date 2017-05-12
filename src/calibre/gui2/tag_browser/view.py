43f8eb3be6fb68eee2ba132658699a648f0a7b3f
Kovid Goyal <kovid@kovidgoyal.net>
2017-05-12 23:32:29 +0530

    String changes

--- src/calibre/gui2/tag_browser/view.py
+++ src/calibre/gui2/tag_browser/view.py
@@ -634,7 +634,7 @@ class TagsView(QTreeView):  # {{{
                     self.context_menu.addAction(_('Manage %s')%category,
                             partial(self.context_menu_handler, action='edit_author_sort'))
                 elif key == 'search':
-                    self.context_menu.addAction(_('Manage Saved Searches'),
+                    self.context_menu.addAction(_('Manage Saved searches'),
                         partial(self.context_menu_handler, action='manage_searches',
                                 category=tag.name if tag else None))
 
