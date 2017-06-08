be38e09a34a9fc2f8a8c00e62db8df61c4574312
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-08 18:23:20 +0530

    Use explicit module imports for Qt to get better tracebacks

--- src/calibre/utils/img.py
+++ src/calibre/utils/img.py
@@ -13,10 +13,9 @@ import tempfile
 from io import BytesIO
 from threading import Thread
 
-from PyQt5.Qt import (
-    QBuffer, QByteArray, QColor, QImage, QImageReader, QImageWriter, QPixmap, Qt,
-    QTransform
-)
+# We use explicit module imports so tracebacks when importing are more useful
+from PyQt5.QtCore import QBuffer, QByteArray, Qt
+from PyQt5.QtGui import QColor, QImage, QImageReader, QImageWriter, QPixmap, QTransform
 
 from calibre import fit_image, force_unicode
 from calibre.constants import iswindows, plugins
