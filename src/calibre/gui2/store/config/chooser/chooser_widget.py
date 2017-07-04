9d6cca4ebdf7c80bfdd605cf20274fc6836ed23d
Kovid Goyal <kovid@kovidgoyal.net>
2017-07-04 10:54:56 +0530

    Fix #1700266 [[Request] Move Advenced search button into Search box](https://bugs.launchpad.net/calibre/+bug/1700266)

--- src/calibre/gui2/store/config/chooser/chooser_widget.py
+++ src/calibre/gui2/store/config/chooser/chooser_widget.py
@@ -6,7 +6,7 @@ __license__ = 'GPL 3'
 __copyright__ = '2011, John Schember <john@nachtimwald.com>'
 __docformat__ = 'restructuredtext en'
 
-from PyQt5.Qt import (QWidget, QIcon, QDialog, QComboBox)
+from PyQt5.Qt import QWidget, QIcon, QDialog, QComboBox, QLineEdit
 
 from calibre.gui2.store.config.chooser.adv_search_builder import AdvSearchBuilderDialog
 from calibre.gui2.store.config.chooser.chooser_widget_ui import Ui_Form
@@ -21,11 +21,10 @@ class StoreChooserWidget(QWidget, Ui_Form):
         self.query.initialize('store_config_chooser_query')
         self.query.setSizeAdjustPolicy(QComboBox.AdjustToMinimumContentsLengthWithIcon)
         self.query.setMinimumContentsLength(25)
-
-        self.adv_search_builder.setIcon(QIcon(I('search.png')))
-
+        self.adv_search_action = ac = self.query.lineEdit().addAction(QIcon(I('gear.png')), QLineEdit.LeadingPosition)
+        ac.triggered.connect(self.build_adv_search)
+        ac.setToolTip(_('Advanced search'))
         self.search.clicked.connect(self.do_search)
-        self.adv_search_builder.clicked.connect(self.build_adv_search)
         self.enable_all.clicked.connect(self.results_view.model().enable_all)
         self.enable_none.clicked.connect(self.results_view.model().enable_none)
         self.enable_invert.clicked.connect(self.results_view.model().enable_invert)
