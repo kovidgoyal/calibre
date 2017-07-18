15ca24722fd38258dd5752d7917d7d349465ac1d
David <davidfor@internode.on.net>
2017-07-19 00:43:47 +1000

    Bump Kobo supported firmware version to 4.5.9587
    New firmware just released. No changes needed except to bump the
    firmware and database versions.

--- src/calibre/devices/kobo/driver.py
+++ src/calibre/devices/kobo/driver.py
@@ -1313,7 +1313,7 @@ class KOBOTOUCH(KOBO):
                     ' Based on the existing Kobo driver by %s.') % KOBO.author
 #    icon        = I('devices/kobotouch.jpg')
 
-    supported_dbversion             = 141
+    supported_dbversion             = 142
     min_supported_dbversion         = 53
     min_dbversion_series            = 65
     min_dbversion_externalid        = 65
@@ -1325,7 +1325,7 @@ class KOBOTOUCH(KOBO):
     # Starting with firmware version 3.19.x, the last number appears to be is a
     # build number. A number will be recorded here but it can be safely ignored
     # when testing the firmware version.
-    max_supported_fwversion         = (4, 4, 9278)
+    max_supported_fwversion         = (4, 5, 9587)
     # The following document firwmare versions where new function or devices were added.
     # Not all are used, but this feels a good place to record it.
     min_fwversion_shelves           = (2, 0, 0)
