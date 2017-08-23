2f35ca7ca52a9a6e851239ddaaace2d73895e2d9
Kovid Goyal <kovid@kovidgoyal.net>
2017-08-23 07:49:45 +0530

    Fix compiler warnings when compiling patience diff module

--- src/calibre/gui2/tweak_book/diff/_patiencediff_c.c
+++ src/calibre/gui2/tweak_book/diff/_patiencediff_c.c
@@ -50,7 +50,7 @@
  * causing <https://bugs.launchpad.net/bzr/+bug/511267> and
  * <https://bugs.launchpad.net/bzr/+bug/331095>.  On glibc it passes, but
  * let's make it fail to aid testing. */
-#define guarded_malloc(x) ( (x) ? malloc(x) : NULL )
+static inline void* guarded_malloc(size_t x) { return x ? malloc(x) : NULL; }
 
 enum {
     OP_EQUAL = 0,
