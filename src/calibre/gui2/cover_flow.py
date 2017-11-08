b95d0a537d3c7bfc095244a6acef79bb74555daf
Kovid Goyal <kovid@kovidgoyal.net>
2017-11-08 07:23:43 +0530

    Fix transient error during startup when using subtititle templates in the cover browser

--- src/calibre/gui2/cover_flow.py
+++ src/calibre/gui2/cover_flow.py
@@ -133,6 +133,8 @@ if pictureflow is not None:
         def subtitle(self, index):
             try:
                 db = self.model.db.new_api
+                if not self.template_inited:
+                    self.init_template(db)
                 field = db.pref('cover_browser_subtitle_field', 'rating')
                 if field and field != 'none':
                     book_id = self.model.id(index)
