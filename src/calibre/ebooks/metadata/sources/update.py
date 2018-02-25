348d6cecce0a35af03b7796f154463a84bf66baf
Kovid Goyal <kovid@kovidgoyal.net>
2018-02-25 10:22:51 +0530

    Fix patching of search engines module not working

--- src/calibre/ebooks/metadata/sources/update.py
+++ src/calibre/ebooks/metadata/sources/update.py
@@ -43,6 +43,18 @@ def load_plugin(src):
             return x
 
 
+class PatchedSearchEngines(object):
+
+    def __init__(self, ns):
+        self.__ns = ns
+
+    def __getattr__(self, attr):
+        try:
+            return self.__ns[attr]
+        except KeyError:
+            raise AttributeError('{} not present in search_engines_module'.format(attr))
+
+
 def patch_search_engines(src):
     global current_search_engines
     src = src.encode('utf-8')
@@ -54,7 +66,7 @@ def patch_search_engines(src):
     cv = ns.get('current_version')
     if cv is None or cv <= builtin_search_engines.current_version:
         return
-    current_search_engines = ns
+    current_search_engines = PatchedSearchEngines(ns)
 
 
 def patch_plugins():
