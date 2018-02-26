581b2c07001b183f876e4ddae93691910e92fbfc
Kovid Goyal <kovid@kovidgoyal.net>
2018-02-26 09:14:19 +0530

    Forgot to add context-menu-split to all_locations

--- src/calibre/gui2/actions/__init__.py
+++ src/calibre/gui2/actions/__init__.py
@@ -96,7 +96,7 @@ class InterfaceAction(QObject):
 
     all_locations = frozenset(['toolbar', 'toolbar-device', 'context-menu',
         'context-menu-device', 'toolbar-child', 'menubar', 'menubar-device',
-        'context-menu-cover-browser'])
+        'context-menu-cover-browser', 'context-menu-split'])
 
     #: Type of action
     #: 'current' means acts on the current view
