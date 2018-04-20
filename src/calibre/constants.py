557a048d104ed7f834d23826a7c4580bfeeecc52
Kovid Goyal <kovid@kovidgoyal.net>
2018-04-20 12:55:09 +0530

    version 3.22.1

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -6,7 +6,7 @@ from future_builtins import map
 import sys, locale, codecs, os, importlib, collections
 
 __appname__   = u'calibre'
-numeric_version = (3, 22, 0)
+numeric_version = (3, 22, 1)
 __version__   = u'.'.join(map(unicode, numeric_version))
 __author__    = u"Kovid Goyal <kovid@kovidgoyal.net>"
 
