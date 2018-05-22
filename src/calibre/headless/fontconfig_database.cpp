b9f223f412683def57523b12c6ddcef606bb6262
Eli Schwartz <eschwartz93@gmail.com>
2018-05-22 09:04:32 -0400

    fix qt 5.11 support
    Mostly just merge https://github.com/qt/qtbase/commit/8bfabb34dec8a437a08b5a6e0ecac4a9dd3ae18c
    but I added the other changes to that file too, for consistency.

--- src/calibre/headless/fontconfig_database.cpp
+++ src/calibre/headless/fontconfig_database.cpp
@@ -284,7 +284,21 @@ static const char specialLanguages[][6] = {
     "", // Hatran
     "", // Multani
     "", // OldHungarian
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 11, 0))
+    "", // SignWriting
+    "", // Adlam
+    "", // Bhaiksuki
+    "", // Marchen
+    "", // Newa
+    "", // Osage
+    "", // Tangut
+    "", // MasaramGondi
+    "", // Nushu
+    "", // Soyombo
+    "" // ZanabazarSquare
+#else
     ""  // SignWriting
+#endif
 #else
     "hoc"  // WarangCiti
 #endif  // Qt >= 5.6.0
@@ -475,7 +489,7 @@ static void populateFromPattern(FcPattern *pattern)
     if (res == FcResultMatch) {
         bool hasLang = false;
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)) && FC_VERSION >= 20297
-        FcChar8 *cap = Q_NULLPTR;
+        FcChar8 *cap = nullptr;
         FcResult capRes = FcResultNoMatch;
 #endif
         for (int j = 1; j < QFontDatabase::WritingSystemsCount; ++j) {
@@ -485,7 +499,7 @@ static void populateFromPattern(FcPattern *pattern)
                 if (langRes != FcLangDifferentLang) {
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)) && FC_VERSION >= 20297
                     if (*capabilityForWritingSystem[j] && requiresOpenType(j)) {
-                        if (cap == Q_NULLPTR)
+                        if (cap == nullptr)
                             capRes = FcPatternGetString(pattern, FC_CAPABILITY, 0, &cap);
                         if (capRes == FcResultMatch && strstr(reinterpret_cast<const char *>(cap), capabilityForWritingSystem[j]) == 0)
                             continue;
@@ -541,7 +555,7 @@ static void populateFromPattern(FcPattern *pattern)
     QPlatformFontDatabase::registerFont(familyName,styleName,QLatin1String((const char *)foundry_value),weight,style,stretch,antialias,scalable,pixel_size,fixedPitch,writingSystems,fontFile);
 //        qDebug() << familyName << (const char *)foundry_value << weight << style << &writingSystems << scalable << true << pixel_size;
 
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 8, 0)) 
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 8, 0))
     for (int k = 1; FcPatternGetString(pattern, FC_FAMILY, k, &value) == FcResultMatch; ++k) {
         const QString altFamilyName = QString::fromUtf8((const char *)value);
         // Extra family names can be aliases or subfamilies.
@@ -674,7 +688,7 @@ QFontEngine::HintStyle defaultHintStyleFromMatch(QFont::HintingPreference hintin
     }
 
     // Removed by Kovid for headless QPA
-	
+
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
     if (QHighDpiScaling::isActive())
         return QFontEngine::HintNone;
