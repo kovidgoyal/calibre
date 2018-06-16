d82d6f823c4598436fb16b954a95fccd7cbfdb76
Kovid Goyal <kovid@kovidgoyal.net>
2018-06-16 13:10:05 +0530

    Add a termplate

---
+++ =template.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+# License: GPL v3 Copyright: %YEAR%, %USER% <%MAIL%>
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+%HERE%
