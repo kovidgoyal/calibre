5961223f773ac994baaf46824df4c826fb08af4a
Kovid Goyal <kovid@kovidgoyal.net>
2018-12-20 21:03:51 +0530

    Add a link to the moving calibre data instructions tothe welcome wizard.  Fixes #1809203 [[Enhancement] - New installs](https://bugs.launchpad.net/calibre/+bug/1809203)

--- src/calibre/gui2/wizard/__init__.py
+++ src/calibre/gui2/wizard/__init__.py
@@ -652,6 +652,11 @@ class LibraryPage(QWizardPage, LibraryUI):
         self.init_languages()
         self.language.currentIndexChanged[int].connect(self.change_language)
         self.location.textChanged.connect(self.location_text_changed)
+        self.move_lib_label.setText(_(
+            'If you are moving calibre from an old computer to a new one,'
+            ' please read <a href="{0}">the instructions</a>.').format(
+                localize_user_manual_link(
+        'https://manual.calibre-ebook.com/faq.html#how-do-i-move-my-calibre-data-from-one-computer-to-another')))
 
     def location_text_changed(self, newtext):
         self.completeChanged.emit()
