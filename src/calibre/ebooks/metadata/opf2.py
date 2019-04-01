8389aab84b2d8e729ac585f5677753a72ccc1f8d
Eli Schwartz <eschwartz@archlinux.org>
2019-04-01 12:48:33 -0400

    py3: pass bytes to io.BytesIO

--- src/calibre/ebooks/metadata/opf2.py
+++ src/calibre/ebooks/metadata/opf2.py
@@ -1759,7 +1759,7 @@ class OPFTest(unittest.TestCase):
 
     def setUp(self):
         self.stream = io.BytesIO(
-'''\
+b'''\
 <?xml version="1.0"  encoding="UTF-8"?>
 <package version="2.0" xmlns="http://www.idpf.org/2007/opf" >
 <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
