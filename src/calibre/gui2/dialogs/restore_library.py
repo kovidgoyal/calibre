6dd0f72bae7406b18f9e455f71a193f467fc7856
Kovid Goyal <kovid@kovidgoyal.net>
2019-04-12 08:57:39 +0530

    ...

--- src/calibre/gui2/dialogs/restore_library.py
+++ src/calibre/gui2/dialogs/restore_library.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python2
 # vim:fileencoding=UTF-8:ts=4:sw=4:sta:et:sts=4:ai
+# License: GPLv3 Copyright: 2011, Kovid Goyal <kovid at kovidgoyal.net>
 
-__license__   = 'GPL v3'
-__copyright__ = '2011, Kovid Goyal <kovid@kovidgoyal.net>'
-__docformat__ = 'restructuredtext en'
+from __future__ import absolute_import, division, print_function, unicode_literals
 
 from PyQt5.Qt import (QDialog, QLabel, QVBoxLayout, QDialogButtonBox,
         QProgressBar, QSize, QTimer, pyqtSignal, Qt)
