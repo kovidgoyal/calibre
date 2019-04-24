54ddf5ec5fac29f3364600277043dff5aeed3804
Kovid Goyal <kovid@kovidgoyal.net>
2019-04-24 12:03:12 +0530

    Log underlying exception when converting RTF fails

--- src/calibre/ebooks/conversion/plugins/rtf_input.py
+++ src/calibre/ebooks/conversion/plugins/rtf_input.py
@@ -256,6 +256,7 @@ class RTFInput(InputFormatPlugin):
         try:
             xml = self.generate_xml(stream.name)
         except RtfInvalidCodeException as e:
+            self.log.exception('Unable to parse RTF')
             raise ValueError(_('This RTF file has a feature calibre does not '
             'support. Convert it to HTML first and then try it.\n%s')%e)
 
