94db9b84cd784cb7a3495ce75f10791f0f8c0614
Eli Schwartz <eschwartz@archlinux.org>
2019-05-27 12:39:57 -0400

    reindent lines for neatness

--- src/calibre/ebooks/conversion/plugins/fb2_output.py
+++ src/calibre/ebooks/conversion/plugins/fb2_output.py
@@ -67,80 +67,80 @@ class FB2Output(OutputFormatPlugin):
         # Children's
         'child_tale',  # Fairy Tales
         'child_verse',  # Verses
-    'child_prose',  # Prose
-    'child_sf',  # Science Fiction
-    'child_det',  # Detectives & Thrillers
-    'child_adv',  # Adventures
-    'child_education',  # Educational
-    'children',  # Other
-    # Poetry & Dramaturgy
-    'poetry',  # Poetry
-    'dramaturgy',  # Dramaturgy
-    # Antique literature
-    'antique_ant',  # Antique
-    'antique_european',  # European
-    'antique_russian',  # Old russian
-    'antique_east',  # Old east
-    'antique_myths',  # Myths. Legends. Epos
-    'antique',  # Other
-    # Scientific#educational
-    'sci_history',  # History
-    'sci_psychology',  # Psychology
-    'sci_culture',  # Cultural science
-    'sci_religion',  # Religious studies
-    'sci_philosophy',  # Philosophy
-    'sci_politics',  # Politics
-    'sci_business',  # Business literature
-    'sci_juris',  # Jurisprudence
-    'sci_linguistic',  # Linguistics
-    'sci_medicine',  # Medicine
-    'sci_phys',  # Physics
-    'sci_math',  # Mathematics
-    'sci_chem',  # Chemistry
-    'sci_biology',  # Biology
-    'sci_tech',  # Technical
-    'science',  # Other
-    # Computers & Internet
-    'comp_www',  # Internet
-    'comp_programming',  # Programming
-    'comp_hard',  # Hardware
-    'comp_soft',  # Software
-    'comp_db',  # Databases
-    'comp_osnet',  # OS & Networking
-    'computers',  # Other
-    # Reference
-    'ref_encyc',  # Encyclopedias
-    'ref_dict',  # Dictionaries
-    'ref_ref',  # Reference
-    'ref_guide',  # Guidebooks
-    'reference',  # Other
-    # Nonfiction
-    'nonf_biography',  # Biography & Memoirs
-    'nonf_publicism',  # Publicism
-    'nonf_criticism',  # Criticism
-    'design',  # Art & design
-    'nonfiction',  # Other
-    # Religion & Inspiration
-    'religion_rel',  # Religion
-    'religion_esoterics',  # Esoterics
-    'religion_self',  # Self#improvement
-    'religion',  # Other
-    # Humor
-    'humor_anecdote',  # Anecdote (funny stories)
-    'humor_prose',  # Prose
-    'humor_verse',  # Verses
-    'humor',  # Other
-    # Home & Family
-    'home_cooking',  # Cooking
-    'home_pets',  # Pets
-    'home_crafts',  # Hobbies & Crafts
-    'home_entertain',  # Entertaining
-    'home_health',  # Health
-    'home_garden',  # Garden
-    'home_diy',  # Do it yourself
-    'home_sport',  # Sports
-    'home_sex',  # Erotica & sex
-    'home',  # Other
+        'child_prose',  # Prose
+        'child_sf',  # Science Fiction
+        'child_det',  # Detectives & Thrillers
+        'child_adv',  # Adventures
+        'child_education',  # Educational
+        'children',  # Other
+        # Poetry & Dramaturgy
+        'poetry',  # Poetry
+        'dramaturgy',  # Dramaturgy
+        # Antique literature
+        'antique_ant',  # Antique
+        'antique_european',  # European
+        'antique_russian',  # Old russian
+        'antique_east',  # Old east
+        'antique_myths',  # Myths. Legends. Epos
+        'antique',  # Other
+        # Scientific#educational
+        'sci_history',  # History
+        'sci_psychology',  # Psychology
+        'sci_culture',  # Cultural science
+        'sci_religion',  # Religious studies
+        'sci_philosophy',  # Philosophy
+        'sci_politics',  # Politics
+        'sci_business',  # Business literature
+        'sci_juris',  # Jurisprudence
+        'sci_linguistic',  # Linguistics
+        'sci_medicine',  # Medicine
+        'sci_phys',  # Physics
+        'sci_math',  # Mathematics
+        'sci_chem',  # Chemistry
+        'sci_biology',  # Biology
+        'sci_tech',  # Technical
+        'science',  # Other
+        # Computers & Internet
+        'comp_www',  # Internet
+        'comp_programming',  # Programming
+        'comp_hard',  # Hardware
+        'comp_soft',  # Software
+        'comp_db',  # Databases
+        'comp_osnet',  # OS & Networking
+        'computers',  # Other
+        # Reference
+        'ref_encyc',  # Encyclopedias
+        'ref_dict',  # Dictionaries
+        'ref_ref',  # Reference
+        'ref_guide',  # Guidebooks
+        'reference',  # Other
+        # Nonfiction
+        'nonf_biography',  # Biography & Memoirs
+        'nonf_publicism',  # Publicism
+        'nonf_criticism',  # Criticism
+        'design',  # Art & design
+        'nonfiction',  # Other
+        # Religion & Inspiration
+        'religion_rel',  # Religion
+        'religion_esoterics',  # Esoterics
+        'religion_self',  # Self#improvement
+        'religion',  # Other
+        # Humor
+        'humor_anecdote',  # Anecdote (funny stories)
+        'humor_prose',  # Prose
+        'humor_verse',  # Verses
+        'humor',  # Other
+        # Home & Family
+        'home_cooking',  # Cooking
+        'home_pets',  # Pets
+        'home_crafts',  # Hobbies & Crafts
+        'home_entertain',  # Entertaining
+        'home_health',  # Health
+        'home_garden',  # Garden
+        'home_diy',  # Do it yourself
+        'home_sport',  # Sports
+        'home_sex',  # Erotica & sex
+        'home',  # Other
     ]
     ui_data = {
         'sectionize': {
