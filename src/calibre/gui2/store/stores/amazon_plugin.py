36604c52eed8f7c8c9fdad40f3d3485a19a15d47
Kovid Goyal <kovid@kovidgoyal.net>
2019-06-01 07:28:53 +0530

    Forgot to bump plugin version

--- src/calibre/gui2/store/stores/amazon_plugin.py
+++ src/calibre/gui2/store/stores/amazon_plugin.py
@@ -3,7 +3,7 @@
 # License: GPLv3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-store_version = 16  # Needed for dynamic plugin loading
+store_version = 17  # Needed for dynamic plugin loading
 
 from contextlib import closing
 try:
