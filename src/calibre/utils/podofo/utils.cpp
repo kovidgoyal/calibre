b808ee00780ef72a5d7d733f0c1fb4000d792dd1
Kovid Goyal <kovid@kovidgoyal.net>
2019-08-10 22:12:53 +0530

    ...

--- src/calibre/utils/podofo/utils.cpp
+++ src/calibre/utils/podofo/utils.cpp
@@ -31,8 +31,7 @@ pdf::podofo_convert_pdfstring(const PdfString &s) {
 const PdfString
 pdf::podofo_convert_pystring(PyObject *val) {
 #if PY_MAJOR_VERSION > 2
-    PdfString s(reinterpret_cast<const pdf_utf8*>(PyUnicode_AsUTF8(val)));
-    return s;
+    return PdfString(reinterpret_cast<const pdf_utf8*>(PyUnicode_AsUTF8(val)));
 #else
     PyObject *temp = PyUnicode_AsUTF8String(val);
     if (!temp) throw std::bad_alloc();
