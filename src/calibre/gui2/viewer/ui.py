351bae75b0ce3f36821276bfe81bc3976b18c652
Kovid Goyal <kovid@kovidgoyal.net>
2019-08-26 11:08:53 +0530

    Specialize controls help for standalone viewer

--- src/calibre/gui2/viewer/ui.py
+++ src/calibre/gui2/viewer/ui.py
@@ -2,8 +2,6 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2018, Kovid Goyal <kovid at kovidgoyal.net>
 
-# TODO: Change the help screen for the standalone viewer
-
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import json
