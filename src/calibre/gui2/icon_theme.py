de505a1a345112273e6eb9beccb5d85b774f252c
Kovid Goyal <kovid@kovidgoyal.net>
2019-09-10 17:31:04 +0530

    Fix from last py3 merge

--- src/calibre/gui2/icon_theme.py
+++ src/calibre/gui2/icon_theme.py
@@ -38,7 +38,7 @@ from calibre.utils.img import image_from_data, Canvas, optimize_png, optimize_jp
 from calibre.utils.zipfile import ZipFile, ZIP_STORED
 from calibre.utils.filenames import atomic_rename
 from lzma.xz import compress, decompress
-from polyglot.builtins import iteritems, map, range, reraise, filter, as_bytes
+from polyglot.builtins import iteritems, map, range, reraise, filter, as_bytes, unicode_type
 from polyglot import http_client
 from polyglot.queue import Queue, Empty
 
