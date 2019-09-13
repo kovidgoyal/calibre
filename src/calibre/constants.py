10bfd1406b674d2c16fd9c3670046d343de5f156
Kovid Goyal <kovid@kovidgoyal.net>
2019-09-13 08:16:01 +0530

    version 3.48.0

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -6,7 +6,7 @@ from polyglot.builtins import map, unicode_type, environ_item, hasenv, getenv
 import sys, locale, codecs, os, importlib, collections
 
 __appname__   = 'calibre'
-numeric_version = (3, 47, 1)
+numeric_version = (3, 48, 0)
 __version__   = '.'.join(map(unicode_type, numeric_version))
 git_version   = None
 __author__    = "Kovid Goyal <kovid@kovidgoyal.net>"
