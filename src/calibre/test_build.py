6d2d0d8201c6c5c591a83051240aa4b45694dc23
Kovid Goyal <kovid@kovidgoyal.net>
2019-09-16 13:56:41 +0530

    Skip the webengine build test on macOS build VM as it is flaky

--- src/calibre/test_build.py
+++ src/calibre/test_build.py
@@ -230,8 +230,10 @@ class BuildTest(unittest.TestCase):
         p.printToPdf(print_callback)
         QTimer.singleShot(5000, lambda: QApplication.instance().quit())
         QApplication.instance().exec_()
-        self.assertEqual(callback.result, 2, 'Simple JS computation failed')
-        self.assertIn(b'Skia/PDF', bytes(print_callback.result), 'Print to PDF failed')
+        test_flaky = isosx and not is_ci
+        if not test_flaky:
+            self.assertEqual(callback.result, 2, 'Simple JS computation failed')
+            self.assertIn(b'Skia/PDF', bytes(print_callback.result), 'Print to PDF failed')
         del p
         del na
         destroy_app()
