e0630cadd345a1b368a2f552cbae08ac1e4da3c6
Kovid Goyal <kovid@kovidgoyal.net>
2019-10-05 10:27:48 +0530

    Viewer: Fix failing to open books if path to cache contains symbolic links. Fixes #1846834 [symlinked .cache cause calibre 4 reader fail](https://bugs.launchpad.net/calibre/+bug/1846834)

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -146,7 +146,7 @@ def _get_cache_dir():
 def cache_dir():
     ans = getattr(cache_dir, 'ans', None)
     if ans is None:
-        ans = cache_dir.ans = _get_cache_dir()
+        ans = cache_dir.ans = os.path.realpath(_get_cache_dir())
     return ans
 
 
