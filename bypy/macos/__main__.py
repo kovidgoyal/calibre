10d5d8b6bc4c96363c9a7d5026e1b8d5d3844602
Kovid Goyal <kovid@kovidgoyal.net>
2019-10-17 11:10:42 +0530

    Fix a regression in 4.0 that broke rendering of PDF covers for PDF files that used JPEG2000 compression. Fixes #1847567 [Cover generation from PDF files is not working](https://bugs.launchpad.net/calibre/+bug/1847567)

--- bypy/macos/__main__.py
+++ bypy/macos/__main__.py
@@ -457,7 +457,7 @@ class Freeze(object):
     @flush
     def add_poppler(self):
         print('\nAdding poppler')
-        for x in ('libpoppler.87.dylib',):
+        for x in ('libopenjp2.7.dylib', 'libpoppler.87.dylib',):
             self.install_dylib(join(PREFIX, 'lib', x))
         for x in ('pdftohtml', 'pdftoppm', 'pdfinfo'):
             self.install_dylib(
