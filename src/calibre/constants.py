82f2412ad582c3376fca50f056f67b80537b4fb3
Kovid Goyal <kovid@kovidgoyal.net>
2019-11-18 20:28:38 +0530

    Remove some more referefences to the old lzma

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -182,7 +182,6 @@ class Plugins(collections.Mapping):
                 'matcher',
                 'tokenizer',
                 'certgen',
-                'lzma_binding',
             ]
         if iswindows:
             plugins.extend(['winutil', 'wpd', 'winfonts'])
