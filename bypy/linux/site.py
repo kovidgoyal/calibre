e5d5d7fbc7cfadad201bca6204e35290b9d3e099
Kovid Goyal <kovid@kovidgoyal.net>
2019-11-21 21:03:42 +0530

    Remove useless if branch

--- bypy/linux/site.py
+++ bypy/linux/site.py
@@ -10,17 +10,7 @@ import _sitebuiltins
 
 
 def set_quit():
-    """Define new builtins 'quit' and 'exit'.
-
-    These are objects which make the interpreter exit when called.
-    The repr of each object contains a hint at how it works.
-
-    """
-    if os.sep == '\\':
-        eof = 'Ctrl-Z plus Return'
-    else:
-        eof = 'Ctrl-D (i.e. EOF)'
-
+    eof = 'Ctrl-D (i.e. EOF)'
     builtins.quit = _sitebuiltins.Quitter('quit', eof)
     builtins.exit = _sitebuiltins.Quitter('exit', eof)
 
