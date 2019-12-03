21ed45bafdd60625444b4c55cbacaae0de2bf0ed
Kovid Goyal <kovid@kovidgoyal.net>
2019-12-03 12:02:57 +0530

    Get rid of narrow build codepaths

--- src/calibre/utils/icu_calibre_utils.h
+++ src/calibre/utils/icu_calibre_utils.h
@@ -22,99 +22,9 @@
 #include <unicode/unorm2.h>
 #include <unicode/ubrk.h>
 
-#if PY_VERSION_HEX < 0x03030000 && PY_VERSION_HEX > 0x03000000
-#error Not implemented for python 3.0 to 3.2
+#if PY_VERSION_HEX < 0x03030000
+#error Not implemented for python < 3.3
 #endif
-
-#if PY_VERSION_HEX < 0x03000000
-#define MIN(x, y) ((x)<(y)) ? (x) : (y)
-#define IS_HIGH_SURROGATE(x) (0xd800 <= x && x <= 0xdbff)
-#define IS_LOW_SURROGATE(x) (0xdc00 <= x && x <= 0xdfff)
-
-#ifndef NO_PYTHON_TO_ICU
-static UChar* python_to_icu(PyObject *obj, int32_t *osz) {
-    UChar *ans = NULL;
-    Py_ssize_t sz = 0;
-#ifdef Py_UNICODE_WIDE
-    UErrorCode status = U_ZERO_ERROR;
-#endif
-
-    if (!PyUnicode_CheckExact(obj)) {
-        PyErr_SetString(PyExc_TypeError, "Not a unicode string");
-        goto end;
-    }
-    sz = PyUnicode_GET_SIZE(obj);
-
-#ifdef Py_UNICODE_WIDE
-// wide build (UCS 4)
-    ans = (UChar*) calloc(2*(sz+1), sizeof(UChar)); // There can be no more than 2 UChars per character + ensure null termination
-    if (ans == NULL) { PyErr_NoMemory(); goto end; }
-    u_strFromUTF32WithSub(ans, (int32_t)(2*(sz+1)), osz, (UChar32*)PyUnicode_AS_UNICODE(obj), (int32_t)sz, 0xfffd, NULL, &status);
-    if (U_FAILURE(status)) { PyErr_SetString(PyExc_ValueError, u_errorName(status)); free(ans); ans = NULL; goto end; }
-#else
-// narrow build (UTF-16)
-    ans = (UChar*) malloc((sz + 1) * sizeof(UChar));
-    if (ans == NULL) { PyErr_NoMemory(); goto end; }
-    for (Py_ssize_t i = 0; i < sz; i++) {
-        UChar ch = PyUnicode_AS_UNICODE(obj)[i];
-        if (IS_HIGH_SURROGATE(ch)) {
-            if (i >= sz - 1 || !IS_LOW_SURROGATE(PyUnicode_AS_UNICODE(obj)[i+1])) ans[i] = 0xfffd;
-            else { ans[i] = ch; ans[i+1] = PyUnicode_AS_UNICODE(obj)[i+1]; i++; }
-        } else if (IS_LOW_SURROGATE(ch)) {
-            ans[i] = 0xfffd;
-        } else ans[i] = ch;
-    }
-    ans[sz] = 0; // Ensure null termination
-    if (osz != NULL) *osz = (int32_t)sz;
-#endif
-end:
-    return ans;
-}
-
-#ifndef NO_PYTHON_TO_ICU32
-static UChar32* python_to_icu32(PyObject *obj, int32_t *osz) {
-    UChar32 *ans = NULL;
-    Py_ssize_t sz = 0;
-#ifndef Py_UNICODE_WIDE
-    UErrorCode status = U_ZERO_ERROR;
-#endif
-
-    if (!PyUnicode_CheckExact(obj)) {
-        PyErr_SetString(PyExc_TypeError, "Not a unicode string");
-        goto end;
-    }
-
-    sz = PyUnicode_GET_SIZE(obj);  // number of UCS2 code-points in narrow build and UCS4 code-points in wide build
-    ans = (UChar32*) calloc(sz+1, sizeof(UChar32));  // Ensure null termination
-    if (ans == NULL) { PyErr_NoMemory(); goto end; }
-
-#ifdef Py_UNICODE_WIDE
-// wide build (UCS 4)
-    memcpy(ans, PyUnicode_AS_DATA(obj), MIN((sizeof(UChar32)*(sz+1)),PyUnicode_GET_DATA_SIZE(obj)));
-    if (osz != NULL) *osz = (int32_t)PyUnicode_GET_SIZE(obj);
-#else
-// narrow build (UTF-16)
-    u_strToUTF32(ans, (int32_t)sz + 1, osz, (UChar*)PyUnicode_AS_UNICODE(obj), (int32_t)sz, &status);
-    if (U_FAILURE(status)) { PyErr_SetString(PyExc_ValueError, u_errorName(status)); free(ans); ans = NULL; goto end; }
-#endif
-end:
-    return ans;
-}
-#endif
-
-#endif
-
-#ifndef NO_ICU_TO_PYTHON
-static PyObject* icu_to_python(UChar *src, int32_t sz) {
-#ifdef Py_UNICODE_WIDE
-    return PyUnicode_DecodeUTF16((char*)src, sz*sizeof(UChar), "strict", NULL);
-#else
-    return PyUnicode_FromUnicode((Py_UNICODE*)src, sz);
-#endif
-}
-#endif
-
-#else  // end PY2; start PY3.3+
 
 static UChar* python_to_icu(PyObject *obj, int32_t *osz) {
     UChar *ans = NULL;
@@ -226,5 +136,3 @@ static PyObject* icu_to_python(UChar *src, int32_t sz) {
     return PyUnicode_DecodeUTF16((char*) src, sz * sizeof(UChar), "replace", NULL);
 }
 #endif
-
-#endif  // end PY3.3+
--- src/calibre/utils/icu_test.py
+++ src/calibre/utils/icu_test.py
@@ -96,8 +96,8 @@ class TestICU(unittest.TestCase):
     def test_find(self):
         ' Test searching for substrings '
         self.ae((1, 1), icu.find(b'a', b'1ab'))
-        self.ae((1, 1 if sys.maxunicode >= 0x10ffff else 2), icu.find('\U0001f431', 'x\U0001f431x'))
-        self.ae((1 if sys.maxunicode >= 0x10ffff else 2, 1), icu.find('y', '\U0001f431y'))
+        self.ae((1, 1), icu.find('\U0001f431', 'x\U0001f431x'))
+        self.ae((1, 1), icu.find('y', '\U0001f431y'))
         self.ae((0, 4), icu.primary_find('pena', 'peña'))
         for k, v in iteritems({u'pèché': u'peche', u'flüße':u'Flusse', u'Štepánek':u'ŠtepaneK'}):
             self.ae((1, len(k)), icu.primary_find(v, ' ' + k), 'Failed to find %s in %s' % (v, k))
@@ -170,7 +170,7 @@ class TestICU(unittest.TestCase):
         self.ae(split(u'-one two-'), ['-one', 'two-'])
         self.ae(split(u'-one a-b-c-d e'), ['-one', 'a-b-c-d', 'e'])
         self.ae(split(u'-one -a-b-c-d- e'), ['-one', '-a-b-c-d-', 'e'])
-        self.ae(split_into_words_and_positions('one \U0001f431 three'), [(0, 3), (7 if icu.is_narrow_build else 6, 5)])
+        self.ae(split_into_words_and_positions('one \U0001f431 three'), [(0, 3), (6, 5)])
         self.ae(count_words('a b c d e f'), 6)
         for needle, haystack, pos in (
                 ('word', 'a word b', 2),
@@ -189,7 +189,7 @@ class TestICU(unittest.TestCase):
                 ('i', 'six i', 4),
                 ('i', '', -1), ('', '', -1), ('', 'i', -1),
                 ('i', 'six clicks', -1),
-                ('i', '\U0001f431 i', (3 if icu.is_narrow_build else 2)),
+                ('i', '\U0001f431 i', 2),
                 ('-a', 'b -a', 2),
                 ('a-', 'a-b a- d', 4),
                 ('-a-', 'b -a -a-', 5),
