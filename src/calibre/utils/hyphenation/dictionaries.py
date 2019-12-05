d2171ebe688c1bafee724b68bb57a946fbb3a64f
Kovid Goyal <kovid@kovidgoyal.net>
2019-12-05 12:02:35 +0530

    Remove more __future__ imports

--- src/calibre/utils/hyphenation/dictionaries.py
+++ src/calibre/utils/hyphenation/dictionaries.py
@@ -2,7 +2,6 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import errno
 import json
