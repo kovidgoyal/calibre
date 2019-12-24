5edf26fe70c7c88f14d6099f13b3aafa4f5d965c
Kovid Goyal <kovid@kovidgoyal.net>
2019-12-24 20:01:29 +0530

    Port the commit to master to fix PDF Output with ebook-convert via symlink on macOS

--- bypy/macos/util.h
+++ bypy/macos/util.h
@@ -2,4 +2,4 @@
 #include <wchar.h>
 #include <stdbool.h>
 
-void run(const wchar_t *program, const wchar_t *module, const wchar_t *function, bool is_gui, int argc, char * const *argv);
+void run(const wchar_t *program, const wchar_t *module, const wchar_t *function, bool is_gui, int argc, char * const *argv, char* exe_path);
