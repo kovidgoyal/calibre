b2c4abd9b09ec39d626b632f71568f8059baaf60
Kovid Goyal <kovid@kovidgoyal.net>
2020-01-06 14:16:45 +0530

    Fix #1858284 [Automatic adding dialog has an unnecessary scrollbar](https://bugs.launchpad.net/calibre/+bug/1858284)

--- src/calibre/gui2/widgets2.py
+++ src/calibre/gui2/widgets2.py
@@ -8,17 +8,17 @@ import weakref
 
 from PyQt5.Qt import (
     QAbstractListModel, QApplication, QCheckBox, QColor, QColorDialog, QComboBox,
-    QDialog, QDialogButtonBox, QFont, QIcon, QKeySequence, QLabel, QLayout,
-    QModelIndex, QPalette, QPixmap, QPoint, QPushButton, QRect, QSize, QSizePolicy,
-    QStyle, QStyledItemDelegate, Qt, QTextBrowser, QToolButton, QUndoCommand, QFontInfo,
-    QUndoStack, QWidget, pyqtSignal
+    QDialog, QDialogButtonBox, QFont, QFontInfo, QIcon, QKeySequence, QLabel,
+    QLayout, QModelIndex, QPalette, QPixmap, QPoint, QPushButton, QRect, QScrollArea,
+    QSize, QSizePolicy, QStyle, QStyledItemDelegate, Qt, QTabWidget, QTextBrowser,
+    QToolButton, QUndoCommand, QUndoStack, QWidget, pyqtSignal
 )
 
 from calibre.ebooks.metadata import rating_to_stars
-from calibre.utils.config_base import tweaks
 from calibre.gui2 import gprefs, rating_font
 from calibre.gui2.complete2 import EditWithComplete, LineEdit
 from calibre.gui2.widgets import history
+from calibre.utils.config_base import tweaks
 from polyglot.builtins import unicode_type
 
 
@@ -480,6 +480,30 @@ class HTMLDisplay(QTextBrowser):
                 self.scrollToAnchor(frag)
                 return
         self.anchor_clicked.emit(qurl)
+
+
+class ScrollingTabWidget(QTabWidget):
+
+    def __init__(self, parent=None):
+        QTabWidget.__init__(self, parent)
+
+    def wrap_widget(self, page):
+        sw = QScrollArea(self)
+        sw.setWidget(page)
+        sw.setWidgetResizable(True)
+        page.setAutoFillBackground(False)
+        sw.setStyleSheet('QScrollArea { background: transparent }')
+        return sw
+
+    def indexOf(self, page):
+        for i in range(self.count()):
+            t = self.widget(i)
+            if t.widget() is page:
+                return i
+        return -1
+
+    def addTab(self, page, *args):
+        return QTabWidget.addTab(self, self.wrap_widget(page), *args)
 
 
 if __name__ == '__main__':
