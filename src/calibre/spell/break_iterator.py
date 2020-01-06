1eba328f26b1f2ce4b107b860d424f64bf20434f
Kovid Goyal <kovid@kovidgoyal.net>
2020-01-06 17:52:29 +0530

    DRYer

--- src/calibre/spell/break_iterator.py
+++ src/calibre/spell/break_iterator.py
@@ -14,37 +14,36 @@ _iterators = {}
 _lock = Lock()
 
 
+def get_iterator(lang):
+    it = _iterators.get(lang)
+    if it is None:
+        it = _iterators[lang] = _icu.BreakIterator(_icu.UBRK_WORD, lang_as_iso639_1(lang) or lang)
+    return it
+
+
 def split_into_words(text, lang='en'):
     with _lock:
-        it = _iterators.get(lang, None)
-        if it is None:
-            it = _iterators[lang] = _icu.BreakIterator(_icu.UBRK_WORD, lang_as_iso639_1(lang) or lang)
+        it = get_iterator(lang)
         it.set_text(text)
         return [text[p:p+s] for p, s in it.split2()]
 
 
 def split_into_words_and_positions(text, lang='en'):
     with _lock:
-        it = _iterators.get(lang, None)
-        if it is None:
-            it = _iterators[lang] = _icu.BreakIterator(_icu.UBRK_WORD, lang_as_iso639_1(lang) or lang)
+        it = get_iterator(lang)
         it.set_text(text)
         return it.split2()
 
 
 def index_of(needle, haystack, lang='en'):
     with _lock:
-        it = _iterators.get(lang, None)
-        if it is None:
-            it = _iterators[lang] = _icu.BreakIterator(_icu.UBRK_WORD, lang_as_iso639_1(lang) or lang)
+        it = get_iterator(lang)
         it.set_text(haystack)
         return it.index(needle)
 
 
 def count_words(text, lang='en'):
     with _lock:
-        it = _iterators.get(lang, None)
-        if it is None:
-            it = _iterators[lang] = _icu.BreakIterator(_icu.UBRK_WORD, lang_as_iso639_1(lang) or lang)
+        it = get_iterator(lang)
         it.set_text(text)
         return it.count_words()
