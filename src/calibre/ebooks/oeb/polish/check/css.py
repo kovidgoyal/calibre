6cd6830c1d328d747a4df836ac4dc26d66b9e294
Kovid Goyal <kovid@kovidgoyal.net>
2020-01-08 22:26:42 +0530

    py3ize new file header

--- src/calibre/ebooks/oeb/polish/check/css.py
+++ src/calibre/ebooks/oeb/polish/check/css.py
@@ -1,8 +1,6 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2020, Kovid Goyal <kovid at kovidgoyal.net>
-
-from __future__ import absolute_import, division, print_function, unicode_literals
 
 import atexit
 import json
