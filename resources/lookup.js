f1d94a6ee70e31fc67de728a5f64555401785767
Kovid Goyal <kovid@kovidgoyal.net>
2020-04-16 10:58:56 +0530

    Only move defines to top if we are doing a define

--- resources/lookup.js
+++ resources/lookup.js
@@ -11,14 +11,14 @@
     function fix_google_markup() {
         var cc = document.getElementById('center_col');
         cc.style.marginLeft = '0';
-        cc.style.position = 'absolute';
-        cc.style.top = '0';
-        cc.style.left = '0';
         cc = document.getElementById('cnt');
         if (cc) cc.style.paddingTop = '0';
         var params = new URLSearchParams(document.location.search.substring(1));
         var q = params.get('q');
         if (q && q.startsWith('define:')) {
+            cc.style.position = 'absolute';
+            cc.style.top = '0';
+            cc.style.left = '0';
             var remove = ['sfcnt', 'top_nav', 'before-appbar', 'appbar', 'searchform', 'easter-egg'];
             remove.forEach(function(id) {
                 var elem = document.getElementById(id);
