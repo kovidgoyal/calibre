9af2fea7108b5ffb7a3ac740441a4c7d77d6f7e2
Kovid Goyal <kovid@kovidgoyal.net>
2020-07-10 12:28:22 +0530

    Allow easily reading viewer config data from the main calibre process

---
+++ src/calibre/gui2/viewer/config.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python2
+# vim:fileencoding=utf-8
+# License: GPL v3 Copyright: 2020, Kovid Goyal <kovid at kovidgoyal.net>
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import os
+from calibre.constants import config_dir
+from calibre.utils.config import JSONConfig
+
+vprefs = JSONConfig('viewer-webengine')
+viewer_config_dir = os.path.join(config_dir, 'viewer')
+vprefs.defaults['session_data'] = {}
+vprefs.defaults['local_storage'] = {}
+vprefs.defaults['main_window_state'] = None
+vprefs.defaults['main_window_geometry'] = None
+vprefs.defaults['old_prefs_migrated'] = False
+vprefs.defaults['bookmarks_sort'] = 'title'
