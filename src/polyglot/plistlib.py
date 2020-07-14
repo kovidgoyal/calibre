4ab24b8ea2f97a711c6c2c5f1d9cd9e15f2fc774
Kovid Goyal <kovid@kovidgoyal.net>
2020-07-14 09:16:25 +0530

    Remove wrap/unwrap for bytes in plistlib not needed on py3

--- src/polyglot/plistlib.py
+++ src/polyglot/plistlib.py
@@ -2,11 +2,5 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
-def unwrap_bytes(x):
-    return x
-
-def wrap_bytes(x):
-    return x
-
 from plistlib import loads, dumps, Data  # noqa
 loads_binary_or_xml = loads
