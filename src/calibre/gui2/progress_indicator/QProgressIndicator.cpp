94479386d97b61b3d1340149db35c1bac6ca4c45
Kovid Goyal <kovid@kovidgoyal.net>
2020-09-10 07:41:43 +0530

    calibre style: Reduce indentation in tree views

--- src/calibre/gui2/progress_indicator/QProgressIndicator.cpp
+++ src/calibre/gui2/progress_indicator/QProgressIndicator.cpp
@@ -196,6 +196,8 @@ class CalibreStyle: public QProxyStyle {
             switch (metric) {
                 case PM_TabBarTabVSpace:
                     return 8;  // Make tab bars a little narrower, the value for the Fusion style is 12
+				case PM_TreeViewIndentation:
+					return 10;  // Reduce indentation in tree views
                 default:
                     break;
             }
