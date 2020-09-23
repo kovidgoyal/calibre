3a9435e537fe5fd99ea6ef2d89f0fb1d61944b2d
Charles Haley <cbhaley@i.wont.say.com>
2020-09-23 16:02:35 +0100

    Some tests for the search template stuff

--- src/calibre/db/search.py
+++ src/calibre/db/search.py
@@ -639,7 +639,7 @@ class Parser(SearchQueryParser):  # {{{
                 if DEBUG:
                     import traceback
                     traceback.print_exc()
-                raise ParseException(_('template: missing or invalid separator (#@[tdnb]?#)'))
+                raise ParseException(_('template: missing or invalid separator (#@[tdnb]#:)'))
             matchkind, query = _matchkind(query, case_sensitive=case_sensitive)
             matches = set()
             error_string = '*@*TEMPLATE_ERROR*@*'
