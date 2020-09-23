740bfd26db315cf471d43940d965159bc0e5dc08
Charles Haley <cbhaley@i.wont.say.com>
2020-09-23 11:37:26 +0100

    Enhancement: allow using templares in search strings

--- src/calibre/gui2/dialogs/search.py
+++ src/calibre/gui2/dialogs/search.py
@@ -235,6 +235,46 @@ def create_date_tab(self, db):
     l.addStretch(10)
 
 
+def create_template_tab(self):
+    self.simple_tab = w = QWidget(self.tab_widget)
+    self.tab_widget.addTab(w, _("&Template search"))
+
+    w.l = l = QFormLayout(w)
+    l.setFieldGrowthPolicy(l.AllNonFixedFieldsGrow)
+
+    self.template_value_box = le = QLineEdit(w)
+    le.setObjectName('template_value_box')
+    le.setPlaceholderText(_('The value to search for'))
+    le.setToolTip('<p>' +
+                  _("You can use the search test specifications described "
+                    "in the calibre documentation. For example, with Number "
+                    "comparisons you can the relational operators like '>=' etc. "
+                    "With Text comparisons you can use exact, comtains "
+                    "or regular expression matches. With Date you can use "
+                    "today, yesterday, etc. Set/not set takes 'true' for set "
+                    "and 'false' for not set.")
+                  + '</p>')
+    l.addRow(_('Template &value:'), le)
+
+    self.template_test_type_box = le = QComboBox(w)
+    le.setObjectName('template_test_type_box')
+    for op, desc in [
+            ('t', _('Text')),
+            ('d', _('Date')),
+            ('n', _('Number')),
+            ('b', _('Set/Not Set'))]:
+        le.addItem(desc, op)
+    le.setToolTip(_('How the template result will be compared to the value'))
+    l.addRow(_('C&omparison type:'), le)
+
+    from calibre.gui2.dialogs.template_line_editor import TemplateLineEditor
+    self.template_program_box = le = TemplateLineEditor(self.tab_widget)
+    le.setObjectName('template_program_box')
+    le.setPlaceholderText(_('The template that generates the value'))
+    le.setToolTip(_('Right click to open a template editor'))
+    l.addRow(_('Tem&plate:'), le)
+
+
 def setup_ui(self, db):
     self.setWindowTitle(_("Advanced search"))
     self.setWindowIcon(QIcon(I('search.png')))
@@ -251,6 +291,7 @@ def setup_ui(self, db):
     create_adv_tab(self)
     create_simple_tab(self, db)
     create_date_tab(self, db)
+    create_template_tab(self)
 # }}}
 
 
@@ -270,6 +311,13 @@ class SearchDialog(QDialog):
             w = getattr(self, focused_field, None)
             if w is not None:
                 w.setFocus(Qt.OtherFocusReason)
+        elif current_tab == 3:
+            self.template_program_box.setText(
+                      gprefs.get('advanced_search_template_tab_program_field', ''))
+            self.template_value_box.setText(
+                      gprefs.get('advanced_search_template_tab_value_field', ''))
+            self.template_test_type_box.setCurrentIndex(
+                      int(gprefs.get('advanced_search_template_tab_test_field', '0')))
         self.resize(self.sizeHint())
 
     def save_state(self):
@@ -279,6 +327,13 @@ class SearchDialog(QDialog):
             fw = self.tab_widget.focusWidget()
             if fw:
                 gprefs.set('advanced_search_simple_tab_focused_field', fw.objectName())
+        elif self.tab_widget.currentIndex() == 3:
+            gprefs.set('advanced_search_template_tab_program_field',
+                       unicode_type(self.template_program_box.text()))
+            gprefs.set('advanced_search_template_tab_value_field',
+                       unicode_type(self.template_value_box.text()))
+            gprefs.set('advanced_search_template_tab_test_field',
+                       unicode_type(self.template_test_type_box.currentIndex()))
 
     def accept(self):
         self.save_state()
@@ -290,9 +345,9 @@ class SearchDialog(QDialog):
 
     def clear_button_pushed(self):
         w = self.tab_widget.currentWidget()
+        for c in w.findChildren(QComboBox):
+            c.setCurrentIndex(0)
         if w is self.date_tab:
-            for c in w.findChildren(QComboBox):
-                c.setCurrentIndex(0)
             for c in w.findChildren(QSpinBox):
                 c.setValue(c.minimum())
             self.sel_date.setChecked(True)
@@ -312,7 +367,18 @@ class SearchDialog(QDialog):
 
     def search_string(self):
         i = self.tab_widget.currentIndex()
-        return (self.adv_search_string, self.box_search_string, self.date_search_string)[i]()
+        return (self.adv_search_string, self.box_search_string,
+                self.date_search_string, self.template_search_string)[i]()
+
+    def template_search_string(self):
+        template = unicode_type(self.template_program_box.text())
+        value = unicode_type(self.template_value_box.text()).replace('"', '\\"')
+        if template and value:
+            cb = self.template_test_type_box
+            op =  unicode_type(cb.itemData(cb.currentIndex()))
+            l = '{0}#@{1}#:{2}'.format(template, op, value)
+            return 'template:"' + l + '"'
+        return ''
 
     def date_search_string(self):
         field = unicode_type(self.date_field.itemData(self.date_field.currentIndex()) or '')
