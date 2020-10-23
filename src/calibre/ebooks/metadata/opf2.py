b0e276435f62b4866aca908a63a40c08e3d11ffc
Kovid Goyal <kovid@kovidgoyal.net>
2020-10-23 09:38:05 +0530

    Fix setting rating via ebook-meta command line tool broken in calibre 5. Fixes #1901113 [ebook-meta --rating fails](https://bugs.launchpad.net/calibre/+bug/1901113)
    More python 3 chicanery

--- src/calibre/ebooks/metadata/opf2.py
+++ src/calibre/ebooks/metadata/opf2.py
@@ -1313,6 +1313,8 @@ class OPF(object):  # {{{
                      'isbn', 'tags', 'category', 'comments', 'book_producer',
                      'pubdate', 'user_categories', 'author_link_map'):
             val = getattr(mi, attr, None)
+            if attr == 'rating' and val:
+                val = float(val)
             is_null = val is None or val in ((), [], (None, None), {}) or (attr == 'rating' and val < 0.1)
             if is_null:
                 if apply_null and attr in {'series', 'tags', 'isbn', 'comments', 'publisher', 'rating'}:
