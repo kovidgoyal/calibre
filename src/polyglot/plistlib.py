238680060c9ab66f87b2d4f4bddc033083b2af4d
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2020-11-02 07:17:07 +0700

    polyglot: don't try to import plistlib.Data
    plistlib.Data is deprecated from Python 3.4 and fully removed in Python
    3.9.
    
    Skimming over the code, it's look like nothing uses this data structure.
    
    Let's remove it.

--- src/polyglot/plistlib.py
+++ src/polyglot/plistlib.py
@@ -2,5 +2,5 @@
 # vim:fileencoding=utf-8
 # License: GPL v3 Copyright: 2019, Kovid Goyal <kovid at kovidgoyal.net>
 
-from plistlib import loads, dumps, Data  # noqa
+from plistlib import loads, dumps  # noqa
 loads_binary_or_xml = loads
