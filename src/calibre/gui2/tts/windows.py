a50c2db7ae958462161334a1e91bcbdc73bb88b8
Kovid Goyal <kovid@kovidgoyal.net>
2020-12-07 21:13:07 +0530

    Start work on windows tts config

--- src/calibre/gui2/tts/windows.py
+++ src/calibre/gui2/tts/windows.py
@@ -19,15 +19,20 @@ class Client:
     def escape_marked_text(cls, text):
         return prepare_string_for_xml(text)
 
-    def __init__(self, settings, dispatch_on_main_thread):
+    def __init__(self, settings=None, dispatch_on_main_thread=lambda f: f()):
         from calibre.utils.windows.winsapi import ISpVoice
-        self.sp_voice = ISpVoice()
         self.events_thread = Thread(name='SAPIEvents', target=self.wait_for_events, daemon=True)
-        self.events_thread.start()
+        self.sp_voice = ISpVoice()
+        self.default_system_rate = self.sp_voice.get_current_rate()
+        self.default_system_voice = self.sp_voice.get_current_voice()
+        self.default_system_sound_output = self.sp_voice.get_current_sound_output()
         self.current_stream_number = None
         self.current_callback = None
         self.dispatch_on_main_thread = dispatch_on_main_thread
+        self.current_marked_text = self.last_mark = None
         self.status = {'synthesizing': False, 'paused': False}
+        self.events_thread.start()
+        self.apply_settings(settings)
 
     def __del__(self):
         if self.sp_voice is not None:
@@ -35,6 +40,12 @@ class Client:
             self.events_thread.join(5)
             self.sp_voice = None
     shutdown = __del__
+
+    def apply_settings(self, new_settings=None):
+        settings = new_settings or {}
+        self.sp_voice.set_current_rate(settings.get('rate', self.default_system_rate))
+        self.sp_voice.set_current_voice(settings.get('voice') or self.default_system_voice)
+        self.sp_voice.set_current_sound_output(settings.get('sound_output') or self.default_system_sound_output)
 
     def wait_for_events(self):
         while True:
@@ -49,6 +60,7 @@ class Client:
         c = self.current_callback
         for (stream_number, event_type, event_data) in self.sp_voice.get_events():
             if event_type == SPEI_TTS_BOOKMARK:
+                self.last_mark = event_data
                 event = Event(EventType.mark, event_data)
             elif event_type == SPEI_START_INPUT_STREAM:
                 event = Event(EventType.begin)
@@ -70,14 +82,20 @@ class Client:
             SPF_ASYNC, SPF_IS_NOT_XML, SPF_PURGEBEFORESPEAK
         )
         self.current_callback = None
+        self.current_marked_text = self.last_mark = None
         self.current_stream_number = self.sp_voice.speak(text, SPF_ASYNC | SPF_PURGEBEFORESPEAK | SPF_IS_NOT_XML, True)
 
-    def speak_marked_text(self, text, callback):
+    def speak_xml(self, text):
         from calibre_extensions.winsapi import (
             SPF_ASYNC, SPF_IS_XML, SPF_PURGEBEFORESPEAK
         )
-        self.current_callback = callback
         self.current_stream_number = self.sp_voice.speak(text, SPF_ASYNC | SPF_PURGEBEFORESPEAK | SPF_IS_XML, True)
+
+    def speak_marked_text(self, text, callback):
+        self.current_marked_text = text
+        self.last_mark = None
+        self.current_callback = callback
+        self.speak_xml(text)
 
     def stop(self):
         from calibre_extensions.winsapi import SPF_PURGEBEFORESPEAK
@@ -102,3 +120,32 @@ class Client:
             self.status = {'synthesizing': True, 'paused': False}
             if self.current_callback is not None:
                 self.current_callback(Event(EventType.resume))
+
+    def resume_after_configure(self):
+        if self.status['paused']:
+            mark = self.mark_template.format(self.last_mark)
+            idx = self.current_marked_text.find(mark)
+            if idx == -1:
+                text = self.current_marked_text
+            else:
+                text = self.current_marked_text[idx:]
+            self.speak_xml(text)
+            self.status = {'synthesizing': True, 'paused': False}
+            if self.current_callback is not None:
+                self.current_callback(Event(EventType.resume))
+
+    def get_voice_data(self):
+        ans = getattr(self, 'voice_data', None)
+        if ans is None:
+            ans = self.voice_data = self.sp_voice.get_all_voices()
+        return ans
+
+    def get_sound_outputs(self):
+        ans = getattr(self, 'sound_outputs', None)
+        if ans is None:
+            ans = self.sound_outputs = self.sp_voice.get_all_sound_outputs()
+        return ans
+
+    def config_widget(self, backend_settings, parent):
+        from calibre.gui2.tts.windows_config import Widget
+        return Widget(self, backend_settings, parent)
