56e69950c182049518297a21e212ad4650ed5b92
Kovid Goyal <kovid@kovidgoyal.net>
2020-12-07 13:35:47 +0530

    Get rid of cocoa_wrapper.c

--- src/calibre/utils/cocoa.m
+++ src/calibre/utils/cocoa.m
@@ -10,14 +10,17 @@
 #import <Availability.h>
 
 #include <string.h>
+#include <Python.h>
 
-void
+extern int nsss_init_module(PyObject*);
+
+static void
 disable_window_tabbing(void) {
 	if ([NSWindow respondsToSelector:@selector(allowsAutomaticWindowTabbing)])
         NSWindow.allowsAutomaticWindowTabbing = NO;
 }
 
-void
+static void
 remove_cocoa_menu_items(void) {
 	// Remove (disable) the "Start Dictation..." and "Emoji & Symbols" menu
 	// items from the "Edit" menu
@@ -28,31 +31,56 @@ remove_cocoa_menu_items(void) {
 	[[NSUserDefaults standardUserDefaults] setBool:NO forKey:@"NSFullScreenMenuItemEverywhere"];
 }
 
-void
-activate_cocoa_multithreading(void) {
+static PyObject*
+disable_cocoa_ui_elements(PyObject *self, PyObject *args) {
+	PyObject *tabbing = Py_True, *menu_items = Py_True;
+	if (!PyArg_ParseTuple(args, "|OO", &tabbing, &menu_items)) return NULL;
+	if (PyObject_IsTrue(tabbing)) disable_window_tabbing();
+	if (PyObject_IsTrue(menu_items)) remove_cocoa_menu_items();
+	Py_RETURN_NONE;
+}
+
+
+static PyObject*
+enable_cocoa_multithreading(PyObject *self, PyObject *args) {
 	if (![NSThread isMultiThreaded]) [[NSThread new] start];
+	Py_RETURN_NONE;
 }
 
-const char*
-cocoa_send2trash(const char *utf8_path) {
+
+static PyObject*
+send2trash(PyObject *self, PyObject *args) {
+	(void)self;
+	char *path = NULL;
+    if (!PyArg_ParseTuple(args, "s", &path)) return NULL;
 	@autoreleasepool {
-	const char *ret = NULL;
-	NSError* ns_error = nil;
-	if (![[NSFileManager defaultManager] trashItemAtURL:[NSURL fileURLWithPath:@(utf8_path)] resultingItemURL:nil error:&ns_error]) {
-		ret = strdup([[ns_error localizedDescription] UTF8String]);
+		NSError* ns_error = nil;
+		if (![[NSFileManager defaultManager] trashItemAtURL:[NSURL fileURLWithPath:@(path)] resultingItemURL:nil error:&ns_error]) {
+			PyErr_SetString(PyExc_OSError, [[ns_error localizedDescription] UTF8String]);
+		}
 	}
-	return ret;
+	if (PyErr_Occurred()) return NULL;
+	Py_RETURN_NONE;
+}
+
+// Notifications {{{
+static PyObject *notification_activated_callback = NULL;
+
+static void
+macos_notification_callback(const char* user_id) {
+	if (notification_activated_callback) {
+		PyObject *ret = PyObject_CallFunction(notification_activated_callback, "z", user_id);
+		if (ret == NULL) PyErr_Print();
+		else Py_DECREF(ret);
 	}
 }
 
-
-extern void macos_notification_callback(const char*);
 
 @interface NotificationDelegate : NSObject <NSUserNotificationCenterDelegate>
 @end
 
 
-void
+static void
 cocoa_send_notification(const char *identifier, const char *title, const char *subtitle, const char *informativeText, const char* path_to_image) {
 	@autoreleasepool {
     NSUserNotificationCenter *center = [NSUserNotificationCenter defaultUserNotificationCenter];
@@ -100,9 +128,31 @@ cocoa_send_notification(const char *identifier, const char *title, const char *s
     }
 @end
 
+static PyObject*
+set_notification_activated_callback(PyObject *self, PyObject *callback) {
+    (void)self;
+    if (notification_activated_callback) Py_DECREF(notification_activated_callback);
+    notification_activated_callback = callback;
+    Py_INCREF(callback);
+    Py_RETURN_NONE;
 
-double
-cocoa_cursor_blink_time(void) {
+}
+
+static PyObject*
+send_notification(PyObject *self, PyObject *args) {
+	(void)self;
+    char *identifier = NULL, *title = NULL, *subtitle = NULL, *informativeText = NULL, *path_to_image = NULL;
+    if (!PyArg_ParseTuple(args, "zsz|zz", &identifier, &title, &informativeText, &path_to_image, &subtitle)) return NULL;
+	cocoa_send_notification(identifier, title, subtitle, informativeText, path_to_image);
+
+    Py_RETURN_NONE;
+}
+// }}}
+
+
+static PyObject*
+cursor_blink_time(PyObject *self) {
+    (void)self;
     NSUserDefaults *defaults = [NSUserDefaults standardUserDefaults];
     double on_period_ms = [defaults doubleForKey:@"NSTextInsertionPointBlinkPeriodOn"];
     double off_period_ms = [defaults doubleForKey:@"NSTextInsertionPointBlinkPeriodOff"];
@@ -113,10 +163,42 @@ cocoa_cursor_blink_time(void) {
     } else if (period_ms) {
         ans = period_ms;
     }
-    return ans > max_value ? 0.0 : ans;
+	if (ans > max_value) ans = 0.0;
+    return PyFloat_FromDouble(ans);
 }
 
-int
-cocoa_transient_scroller(void) {
-    return [NSScroller preferredScrollerStyle] == NSScrollerStyleOverlay;
+static PyObject*
+transient_scroller(PyObject *self) {
+    (void)self;
+    return PyBool_FromLong([NSScroller preferredScrollerStyle] == NSScrollerStyleOverlay);
+}
+
+static PyMethodDef module_methods[] = {
+    {"transient_scroller", (PyCFunction)transient_scroller, METH_NOARGS, ""},
+    {"cursor_blink_time", (PyCFunction)cursor_blink_time, METH_NOARGS, ""},
+    {"enable_cocoa_multithreading", (PyCFunction)enable_cocoa_multithreading, METH_NOARGS, ""},
+    {"set_notification_activated_callback", (PyCFunction)set_notification_activated_callback, METH_O, ""},
+    {"send_notification", (PyCFunction)send_notification, METH_VARARGS, ""},
+    {"disable_cocoa_ui_elements", (PyCFunction)disable_cocoa_ui_elements, METH_VARARGS, ""},
+    {"send2trash", (PyCFunction)send2trash, METH_VARARGS, ""},
+    {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+static int
+exec_module(PyObject *module) {
+	if (nsss_init_module(module) == -1) return -1;
+	return 0;
+}
+
+static PyModuleDef_Slot slots[] = { {Py_mod_exec, exec_module}, {0, NULL} };
+
+static struct PyModuleDef module_def = {
+    .m_base     = PyModuleDef_HEAD_INIT,
+    .m_name     = "cocoa",
+    .m_methods  = module_methods,
+    .m_slots    = slots,
+};
+
+CALIBRE_MODINIT_FUNC PyInit_cocoa(void) {
+	return PyModuleDef_Init(&module_def);
 }
