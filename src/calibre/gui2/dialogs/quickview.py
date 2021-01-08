84b0c4a3180c2660703a37d6dcdee8aaa1d68bcb
Charles Haley <cbhaley@i.wont.say.com>
2021-01-08 10:27:31 +0000

    Bug #1910662: Composite column shows as one string

--- src/calibre/gui2/dialogs/quickview.py
+++ src/calibre/gui2/dialogs/quickview.py
@@ -515,7 +515,9 @@ class Quickview(QDialog, Ui_Quickview):
 
         mi = self.db.get_metadata(book_id, index_is_id=True, get_user_categories=False)
         vals = mi.get(key, None)
-
+        if self.fm[key]['datatype'] == 'composite' and self.fm[key]['is_multiple']:
+            sep = self.fm[key]['is_multiple'].get('cache_to_list', ',')
+            vals = [v.strip() for v in vals.split(sep) if v.strip()]
         try:
             # Check if we are in the GridView and there are no values for the
             # selected column. In this case switch the column to 'authors'
