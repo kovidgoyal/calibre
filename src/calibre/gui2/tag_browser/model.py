7eb7cc4971eaab5a3830913ff08c76f731230855
Charles Haley <cbhaley@i.wont.say.com>
2021-02-02 10:55:01 +0000

    Bug #1914207: Average rating tooltip shows on hierarchical search Edit

--- src/calibre/gui2/tag_browser/model.py
+++ src/calibre/gui2/tag_browser/model.py
@@ -216,7 +216,7 @@ class TagTreeItem(object):  # {{{
                 ar = self.average_rating
                 if ar:
                     tt.append(_('Average rating for books in this category: %.1f') % ar)
-                elif self.type == self.TAG and ar is not None:
+                elif self.type == self.TAG and ar is not None and self.tag.category != 'search':
                     tt.append(_('Books in this category are unrated'))
                 if self.type == self.TAG and self.tag.category == 'search':
                     tt.append(_('Search expression:') + ' ' + self.tag.search_expression)
