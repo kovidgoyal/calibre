65eda67d7c5f8d7cea89050cb4277336aebf2a14
Kovid Goyal <kovid@kovidgoyal.net>
2021-02-23 09:59:16 +0530

    Python 3.7 compat

--- setup/translations.py
+++ setup/translations.py
@@ -159,7 +159,7 @@ class POT(Command):  # {{{
                             f.seek(0), f.truncate(), f.write('\n'.join(lines).encode('utf-8'))
                             break
                     else:
-                        raise SystemExit(f'Failed to add file_filter for {slug=} to config file')
+                        raise SystemExit(f'Failed to add file_filter for slug={slug} to config file')
                 self.git('add .tx/config')
             self.upload_pot(resource=slug)
             self.git(['add', dest])
