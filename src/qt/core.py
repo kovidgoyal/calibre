203707010d8b29ab3535558df3b7c65b9dc9983e
Kovid Goyal <kovid@kovidgoyal.net>
2021-03-01 14:08:56 +0530

    Shortcut module to ease importing PyQt names

---
+++ src/qt/core.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPL v3 Copyright: 2021, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+from .loader import dynamic_load
+from .core_name_map import name_map
+
+already_imported = {}
+qt_modules = {}
+
+
+def __getattr__(name):
+    return dynamic_load(name, name_map, already_imported, qt_modules)
---
+++ src/qt/loader.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# vim:fileencoding=utf-8
+# License: GPL v3 Copyright: 2021, Kovid Goyal <kovid at kovidgoyal.net>
+
+from importlib import import_module
+import sys
+
+
+def dynamic_load(name, name_map, already_imported, qt_modules):
+    ans = already_imported.get(name, already_imported)
+    if ans is not already_imported:
+        return ans
+    mod_name = name_map.get(name)
+    if mod_name is not None:
+        mod = qt_modules.get(mod_name)
+        if mod is None:
+            try:
+                mod = qt_modules[mod_name] = import_module(mod_name)
+            except ImportError as err:
+                mod = qt_modules[mod_name] = False
+                print(
+                    'Failed to import PyQt module:',
+                    mod_name,
+                    'with error:',
+                    err,
+                    file=sys.stderr
+                )
+        if mod is not False:
+            q = getattr(mod, name, qt_modules)
+            if q is not qt_modules:
+                already_imported[name] = q
+                return q
+    raise AttributeError(f'The object {name} is not a known Qt object')
