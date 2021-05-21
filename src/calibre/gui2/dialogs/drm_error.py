4f6763624b7d5068cba6509777adf148bd50eea4
Kovid Goyal <kovid@kovidgoyal.net>
2021-05-21 07:07:19 +0530

    Make link in DRM error dialog localized

--- src/calibre/gui2/dialogs/drm_error.py
+++ src/calibre/gui2/dialogs/drm_error.py
@@ -8,8 +8,9 @@ __docformat__ = 'restructuredtext en'
 
 
 from qt.core import QDialog
+
 from calibre.gui2.dialogs.drm_error_ui import Ui_Dialog
-from polyglot.builtins import unicode_type
+from calibre.utils.localization import localize_website_link
 
 
 class DRMErrorMessage(QDialog, Ui_Dialog):
@@ -17,7 +18,19 @@ class DRMErrorMessage(QDialog, Ui_Dialog):
     def __init__(self, parent=None, title=None):
         QDialog.__init__(self, parent)
         self.setupUi(self)
+        msg = _('<p>This book is locked by <b>DRM</b>. To learn more about DRM'
+                ' and why you cannot read or convert this book in calibre,'
+                ' <a href="{0}">click here</a>.'
+                ' </p>').format(localize_website_link('https://manual.calibre-ebook.com/drm.html'))
         if title is not None:
-            t = unicode_type(self.msg.text())
-            self.msg.setText('<h2>%s</h2>%s'%(title, t))
+            msg = '<h2>%s</h2>%s'%(title, msg)
+        self.msg.setText(msg)
         self.resize(self.sizeHint())
+
+
+if __name__ == '__main__':
+    from calibre.gui2 import Application
+    app = Application([])
+    d = DRMErrorMessage(title='testing title')
+    d.exec_()
+    del d
