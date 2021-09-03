ed32130ca4cbbf235f5560e7c53f78c01eab1106
Kovid Goyal <kovid@kovidgoyal.net>
2021-09-03 07:23:09 +0530

    oops

--- src/calibre/utils/formatter.py
+++ src/calibre/utils/formatter.py
@@ -926,7 +926,7 @@ class _Interpreter(object):
     def do_node_first_non_empty(self, prog):
         for expr in prog.expression_list:
             v = self.expr(expr)
-            if v == self.expr(expr):
+            if v:
                 if self.break_reporter:
                     self.break_reporter(prog.node_name, v, prog.line_number)
                 return v
