kovidgoyal/calibre

commit bde8cd5e65984786471851309bd9c95be1f354a5

1) Fix regression in template language: at some point in V5 the template language was changed to disallow using an expression_list as a parameter. This was allowed in previous major revisions. 2) Allow expression lists in parameterized expressions. 3) Correct the grammar in the documentation.

src/calibre/utils/formatter.py
@@ -568,7 +568,7 @@ class _Parser(object):
     def expr(self):
         if self.token_op_is_lparen():
             self.consume()
-            rv = self.top_expr()
+            rv = self.expression_list()
             if not self.token_op_is_rparen():
                 self.error(_('Missing )'))
             self.consume()
@@ -598,7 +598,7 @@ class _Parser(object):
             arguments = list()
             while not self.token_op_is_rparen():
                 # evaluate the expression (recursive call)
-                arguments.append(self.top_expr())
+                arguments.append(self.expression_list())
                 if not self.token_op_is_comma():
                     break
                 self.consume()
@@ -906,6 +906,8 @@ class _Interpreter(object):
 
     def expr(self, prog):
         try:
+            if isinstance(prog, list):
+                return self.expression_list(prog)
             return self.NODE_OPS[prog.node_type](self, prog)
         except ValueError as e:
             raise e

commit 22b32638e83b0595f47d4b3cfb7cf11f6fbd5b17

Fix parsing of assign() template function
This fixes a regression from commit
bde8cd5e65984786471851309bd9c95be1f354a5, which allowed expression lists
as function arguments. However, assign() still expects simple expression
as its first argument. I don't think allowing expression list there
makes much sense, so let's restrict this.

src/calibre/utils/formatter.py
@@ -540,8 +540,8 @@ class _Parser:
                              lambda ln, args: IfNode(ln, args[0], (args[1],), (args[2],))),
         'first_non_empty':  (lambda args: len(args) == 1,
                              lambda ln, args: FirstNonEmptyNode(ln, args)),
-        'assign':           (lambda args: len(args) == 2 and args[0].node_type == Node.NODE_RVALUE,
-                             lambda ln, args: AssignNode(ln, args[0].name, args[1])),
+        'assign':           (lambda args: len(args) == 2 and len(args[0]) == 1 and args[0][0].node_type == Node.NODE_RVALUE,
+                             lambda ln, args: AssignNode(ln, args[0][0].name, args[1])),
         'contains':         (lambda args: len(args) == 4,
                              lambda ln, args: ContainsNode(ln, args)),
         'character':        (lambda args: len(args) == 1,