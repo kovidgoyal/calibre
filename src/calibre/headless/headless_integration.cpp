30761d8a4d02a4790e3fd84c06b7dce9fcbbcaf0
Kovid Goyal <kovid@kovidgoyal.net>
2021-11-22 10:11:18 +0530

    Update comment

--- src/calibre/headless/headless_integration.cpp
+++ src/calibre/headless/headless_integration.cpp
@@ -32,10 +32,8 @@ class GenericUnixServices : public QGenericUnixServices {
      * leading to a segfault.  For example, defaultHintStyleFromMatch() queries
      * the nativeInterface() without checking that it is NULL. See
      * https://bugreports.qt-project.org/browse/QTBUG-40946
-     * This is no longer strictly necessary since we implement our own fontconfig database
-     * (a patched version of the Qt fontconfig database). However, it is probably a good idea to
-     * keep it unknown, since the headless QPA is used in contexts where a desktop environment
-     * does not make sense anyway.
+     *
+     * Also for the portal BS, we need to ignore openUrl and openDocument
      */
     QByteArray desktopEnvironment() const { return QByteArrayLiteral("UNKNOWN"); }
 	bool openUrl(const QUrl &url) { Q_UNUSED(url); return false; }
