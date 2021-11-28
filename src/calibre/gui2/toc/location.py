709e4b44ab37877ae56c89780fa6e43a51cc9a59
Kovid Goyal <kovid@kovidgoyal.net>
2021-11-28 13:40:22 +0530

    Use native webengine dark mode support in the toc and preview tools

--- src/calibre/gui2/toc/location.py
+++ src/calibre/gui2/toc/location.py
@@ -49,8 +49,6 @@ class Page(QWebEnginePage):  # {{{
             else:
                 settings = {}
         js = js.replace('SETTINGS', json.dumps(settings), 1)
-        dark_mode_css = P('dark_mode.css', data=True, allow_user_override=False).decode('utf-8')
-        js = js.replace('CSS', json.dumps(dark_mode_css), 1)
         s.setSourceCode(js)
         self.scripts().insert(s)
 
