075eb30b7958c97dd891dfe51caa8964394a1785
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-19 10:42:28 +0530

    Delay load hunspell

--- src/calibre/spell/dictionary.py
+++ src/calibre/spell/dictionary.py
@@ -7,7 +7,6 @@ import os
 import re
 import shutil
 import sys
-from calibre_extensions import hunspell
 from collections import defaultdict, namedtuple
 from functools import partial
 from itertools import chain
@@ -164,6 +163,7 @@ def get_dictionary(locale, exact_match=False):
 
 
 def load_dictionary(dictionary):
+    from calibre_extensions import hunspell
 
     def fix_path(path):
         if isinstance(path, bytes):
@@ -456,3 +456,12 @@ def find_tests():
             self.assertIn('List', self.suggestions('Lis𝑘t'))
 
     return unittest.TestLoader().loadTestsFromTestCase(TestDictionaries)
+
+
+def test():
+    from calibre.utils.run_tests import run_cli
+    run_cli(find_tests())
+
+
+if __name__ == '__main__':
+    test()
