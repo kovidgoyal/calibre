ad78f1bddca9395840529f6e2bf8963ac1ff7b23
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-23 14:48:52 +0530

    Use Qt 6.2 src on CI

--- setup/unix-ci.py
+++ setup/unix-ci.py
@@ -91,7 +91,7 @@ def download_and_decompress(url, dest, compression=None):
 def install_qt_source_code():
     dest = os.path.expanduser('~/qt-base')
     os.mkdir(dest)
-    download_and_decompress('https://download.calibre-ebook.com/qtbase-everywhere-src-5.15.2.tar.xz', dest, 'J')
+    download_and_decompress('https://download.calibre-ebook.com/qtbase-everywhere-src-6.2.2.tar.xz', dest, 'J')
     qdir = glob.glob(dest + '/*')[0]
     os.environ['QT_SRC'] = qdir
 
