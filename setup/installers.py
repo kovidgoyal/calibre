c9c4a29af1612eab6fe03f61ac25eb2037ba411c
Kovid Goyal <kovid@kovidgoyal.net>
2021-12-31 10:10:39 +0530

    oops

--- setup/installers.py
+++ setup/installers.py
@@ -149,7 +149,7 @@ class BuildInstaller(Command):
 class BuildInstallers(BuildInstaller):
 
     OS = ''
-    ALL_ARCHES = '64'
+    ALL_ARCHES = '64',
 
     def run(self, opts):
         for bitness in self.ALL_ARCHES:
