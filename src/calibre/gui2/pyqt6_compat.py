f2c058fea33a0355c783879df1fe49c43a47ccbb
Kovid Goyal <kovid@kovidgoyal.net>
2022-01-01 16:23:48 +0530

    Also enumify QMessgaeBox

--- src/calibre/gui2/pyqt6_compat.py
+++ src/calibre/gui2/pyqt6_compat.py
@@ -60,7 +60,7 @@ QFontMetrics.width = lambda self, text: self.horizontalAdvance(text)
 # Restore enum values to various classes
 for cls in (
     Qt, QDialog, QToolButton, QAbstractItemView, QDialogButtonBox, QFrame, QComboBox,
-    QLineEdit, QAction, QImage, QIODevice, QPalette, QFormLayout, QEvent
+    QLineEdit, QAction, QImage, QIODevice, QPalette, QFormLayout, QEvent, QMessageBox
 ):
     for var in tuple(vars(cls).values()):
         m = getattr(var, '__members__', {})
