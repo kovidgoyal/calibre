307cfea17100104024e87bcf7c371c47212ec4a4
Kovid Goyal <kovid@kovidgoyal.net>
2022-01-03 13:15:10 +0530

    bump beta version

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -4,7 +4,7 @@ from polyglot.builtins import environ_item, hasenv
 import sys, locale, codecs, os, collections, collections.abc
 
 __appname__   = 'calibre'
-numeric_version = (5, 99, 0)
+numeric_version = (5, 99, 1)
 __version__   = '.'.join(map(str, numeric_version))
 git_version   = None
 __author__    = "Kovid Goyal <kovid@kovidgoyal.net>"
