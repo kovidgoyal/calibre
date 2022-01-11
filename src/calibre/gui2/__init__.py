168f583396297ffe659c02d137c32552abbf781d
Kovid Goyal <kovid@kovidgoyal.net>
2022-01-11 20:56:48 +0530

    ...

--- src/calibre/gui2/__init__.py
+++ src/calibre/gui2/__init__.py
@@ -138,8 +138,7 @@ class IconResourceManager:
             q = os.path.join(self.override_icon_path, name)
             if os.path.exists(q):
                 return QIcon(q)
-        name = '__'.join(name.split('/'))
-        return QIcon.fromTheme(os.path.splitext(name)[0])
+        return QIcon.fromTheme(os.path.splitext(name.replace('/', '__'))[0])
 
     def set_theme(self, is_dark_theme):
         QIcon.setThemeName(self.dark_theme_name if is_dark_theme else self.light_theme_name)
