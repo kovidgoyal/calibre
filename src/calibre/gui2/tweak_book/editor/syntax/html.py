a6800fb68505725c35773cacaec2472237c0fe2b
Kovid Goyal <kovid@kovidgoyal.net>
2022-02-04 12:45:25 +0530

    More pyqt6 goodness
    Have to wrap locales in QVariant otherwise pyqt6 treats them a
    QList<QTextLength>

--- src/calibre/gui2/tweak_book/editor/syntax/html.py
+++ src/calibre/gui2/tweak_book/editor/syntax/html.py
@@ -7,7 +7,7 @@ __copyright__ = '2013, Kovid Goyal <kovid at kovidgoyal.net>'
 import re
 from collections import namedtuple
 from functools import partial
-from qt.core import QFont, QTextBlockUserData, QTextCharFormat
+from qt.core import QFont, QTextBlockUserData, QTextCharFormat, QVariant
 
 from calibre.ebooks.oeb.polish.spell import html_spell_tags, patterns, xml_spell_tags
 from calibre.gui2.tweak_book import dictionaries, tprefs, verify_link
@@ -71,7 +71,7 @@ State = _speedup.State
 
 def spell_property(sfmt, locale):
     s = QTextCharFormat(sfmt)
-    s.setProperty(SPELL_LOCALE_PROPERTY, locale)
+    s.setProperty(SPELL_LOCALE_PROPERTY, QVariant(locale))
     return s
 
 
