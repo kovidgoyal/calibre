58e8bf98cdaeb30bc858887e6acfc39d46f6786c
Kovid Goyal <kovid@kovidgoyal.net>
2022-07-11 11:02:00 +0530

    Change the minimum version on windows to 10 in the installer
    Qt 6 does not support windows 8. https://www.qt.io/blog/qt6-development-hosts-and-targets

--- bypy/windows/wix.py
+++ bypy/windows/wix.py
@@ -14,7 +14,7 @@ if is64bit:
     UPGRADE_CODE = '5DD881FF-756B-4097-9D82-8C0F11D521EA'
 else:
     UPGRADE_CODE = 'BEB2A80D-E902-4DAD-ADF9-8BD2DA42CFE1'
-MINVERHUMAN = 'Windows 8'
+MINVERHUMAN = 'Windows 10'
 calibre_constants = globals()['calibre_constants']
 
 CANDLE = WIXP + r'\bin\candle.exe'
@@ -39,7 +39,7 @@ def create_installer(env):
         ProgramFilesFolder='ProgramFiles64Folder' if is64bit else 'ProgramFilesFolder',
         x64=' 64bit' if is64bit else '',
         minverhuman=MINVERHUMAN,
-        minver='602',
+        minver='603',
         fix_wix='<Custom Action="OverwriteWixSetDefaultPerMachineFolder" After="WixSetDefaultPerMachineFolder" />' if is64bit else '',
         compression='high',
         app_components=components,
