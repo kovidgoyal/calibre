da9426aaabfd85e7201b7edfb41483fa69238794
Kovid Goyal <kovid@kovidgoyal.net>
2022-09-26 22:30:49 +0530

    DRYer

--- src/calibre/ebooks/oeb/polish/toc.py
+++ src/calibre/ebooks/oeb/polish/toc.py
@@ -281,12 +281,8 @@ def find_existing_nav_toc(container):
 
 
 def mark_as_nav(container, name):
-    from calibre.ebooks.metadata.opf3 import read_prefixes, set_unique_property
     if container.opf_version_parsed.major > 2:
-        prefixes = read_prefixes(container.opf)
-        href = container.href_to_name(name, container.opf_name)
-        if set_unique_property('nav', container.opf, prefixes, href):
-            container.dirty(container.opf_name)
+        container.apply_unique_properties(name, 'nav')
 
 
 def get_x_toc(container, find_toc, parse_toc, verify_destinations=True):
