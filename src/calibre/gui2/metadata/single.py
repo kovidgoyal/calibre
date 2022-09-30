0f46046c798985514afdfc587a39afb84909c1b3
Kovid Goyal <kovid@kovidgoyal.net>
2022-09-30 15:42:51 +0530

    Ensure edit metadata dialog is not sized larger than screen size

--- src/calibre/gui2/metadata/single.py
+++ src/calibre/gui2/metadata/single.py
@@ -120,11 +120,16 @@ class MetadataSingleDialogBase(QDialog):
         self.comments_edit_state_at_apply = {self.comments:None}
 
         self.do_layout()
+        max_size = self.screen().availableSize()
+
         try:
             w, h = gprefs.get('metasingle_window_size')
-            self.resize(QSize(w, h))
+            sz = QSize(w, h)
         except Exception:
-            self.resize(self.sizeHint())
+            sz = self.sizeHint()
+        sz.setWidth(min(max_size.width(), sz.width()))
+        sz.setHeight(min(sz.height(), max_size.height()))
+        self.resize(sz)
         self.restore_widget_settings()
     # }}}
 
