e227e0f1232b667d8e6b3771a08df3182fbb6267
Guido Falsi <mad@madpilot.net>
2023-04-21 15:44:22 +0200

    Adapt union syntax to work on python 3.9

--- src/calibre/utils/copy_files.py
+++ src/calibre/utils/copy_files.py
@@ -127,7 +127,7 @@ class WindowsFileCopier:
             winutil.delete_file(make_long_path_useable(src_path))
 
 
-def get_copier() -> Union[UnixFileCopier | WindowsFileCopier]:
+def get_copier() -> Union[UnixFileCopier, WindowsFileCopier]:
     return WindowsFileCopier() if iswindows else UnixFileCopier()
 
 
