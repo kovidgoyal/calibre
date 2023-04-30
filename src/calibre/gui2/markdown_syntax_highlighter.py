9b7216b4ff7bc6dc96c2eacd93bde92f4aca36bf
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2023-04-30 14:00:18 +0200

    fix overflow

--- src/calibre/gui2/markdown_syntax_highlighter.py
+++ src/calibre/gui2/markdown_syntax_highlighter.py
@@ -14,12 +14,12 @@ from calibre.gui2.palette import dark_link_color, light_link_color
 class MarkdownHighlighter(QSyntaxHighlighter):
 
     MARKDOWN_KEYS_REGEX = {
-        'Bold' : re.compile(r'(?P<delim>\*\*)(?P<text>.+)(?P=delim)'),
-        'uBold': re.compile('(?P<delim>__)(?P<text>.+)(?P=delim)'),
-        'Italic': re.compile(r'(?P<delim>\*)(?P<text>([^*]{2,}|[^*]))(?P=delim)'),
-        'uItalic': re.compile('(?P<delim>_)(?P<text>([^_]{2,}|[^_]))(?P=delim)'),
-        'BoldItalic': re.compile(r'(?P<delim>\*\*\*)(?P<text>([^*]{2,}|[^*]))(?P=delim)'),
-        'uBoldItalic': re.compile(r'(?P<delim>___)(?P<text>([^_]{2,}|[^_]))(?P=delim)'),
+        'Bold' : re.compile(r'(?P<delim>\*\*)(?P<text>.+?)(?P=delim)'),
+        'uBold': re.compile('(?P<delim>__)(?P<text>.+?)(?P=delim)'),
+        'Italic': re.compile(r'(?P<delim>\*)(?P<text>([^*]{2,}?|[^*]))(?P=delim)'),
+        'uItalic': re.compile('(?P<delim>_)(?P<text>([^_]{2,}?|[^_]))(?P=delim)'),
+        'BoldItalic': re.compile(r'(?P<delim>\*\*\*)(?P<text>([^*]{2,}?|[^*]))(?P=delim)'),
+        'uBoldItalic': re.compile(r'(?P<delim>___)(?P<text>([^_]{2,}?|[^_]))(?P=delim)'),
         'Link': re.compile(r'(?u)(^|(?P<pre>[^!]))\[.*?\]:?[ ''\t'r']*\(?[^)]+\)?'),
         'Image': re.compile(r'(?u)!\[.*?\]\(.+?\)'),
         'HeaderAtx': re.compile(r'(?u)^\#{1,6}(.*?)\#*(''\n|$)'),
