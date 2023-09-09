7c406b5d010416f9bf4d0ff52849fc72d571074f
Kovid Goyal <kovid@kovidgoyal.net>
2023-09-09 09:37:55 +0530

    Kobo driver: Support updated firmware. Fixes #2034954 [Database error on Kobo sice firmware 4.38.21908](https://bugs.launchpad.net/calibre/+bug/2034954)

--- src/calibre/devices/kobo/driver.py
+++ src/calibre/devices/kobo/driver.py
@@ -1399,7 +1399,7 @@ class KOBOTOUCH(KOBO):
     # Starting with firmware version 3.19.x, the last number appears to be is a
     # build number. A number will be recorded here but it can be safely ignored
     # when testing the firmware version.
-    max_supported_fwversion         = (4, 37, 21533)
+    max_supported_fwversion         = (4, 38, 21908)
     # The following document firmware versions where new function or devices were added.
     # Not all are used, but this feels a good place to record it.
     min_fwversion_shelves           = (2, 0, 0)
