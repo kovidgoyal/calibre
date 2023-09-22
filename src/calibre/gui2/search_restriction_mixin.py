ceb1d9d2226aadb115c69579bfd6276a2d744da1
Kovid Goyal <kovid@kovidgoyal.net>
2023-09-22 19:54:23 +0530

    Fix #2035340 [[Enhancement] Save window size of the 'Create library based on' popup](https://bugs.launchpad.net/calibre/+bug/2035340)

--- src/calibre/gui2/search_restriction_mixin.py
+++ src/calibre/gui2/search_restriction_mixin.py
@@ -194,6 +194,7 @@ class CreateVirtualLibrary(QDialog):  # {{{
             self.vl_name.lineEdit().textEdited.connect(self.name_text_edited)
 
         self.resize(self.sizeHint()+QSize(150, 25))
+        self.restore_geometry(gprefs, 'create-virtual-library-dialog')
 
     def search_text_changed(self, txt):
         db = self.gui.current_db
@@ -315,7 +316,12 @@ class CreateVirtualLibrary(QDialog):  # {{{
 
         self.library_name = n
         self.library_search = v
+        self.save_geometry(gprefs, 'create-virtual-library-dialog')
         QDialog.accept(self)
+
+    def reject(self):
+        self.save_geometry(gprefs, 'create-virtual-library-dialog')
+        QDialog.reject(self)
 # }}}
 
 
