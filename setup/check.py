16fbab9362570a4d361c5092b55ad11efe050e2c
Paolo Melchiorre <paolo@melchiorre.org>
2023-11-22 11:37:17 +0100

    Fix minimum required Python version for ruff, black and pyupgrade

--- setup/check.py
+++ setup/check.py
@@ -137,4 +137,4 @@ class UpgradeSourceCode(Command):
             if '/metadata/sources/' in q or '/store/stores/' in q:
                 continue
             files.append(q)
-        subprocess.call(['pyupgrade', '--py37-plus'] + files)
+        subprocess.call(['pyupgrade', '--py38-plus'] + files)
