359647125795245cd894ae8735051a009a102ef5
Kovid Goyal <kovid@kovidgoyal.net>
2023-12-01 10:07:39 +0530

    Windows: Edit book: Fix a crash when using the check book tool with a book that contains malformed markup. Fixes #2045133 [Editor closes immediately if epub-check finds parsing-failures in too much text-files](https://bugs.launchpad.net/calibre/+bug/2045133)
    Bloody lxml doesnt initialize libxml2 correctly.

--- bypy/windows/site.py
+++ bypy/windows/site.py
@@ -60,6 +60,16 @@ def set_quit():
     builtins.exit = _sitebuiltins.Quitter('exit', eof)
 
 
+def workaround_lxml_bug():
+    # Without calling xmlInitParser() import lxml causes a segfault
+    import ctypes
+    x = ctypes.WinDLL('libxml2.dll')
+    x.xmlInitParser()
+    workaround_lxml_bug.libxml2 = x
+    from lxml import etree
+    del etree
+
+
 def main():
     sys.meta_path.insert(0, PydImporter())
     os.add_dll_directory(os.path.abspath(os.path.join(sys.app_dir, 'app', 'bin')))
@@ -74,6 +84,8 @@ def main():
 
     set_helper()
     set_quit()
+
+    workaround_lxml_bug()
 
     return run_entry_point()
 
