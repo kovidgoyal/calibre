1ea4040a0dba00b5482e173f81d91c5ec0a80b45
Kovid Goyal <kovid@kovidgoyal.net>
2024-02-25 13:40:53 +0530

    ...

--- src/calibre/gui2/store/stores/amazon_mx_plugin.py
+++ src/calibre/gui2/store/stores/amazon_mx_plugin.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # vim:fileencoding=utf-8
 # License: GPLv3 Copyright: 2015, Kovid Goyal <kovid at kovidgoyal.net>
-from __future__ import absolute_import, division, print_function, unicode_literals
 
-store_version = 20  # Needed for dynamic plugin loading
+store_version = 1  # Needed for dynamic plugin loading
 
 from calibre.gui2.store import StorePlugin
 try:
