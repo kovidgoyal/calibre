b0d3fe6677102c82232a02f990a7a5d488c68de4
Kovid Goyal <kovid@kovidgoyal.net>
2024-03-02 14:09:35 +0530

    DOCX Output: Preserve space before soft hyphen

--- src/calibre/ebooks/docx/writer/from_html.py
+++ src/calibre/ebooks/docx/writer/from_html.py
@@ -116,6 +116,12 @@ class TextRun:
                 if text:
                     for x in self.soft_hyphen_pat.split(text):
                         if x == '\u00ad':
+                            # trailing spaces in <w:t> before a soft hyphen are
+                            # ignored, so put them in a preserve whitespace
+                            # element with a single space.
+                            if not preserve_whitespace and len(r) and r[-1].text and r[-1].text.endswith(' '):
+                                r[-1].text = r[-1].text.rstrip()
+                                add_text(' ', True)
                             makeelement(r, 'w:softHyphen')
                         elif x:
                             add_text(x, preserve_whitespace)
