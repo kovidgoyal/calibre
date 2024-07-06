d01c4c03ac8dfc97aa45ad1cabbdb907d0d52051
Kovid Goyal <kovid@kovidgoyal.net>
2024-07-06 07:20:24 +0530

    typo caused disabling merge confirmation dialog to not work

--- src/calibre/gui2/dialogs/confirm_merge.py
+++ src/calibre/gui2/dialogs/confirm_merge.py
@@ -100,9 +100,8 @@ class ConfirmMerge(Dialog):
 
 
 def confirm_merge(msg, name, parent, mi, ask_about_save_alternate_cover=False):
-    config_set = dynamic
-    if not config_set.get(confirm_config_name(name), True):
-        True, bool(gprefs.get('choose-merge-cb-save_alternate_cover', False))
+    if not dynamic.get(confirm_config_name(name), True):
+        return True, bool(gprefs.get('choose-merge-cb-save_alternate_cover', False))
     d = ConfirmMerge(msg, name, parent, mi, ask_about_save_alternate_cover)
     return d.exec() == QDialog.DialogCode.Accepted, d.save_alternate_cover_cb.isChecked()
 
