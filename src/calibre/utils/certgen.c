60db6aae2fd207f82821c5ed00782a5f12fd0199
Kovid Goyal <kovid@kovidgoyal.net>
2024-08-16 17:18:00 +0530

    Proper fix for ssl server test
    Needed to generate subject key id for the CA cert and authority key id
    for the server cert

--- src/calibre/utils/certgen.c
+++ src/calibre/utils/certgen.c
@@ -272,15 +272,17 @@ static PyObject* create_rsa_cert(PyObject *self, PyObject *args) {
     if (!PubKey) { set_error("X509_REQ_get_pubkey"); goto error; }
     if (!X509_REQ_verify(req, PubKey)) { set_error("X509_REQ_verify"); goto error; }
     if (!X509_set_pubkey(Cert, PubKey)) { set_error("X509_set_pubkey"); goto error; }
-    if (!req_is_for_CA_cert) {
-        X509V3_CTX ctx;
-        X509V3_set_ctx(&ctx, Cert, Cert, NULL, NULL, 0);
+    X509_EXTENSION *ex;
+    if (req_is_for_CA_cert) {
+        X509V3_set_ctx(&ctx, NULL, Cert, NULL, NULL, 0);
         X509V3_set_ctx_nodb(&ctx);
-        X509_EXTENSION *ex;
         ex = X509V3_EXT_conf_nid(NULL, &ctx, NID_subject_key_identifier, "hash");
         if (!ex) { set_error("creating subject key identifier failed"); goto error; }
         X509_add_ext(Cert, ex, -1);
         X509_EXTENSION_free(ex);
+    } else {
+        X509V3_set_ctx(&ctx, CA_cert, Cert, NULL, NULL, 0);
+        X509V3_set_ctx_nodb(&ctx);
         ex = X509V3_EXT_conf_nid(NULL, &ctx, NID_authority_key_identifier, "keyid:always");
         if (!ex) { set_error("creating authority key identifier failed"); goto error; }
         X509_add_ext(Cert, ex, -1);
