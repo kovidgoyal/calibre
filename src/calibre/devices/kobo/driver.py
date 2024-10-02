7165448a44170fa736bf70372da7ec10685c5b29
Martin Brodbeck <martin@brodbeck-online.de>
2024-10-02 13:57:11 +0200

    Bump supported fw version in kobo/tolino driver
    New Tolino Shine 5 firmware 5.3.195056 still uses db version 190.

--- src/calibre/devices/kobo/driver.py
+++ src/calibre/devices/kobo/driver.py
@@ -1419,7 +1419,7 @@ class KOBOTOUCH(KOBO):
     # Starting with firmware version 3.19.x, the last number appears to be is a
     # build number. A number will be recorded here but it can be safely ignored
     # when testing the firmware version.
-    max_supported_fwversion         = (5, 2, 190625)
+    max_supported_fwversion         = (5, 3, 195056)
     # The following document firmware versions where new function or devices were added.
     # Not all are used, but this feels a good place to record it.
     min_fwversion_shelves           = (2, 0, 0)
