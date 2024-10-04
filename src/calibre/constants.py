871f262cac994c616f58c582b0c2899d711c8d09
Kovid Goyal <kovid@kovidgoyal.net>
2024-10-04 07:12:26 +0530

    Bump version for preview release

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -11,7 +11,7 @@ from functools import lru_cache
 from polyglot.builtins import environ_item, hasenv
 
 __appname__   = 'calibre'
-numeric_version = (7, 19, 0)
+numeric_version = (7, 19, 100)
 __version__   = '.'.join(map(str, numeric_version))
 git_version   = None
 __author__    = "Kovid Goyal <kovid@kovidgoyal.net>"
