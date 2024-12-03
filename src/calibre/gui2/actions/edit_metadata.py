daef0914cd2e6456927faf75cbc8ef3cfab2d319
Kovid Goyal <kovid@kovidgoyal.net>
2024-12-03 08:23:26 +0530

    Fix #2090888 [id_to_index failure during metadata edit](https://bugs.launchpad.net/calibre/+bug/2090888)

--- src/calibre/gui2/actions/edit_metadata.py
+++ src/calibre/gui2/actions/edit_metadata.py
@@ -506,8 +506,7 @@ class EditMetadataAction(InterfaceActionWithLibraryDrop):
         if id_ is None:
             view._view_file(fmt)
         else:
-            db = self.gui.library_view.model().db
-            view.view_format(db.row(id_), fmt)
+            view.view_format_by_id(id_, fmt)
 
     def edit_format_callback(self, id_, fmt):
         edit = self.gui.iactions['Tweak ePub']
