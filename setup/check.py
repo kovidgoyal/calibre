f172c6770670ebc319bf92cc95496caf7368b26e
Kovid Goyal <kovid@kovidgoyal.net>
2025-01-26 10:35:19 +0530

    ./setup.py check now uses strict checking
    No need to maintain two ruff configs. And strict checking happens in
    editors/CI automatically, making my life a bit easier.
    
    Remove the no longer needed --strict and --pep8 options from the check
    command.

--- setup/check.py
+++ setup/check.py
@@ -23,13 +23,6 @@ class Message:
         return f'{self.filename}:{self.lineno}: {self.msg}'
 
 
-def get_ruff_config(is_strict_check):
-    if is_strict_check:
-        return ['--config=ruff-strict-pep8.toml']
-    else:
-        return []
-
-
 def files_walker(root_path, ext):
     for x in os.walk(root_path):
         for f in x[-1]:
@@ -50,15 +43,10 @@ class Check(Command):
     description = 'Check for errors in the calibre source code'
 
     CACHE = 'check.json'
-    CACHE_STRICT = 'check-strict.json'
 
     def add_options(self, parser):
         parser.add_option('--fix', '--auto-fix', default=False, action='store_true',
-                help='Try to automatically fix some of the smallest errors')
-        parser.add_option('--pep8', '--pep8-commit', default=False, action='store_true',
-                help='Try to automatically fix some of the smallest errors, then perform a pep8 commit')
-        parser.add_option('--strict', '--strict-pep8', default=False, action='store_true',
-                help='Perform the checking more strictely. See the file "strict-pep8.toml"')
+                help='Try to automatically fix some of the smallest errors instead of opening an editor for bad files.')
 
     def get_files(self):
         yield from checkable_python_files(self.SRC)
@@ -86,7 +74,7 @@ class Check(Command):
 
     @property
     def cache_file(self):
-        return self.j(build_cache_dir(), self.CACHE_STRICT if self.is_strict_check else self.CACHE)
+        return self.j(build_cache_dir(), self.CACHE)
 
     def save_cache(self, cache):
         dump_json(cache, self.cache_file)
@@ -94,7 +82,10 @@ class Check(Command):
     def file_has_errors(self, f):
         ext = os.path.splitext(f)[1]
         if ext in {'.py', '.recipe'}:
-            p = subprocess.Popen(['ruff', 'check', f] + get_ruff_config(self.is_strict_check))
+            if self.auto_fix:
+                p = subprocess.Popen(['ruff', 'check', '--fix', f])
+            else:
+                p = subprocess.Popen(['ruff', 'check', f])
             return p.wait() != 0
         if ext == '.pyj':
             p = subprocess.Popen(['rapydscript', 'lint', f])
@@ -103,35 +94,12 @@ class Check(Command):
             p = subprocess.Popen(['python', self.j(self.wn_path, 'whats_new.py'), f])
             return p.wait() != 0
 
-    def perform_auto_fix(self):
-        cp = subprocess.run(['ruff', 'check', '--fix-only'], stdout=subprocess.PIPE)
-        if cp.returncode != 0:
-            raise SystemExit('ruff fixing failed')
-        return cp.stdout.decode('utf-8') or 'Fixed 0 errors.'
-
-    def perform_pep8_git_commit(self):
-        return subprocess.run(['git', 'commit', '--all', '-m', 'pep8']).returncode != 0
-
-    def check_errors_remain(self):
-        return subprocess.run(['ruff', 'check', '--statistics']).returncode != 0
-
     def run(self, opts):
-        if opts.fix and opts.pep8:
-            self.info('setup.py check: error: options --fix and --pep8 are mutually exclusive')
-            raise SystemExit(2)
-        if opts.strict and opts.pep8:
-            self.info('setup.py check: error: options --strict and --pep8 are mutually exclusive')
-            raise SystemExit(2)
-
         self.fhash_cache = {}
         self.wn_path = os.path.expanduser('~/work/srv/main/static')
         self.has_changelog_check = os.path.exists(self.wn_path)
         self.auto_fix = opts.fix
-        self.is_strict_check = opts.strict
-        if opts.pep8:
-            self.run_pep8_commit()
-        else:
-            self.run_check_files()
+        self.run_check_files()
 
     def run_check_files(self):
         cache = {}
@@ -141,10 +109,6 @@ class Check(Command):
         except OSError as err:
             if err.errno != errno.ENOENT:
                 raise
-        if self.auto_fix:
-            self.info('\tAuto-fixing')
-            msg = self.perform_auto_fix()
-            self.info(msg+'\n')
         dirty_files = tuple(f for f in self.get_files() if not self.is_cache_valid(f, cache))
         try:
             for i, f in enumerate(dirty_files):
@@ -161,28 +125,16 @@ class Check(Command):
         finally:
             self.save_cache(cache)
 
-    def run_pep8_commit(self):
-        require_git_master()
-        require_clean_git()
-        msg = self.perform_auto_fix()
-        self.info(msg+'\n')
-        self.info('Commit the pep8 change...')
-        self.perform_pep8_git_commit()
-        self.info()
-        if self.check_errors_remain():
-            self.info('There are remaining errors. Execute "setup.py check" without options to locate them.')
-
     def report_errors(self, errors):
         for err in errors:
             self.info('\t\t', str(err))
 
     def clean(self):
-        for cache_file in [self.j(build_cache_dir(), self.CACHE), self.j(build_cache_dir(), self.CACHE_STRICT)]:
-            try:
-                os.remove(cache_file)
-            except OSError as err:
-                if err.errno != errno.ENOENT:
-                    raise
+        try:
+            os.remove(self.cache_file)
+        except OSError as err:
+            if err.errno != errno.ENOENT:
+                raise
 
 
 class UpgradeSourceCode(Command):
