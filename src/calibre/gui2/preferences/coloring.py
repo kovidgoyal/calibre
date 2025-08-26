2a390961d6d685bc7cf46d3c06eae17b8d2cb2b4
Kovid Goyal <kovid@kovidgoyal.net>
2025-08-26 08:10:31 +0530

    string changes

--- src/calibre/gui2/preferences/coloring.py
+++ src/calibre/gui2/preferences/coloring.py
@@ -1063,7 +1063,7 @@ class EditRules(QWidget):  # {{{
         b.setToolTip('<p>' + _('Click to move the row up 1. Shift+click '
                        'to move the row up 5. Ctrl+click to move the row up 10. '
                        'Ctrl+Shift+click to move the row to the top. '
-                       'Keyboard shortcut: Ctrl-Up arrow') + '</p>')
+                       'Keyboard shortcut: Ctrl+Up arrow') + '</p>')
         b.clicked.connect(partial(self.move_rows, moving_up=True))
         g.addWidget(b, 0, 1, 1, 1, Qt.AlignmentFlag.AlignTop)
         self.down_button = b = QToolButton(self)
@@ -1071,7 +1071,7 @@ class EditRules(QWidget):  # {{{
         b.setToolTip('<p>' + _('Click to move the row down 1. Shift+click to move the '
                         'row down 5. Ctrl+click to move the row down 10. Ctrl+Shift+click '
                         'to move the row to the bottom. '
-                        'Keyboard shortcut: Ctrl-Down arrow') + '</p>')
+                        'Keyboard shortcut: Ctrl+Down arrow') + '</p>')
         b.clicked.connect(partial(self.move_rows, moving_up=False))
         self.rules_view.set_movement_functions(partial(self.move_rows, moving_up=True),
                                                partial(self.move_rows, moving_up=False))
