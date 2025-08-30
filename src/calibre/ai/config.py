51a301c50b4a39f4ff13b4d142fd7332f5760af1
Kovid Goyal <kovid@kovidgoyal.net>
2025-08-30 13:17:21 +0530

    Start work on configuration UI for OpenRouter

---
+++ src/calibre/ai/config.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2025, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+from copy import deepcopy
+from functools import lru_cache
+from typing import Any
+
+from calibre.utils.config import JSONConfig
+
+
+@lru_cache(2)
+def prefs() -> JSONConfig:
+    ans = JSONConfig('ai')
+    ans.defaults['providers'] = {}
+    return ans
+
+
+def pref_for_provider(name: str, key: str, defval: Any = None) -> Any:
+    return prefs()['providers'].get(key, defval)
+
+
+def set_prefs_for_provider(name: str, pref_map: dict[str, Any]) -> None:
+    p = prefs()
+    p['providers'][name] = deepcopy(pref_map)
+    p.set('providers', p['providers'])
