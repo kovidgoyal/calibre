971143faa3a7a45032c5bea244a851d7104e16a7
Kovid Goyal <kovid@kovidgoyal.net>
2025-09-02 21:06:11 +0530

    Abstract message types when chatting with AI

--- src/calibre/ai/__init__.py
+++ src/calibre/ai/__init__.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python
 # License: GPLv3 Copyright: 2025, Kovid Goyal <kovid at kovidgoyal.net>
 
-from enum import Flag, auto
+from enum import Enum, Flag, auto
+from typing import Any, NamedTuple
+
+
+class ChatMessageType(Enum):
+    system = auto()
+    user = auto()
+    assistant = auto()
+    tool = auto()
+    developer = auto()
+
+
+class ChatMessage(NamedTuple):
+    id: int
+    query: str
+    type: ChatMessageType = ChatMessageType.user
+    extra_data: Any = None
+
+    @property
+    def from_assistant(self) -> bool:
+        return self.type is ChatMessageType.assistant
+
+    def for_assistant(self) -> dict[str, str]:
+        return {'role': self.type.value, 'content': self.query}
+
+    def for_display_to_human(self) -> str:
+        if self.type is ChatMessageType.system:
+            return ''
+        from html import escape
+        return escape(self.query).replace('\n', '<br>')
 
 
 class AICapabilities(Flag):
