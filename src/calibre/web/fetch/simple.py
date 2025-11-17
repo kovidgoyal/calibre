9c6a346fee44cb0623a6554b682b1a480a9faf8f
un-pogaz <46523284+un-pogaz@users.noreply.github.com>
2025-11-17 09:40:13 +0100

    http_client import (manual 1)

--- src/calibre/web/fetch/simple.py
+++ src/calibre/web/fetch/simple.py
@@ -18,6 +18,7 @@ import threading
 import time
 import traceback
 from base64 import standard_b64decode
+from http.client import responses
 from urllib.request import urlopen
 
 from calibre import browser, relpath, unicode_path
@@ -30,7 +31,6 @@ from calibre.utils.imghdr import what
 from calibre.utils.localization import _
 from calibre.utils.logging import Log
 from calibre.web.fetch.utils import rescale_image
-from polyglot.http_client import responses
 from polyglot.urllib import URLError, quote, url2pathname, urljoin, urlparse, urlsplit, urlunparse, urlunsplit
 
 
