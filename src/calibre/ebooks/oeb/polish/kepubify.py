d4a11a89303208a839e060f1e1ab558c9e26b037
xyzzy-foo <plus2ch@gmail.com>
2026-01-10 11:20:25 +0900

    KEPUB Output: Fix for issue where Tate-chu-yoko is not working

--- src/calibre/ebooks/oeb/polish/kepubify.py
+++ src/calibre/ebooks/oeb/polish/kepubify.py
@@ -251,7 +251,18 @@ def add_kobo_spans(inner, root_lang, prefer_justification=False):
             if child.tail:
                 a((child.tail, node, child, node_lang))
             if child_name not in SKIPPED_TAGS:
-                a((child, None, child_name, lang_for_elem(child, node_lang)))
+                is_tcy = False
+                if child_name == 'span':
+                    style = child.get('style') or ''
+                    if 'text-combine' in style:
+                        is_tcy = True
+                    elif 'tcy' in (child.get('class') or '').split():
+                        is_tcy = True
+
+                if is_tcy:
+                    wrap_child(child)
+                else:
+                    a((child, None, child_name, lang_for_elem(child, node_lang)))
         if node.text:
             wrap_text_in_spans(node.text, node, None, node_lang)
 
