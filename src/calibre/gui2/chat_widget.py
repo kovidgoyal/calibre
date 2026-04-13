e5e9ad405545f42eb74c65273ed3e36178eeb1ae
Kovid Goyal <kovid@kovidgoyal.net>
2026-04-13 19:54:53 +0530

    Remove unused code

--- src/calibre/gui2/chat_widget.py
+++ src/calibre/gui2/chat_widget.py
@@ -203,11 +203,6 @@ class ChatWidget(QWidget):
         bg = self.response_color if is_response else self.base_color
         self.blocks.append(self.wrap_content_in_padding_table(html, bg))
 
-    def replace_last_block(self, body_html: str, header: Header = Header(), is_response: bool = False) -> None:
-        if self.blocks:
-            del self.blocks[-1]
-        self.add_block(body_html, header, is_response)
-
     def show_message(self, msg_html: str, details: str = '', level: int = INFO) -> None:
         self.blocks = []
         style = ''
