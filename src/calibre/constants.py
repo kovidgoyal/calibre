b09d30a6600bf444b6aafe6451360adda36c8188
Kovid Goyal <kovid@kovidgoyal.net>
2026-05-08 14:39:07 +0530

    Missed a couple of places that needed to change SSL_CERT_FILE to SSL_CERT_DIR

--- src/calibre/constants.py
+++ src/calibre/constants.py
@@ -522,12 +522,12 @@ def sanitize_env_vars():
     elif iswindows:
         env_vars = {'OPENSSL_MODULES': None, 'QTWEBENGINE_DISABLE_SANDBOX': None}
         if os.environ.get('CALIBRE_USE_SYSTEM_CERTIFICATES', '') != '1':
-            env_vars['SSL_CERT_FILE'] = None
+            env_vars['SSL_CERT_DIR'] = None
     elif ismacos:
         env_vars = {k:None for k in (
                     'FONTCONFIG_FILE FONTCONFIG_PATH OPENSSL_ENGINES OPENSSL_MODULES').split()}
         if os.environ.get('CALIBRE_USE_SYSTEM_CERTIFICATES', '') != '1':
-            env_vars['SSL_CERT_FILE'] = None
+            env_vars['SSL_CERT_DIR'] = None
     else:
         env_vars = {}
 
