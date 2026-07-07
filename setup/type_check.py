070775ac97665aba7f8ff5e71d60f8bdcebc1988
Kovid Goyal <kovid@kovidgoyal.net>
2026-07-07 17:33:08 +0530

    A type_check sub-command to setup.py

---
+++ setup/type_check.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# License: GPL v3 Copyright: 2026, Kovid Goyal <kovid at kovidgoyal.net>
+
+import subprocess
+
+from setup import Command, is_ci
+
+
+class TypeCheck(Command):
+
+    description = 'Run the ty type checker over the calibre source code'
+    usage_help = 'To type check specific files, specify them as command line arguments'
+
+    @property
+    def project_root(self):
+        return self.d(self.SRC)
+
+    @property
+    def venv_dir(self):
+        return self.j(self.project_root, '.venv')
+
+    def ensure_venv(self):
+        if not self.e(self.venv_dir):
+            subprocess.check_call(['uv', 'venv'], cwd=self.project_root)
+        deps = subprocess.check_output(['uv', 'pip', 'compile', 'pyproject.toml'], cwd=self.project_root)
+        subprocess.run(['uv', 'pip', 'sync', '-'], check=True, input=deps, cwd=self.project_root)
+
+    def run(self, opts):
+        if not is_ci:
+            self.ensure_venv()
+        cp = subprocess.run(['ty', 'check'] + list(opts.cli_args), cwd=self.project_root)
+        if cp.returncode != 0:
+            raise SystemExit(cp.returncode)
