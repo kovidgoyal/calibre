9aa22c6aaf379dda0410114198487c50a8f13295
Kovid Goyal <kovid@kovidgoyal.net>
2026-08-26 06:28:30 +0530

    ...

---
+++ src/calibre/gui2/dialogs/llm_cover.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# License: GPLv3 Copyright: 2026, Kovid Goyal <kovid at kovidgoyal.net>
+
+
+from calibre.gui2.widgets2 import Dialog
+
+
+class CoverCreateDialog(Dialog):
+    pass
