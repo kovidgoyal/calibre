8bbd03083093e16ff6950f9338ee70d453604f02
Kovid Goyal <kovid@kovidgoyal.net>
2026-08-30 21:59:14 +0530

    Use up to date podofo in Arch CI

--- setup/arch-ci.sh
+++ setup/arch-ci.sh
@@ -5,7 +5,16 @@
 
 set -xe
 
-pacman -S --noconfirm --needed base-devel sudo git sip pyqt-builder cmake chmlib icu jxrlib hunspell libmtp libusb libwmf optipng python-apsw python-beautifulsoup4 python-cssselect python-css-parser python-dateutil python-jeepney python-dnspython python-feedparser python-html2text python-html5-parser python-lxml python-lxml-html-clean python-markdown python-mechanize python-msgpack python-netifaces python-unrardll python-pillow python-psutil python-pygments python-pyqt6 python-regex python-zeroconf python-pyqt6-webengine qt6-svg qt6-imageformats qt6-speech udisks2 hyphen python-pychm python-pycryptodome speech-dispatcher python-sphinx python-urllib3 python-py7zr python-pip python-fonttools python-xxhash python-tzdata python-tzlocal uchardet libstemmer poppler tk podofo libwebp-utils python-jaconv python-pykakasi protobuf onnxruntime uv
+pacman -S --noconfirm --needed base-devel sudo git sip pyqt-builder cmake chmlib icu jxrlib hunspell libmtp libusb libwmf optipng python-apsw python-beautifulsoup4 python-cssselect python-css-parser python-dateutil python-jeepney python-dnspython python-feedparser python-html2text python-html5-parser python-lxml python-lxml-html-clean python-markdown python-mechanize python-msgpack python-netifaces python-unrardll python-pillow python-psutil python-pygments python-pyqt6 python-regex python-zeroconf python-pyqt6-webengine qt6-svg qt6-imageformats qt6-speech udisks2 hyphen python-pychm python-pycryptodome speech-dispatcher python-sphinx python-urllib3 python-py7zr python-pip python-fonttools python-xxhash python-tzdata python-tzlocal uchardet libstemmer poppler tk libwebp-utils python-jaconv python-pykakasi protobuf onnxruntime uv
+
+curl -L "https://github.com/podofo/podofo/archive/refs/tags/1.1.2.tar.gz" -o podofo.tar.gz
+tar xf podofo.tar.gz && rm podofo.tar.gz
+chown -R nobody:nobody podofo-1.1.2
+cd podofo-1.1.2
+cmake -B build -S . -DCMAKE_INSTALL_PREFIX=/usr -DPODOFO_BUILD_STATIC=FALSE
+cmake --build build
+cmake --install build
+cd .. && rm -rf podofo-1.1.2
 
 git clone --depth=1 https://github.com/espeak-ng/espeak-ng.git
 chown -R nobody:nobody espeak-ng
